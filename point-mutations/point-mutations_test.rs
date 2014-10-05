2cb369f848b6159144fd20c2bb654f20c5541e61
Peter Minten <peter@pminten.nl>
2014-10-05 14:06:16 +0200

    Remove unequal length tests for point-mutations'
    Fixes #16

--- point-mutations/point-mutations_test.rs
+++ point-mutations/point-mutations_test.rs
@@ -23,16 +23,6 @@ fn test_complete_hamming_distance_in_small_strand() {
 
 #[test]
 #[ignore]
-fn test_hamming_distance_in_off_by_one_strand() {
-    assert_eq!(
-        dna::hamming_distance(
-            "GGACGGATTCTGACCTGGACTAATTTTGGGG",
-            "AGGACGGATTCTGACCTGGACTAATTTTGGGG"),
-        19);
-}
-
-#[test]
-#[ignore]
 fn test_small_hamming_distance_in_the_middle_somewhere() {
     assert_eq!(dna::hamming_distance("GGACG", "GGTCG"), 1);
 }
@@ -41,30 +31,4 @@ fn test_small_hamming_distance_in_the_middle_somewhere() {
 #[ignore]
 fn test_larger_distance() {
     assert_eq!(dna::hamming_distance("ACCAGGG", "ACTATGG"), 2);
-}
-
-#[test]
-#[ignore]
-fn test_ignores_extra_length_on_the_other_strand_when_longer() {
-    assert_eq!(dna::hamming_distance("AAACTAGGGG", "AGGCTAGCGGTAGGAC"), 3);
-}
-
-#[test]
-#[ignore]
-fn test_ignores_extra_length_on_the_original_strand_when_longer() {
-    assert_eq!(
-        dna::hamming_distance("GACTACGGACAGGGTAGGGAAT", "GACATCGCACACC"),
-        5);
-}
-
-#[test]
-#[ignore]
-fn test_hamming_distance() {
-    let cases = [
-        ("AGACAACAGCCAGCCGCCGGATT", "AGGCAA", 1),
-        ("AGACAACAGCCAGCCGCCGGATT", "AGACATCTTTCAGCCGCCGGATTAGaGCAA", 4),
-        ("AGACAACAGCCAGCCGCCGGATT", "AGG", 1)];
-    for &(a, b, expect) in cases.iter() {
-        assert_eq!(dna::hamming_distance(a, b), expect);
-    } 
 }
