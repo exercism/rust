05981e9ca24298d1bd1e2ce047da78c8f3c87714
Peter Minten <peter@pminten.nl>
2015-01-13 15:46:28 +0100

    Fix nucleotide count for newest rust

--- nucleotide-count/example.rs
+++ nucleotide-count/example.rs
@@ -1,18 +1,20 @@
-use std::collections::HashMap;
-use std::collections::hashmap::{Occupied, Vacant};
+#![allow(unstable)] // for entry
 
-pub fn count(nucleotide: char, input: &str) -> uint {
+use std::collections::BTreeMap;
+use std::collections::btree_map::Entry;
+
+pub fn count(nucleotide: char, input: &str) -> usize {
     input.chars().filter(|&c| c == nucleotide).count()
 }
 
-pub fn nucleotide_counts(input: &str) -> HashMap<char, uint> {
-    let mut map: HashMap<char, uint> = "ACGT".chars().map(|c| (c, 0)).collect();
+pub fn nucleotide_counts(input: &str) -> BTreeMap<char, usize> {
+    let mut map: BTreeMap<char, usize> = "ACGT".chars().map(|c| (c, 0)).collect();
     for nucleotide in input.chars() {
         match map.entry(nucleotide) {
-            Vacant(entry) => entry.set(1),
-            Occupied(mut entry) => {
-                *entry.get_mut() += 1;
-                entry.into_mut()
+            Entry::Vacant(view) => view.insert(1),
+            Entry::Occupied(mut view) => {
+                *view.get_mut() += 1;
+                view.into_mut()
             }
         };
     }
--- nucleotide-count/nucleotide-count_test.rs
+++ nucleotide-count/nucleotide-count_test.rs
@@ -1,21 +1,21 @@
 #![crate_name = "nucleotide-count_test"]
 #![crate_type = "lib"]
 
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 mod dna;
 
-fn check_dna(s: &str, pairs: &[(char, uint)]) {
+fn check_dna(s: &str, pairs: &[(char, usize)]) {
     // The reason for the awkward code in here is to ensure that the failure
     // message for assert_eq! is as informative as possible. A simpler
     // solution would simply check the length of the map, and then
     // check for the presence and value of each key in the given pairs vector.
-    let mut m: HashMap<char, uint> = dna::nucleotide_counts(s);
+    let mut m: BTreeMap<char, usize> = dna::nucleotide_counts(s);
     for &(k, v) in pairs.iter() {
-        assert_eq!((k, m.pop(&k).unwrap()), (k, v));
+        assert_eq!((k, m.remove(&k).unwrap()), (k, v));
     }
     // may fail with a message that clearly shows all extra pairs in the map
-    assert_eq!(m.iter().collect::<Vec<(&char,&uint)>>(), vec!());
+    assert_eq!(m.iter().collect::<Vec<(&char,&usize)>>(), vec!());
 }
 
 #[test]
@@ -41,7 +41,7 @@ fn test_count_only_thymine() {
 fn test_nucleotide_count_empty() {
     check_dna(
         "",
-        [('A', 0), ('T', 0), ('C', 0), ('G', 0)]);
+        &[('A', 0), ('T', 0), ('C', 0), ('G', 0)]);
 }
 
 #[test]
@@ -49,7 +49,7 @@ fn test_nucleotide_count_empty() {
 fn test_nucleotide_count_only_guanine() {
     check_dna(
         "GGGGGGGG",
-        [('A', 0), ('T', 0), ('C', 0), ('G', 8)]);
+        &[('A', 0), ('T', 0), ('C', 0), ('G', 8)]);
 }
 
 #[test]
@@ -58,5 +58,5 @@ fn test_nucleotide_count_counts_all() {
     check_dna(
         "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAA\
          GAGTGTCTGATAGCAGC",
-        [('A', 20), ('T', 21), ('C', 12), ('G', 17)]);
+        &[('A', 20), ('T', 21), ('C', 12), ('G', 17)]);
 }
