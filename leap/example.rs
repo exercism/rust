42f4c5944dd8c170f3c416e2d6024566b184eeaa
Peter Minten <peter@pminten.nl>
2015-01-13 15:41:01 +0100

    Fix leap for newest rust

--- leap/example.rs
+++ leap/example.rs
@@ -1,4 +1,4 @@
-pub fn is_leap_year(year: int) -> bool {
-    let has_factor = |n| year % n == 0;
+pub fn is_leap_year(year: i32) -> bool {
+    let has_factor = |&:n| year % n == 0;
     has_factor(4) && (!has_factor(100) || has_factor(400))
 }
