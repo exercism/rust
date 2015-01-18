85c7d8e42784b49c54ecafb6b1852a00f6d09d41
Peter Minten <peter@pminten.nl>
2015-01-18 15:40:28 +0100

    Update word-count for latest rust

--- word-count/example.rs
+++ word-count/example.rs
@@ -1,15 +1,16 @@
-use std::collections::HashMap;
-use std::collections::hashmap::{Occupied, Vacant};
+#![allow(unstable)] // Entry, to_lowercase()
 
-pub fn word_count(input: &str) -> HashMap<String, uint> {
-    let mut map: HashMap<String, uint> = HashMap::new();
-    let norm = input.chars().map(|c| c.to_lowercase()).collect::<String>();
-    for word in norm.as_slice().split(|c: char| !c.is_alphanumeric()).filter(|s| !s.is_empty()) {
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
+
+pub fn word_count(input: &str) -> HashMap<String, u32> {
+    let mut map: HashMap<String, u32> = HashMap::new();
+    let norm = input.chars().map(|&: c| c.to_lowercase()).collect::<String>();
+    for word in norm.as_slice().split(|&: c: char| !c.is_alphanumeric()).filter(|s| !s.is_empty()) {
         match map.entry(word.to_string()) {
-            Vacant(entry) => entry.set(1),
-            Occupied(mut entry) => {
+            Entry::Vacant(entry) => { entry.insert(1); },
+            Entry::Occupied(mut entry) => {
                 *entry.get_mut() += 1;
-                entry.into_mut()
             }
         };
     }
--- word-count/word-count_test.rs
+++ word-count/word-count_test.rs
@@ -1,34 +1,36 @@
 #![crate_name = "word-count_test"]
 #![crate_type = "lib"]
 
+#![allow(unstable)] // to_string
+
 use std::collections::HashMap;
 
 mod word_count;
 
-fn check_word_count(s: &str, pairs: &[(&str, uint)]) {
+fn check_word_count(s: &str, pairs: Vec<(&str, u32)>) {
     // The reason for the awkward code in here is to ensure that the failure
     // message for assert_eq! is as informative as possible. A simpler
     // solution would simply check the length of the map, and then
     // check for the presence and value of each key in the given pairs vector.
-    let mut m: HashMap<String, uint> = word_count::word_count(s);
+    let mut m: HashMap<String, u32> = word_count::word_count(s);
     for &(k, v) in pairs.iter() {
-        assert_eq!((k, m.pop(&k.to_string()).unwrap()), (k, v));
+        assert_eq!((k, m.remove(&k.to_string()).unwrap()), (k, v));
     }
     // may fail with a message that clearly shows all extra pairs in the map
-    assert_eq!(m.iter().collect::<Vec<(&String,&uint)>>(), vec!());
+    assert_eq!(m.iter().collect::<Vec<(&String,&u32)>>(), vec!());
 }
 
 #[test]
 #[ignore]
 fn test_no_words() {
-    check_word_count("", []);
-    check_word_count("-!@#$ $#() @@@", []);
+    check_word_count("", vec![]);
+    check_word_count("-!@#$ $#() @@@", vec![]);
 }
 
 #[test]
 #[ignore]
 fn test_count_one_word() {
-    check_word_count("word", [("word", 1)]);
+    check_word_count("word", vec![("word", 1)]);
 }
 
 #[test]
@@ -36,9 +38,9 @@ fn test_count_one_word() {
 fn test_count_one_of_each() {
     check_word_count(
         "one of each",
-        [("one", 1),
-         ("of", 1),
-         ("each", 1)]);
+        vec![("one", 1),
+             ("of", 1),
+             ("each", 1)]);
 }
 
 #[test]
@@ -46,11 +48,11 @@ fn test_count_one_of_each() {
 fn test_count_multiple_occurrences() {
     check_word_count(
         "one fish two fish red fish blue fish",
-        [("one", 1),
-         ("fish", 4),
-         ("two", 1),
-         ("red", 1),
-         ("blue", 1)]);
+        vec![("one", 1),
+             ("fish", 4),
+             ("two", 1),
+             ("red", 1),
+             ("blue", 1)]);
 }
 
 #[test]
@@ -58,11 +60,11 @@ fn test_count_multiple_occurrences() {
 fn test_ignore_punctuation() {
     check_word_count(
         "car : carpet as java : javascript!!&@$%^&",
-        [("car", 1),
-         ("carpet", 1),
-         ("as", 1),
-         ("java", 1),
-         ("javascript", 1)]);
+        vec![("car", 1),
+             ("carpet", 1),
+             ("as", 1),
+             ("java", 1),
+             ("javascript", 1)]);
 }
 
 #[test]
@@ -70,9 +72,9 @@ fn test_ignore_punctuation() {
 fn test_include_numbers() {
     check_word_count(
         "testing, 1, 2 testing",
-        [("testing", 2),
-         ("1", 1),
-         ("2", 1)]);
+        vec![("testing", 2),
+             ("1", 1),
+             ("2", 1)]);
 }
 
 #[test]
@@ -80,7 +82,7 @@ fn test_include_numbers() {
 fn test_normalize_case() {
     check_word_count(
         "go Go GO",
-        [("go", 3)]);
+        vec![("go", 3)]);
 }
 
 #[test]
@@ -88,9 +90,9 @@ fn test_normalize_case() {
 fn test_prefix_punctuation() {
     check_word_count(
         "!%%#testing, 1, 2 testing",
-        [("testing", 2),
-         ("1", 1),
-         ("2", 1)]);
+        vec![("testing", 2),
+             ("1", 1),
+             ("2", 1)]);
 }
 
 #[test]
@@ -98,9 +100,9 @@ fn test_prefix_punctuation() {
 fn test_symbols_are_separators() {
     check_word_count(
         "hey,my_spacebar_is_broken.",
-        [("hey", 1),
-         ("my", 1),
-         ("spacebar", 1),
-         ("is", 1),
-         ("broken", 1)]);
+        vec![("hey", 1),
+             ("my", 1),
+             ("spacebar", 1),
+             ("is", 1),
+             ("broken", 1)]);
 }
