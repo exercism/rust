f9685ed6926b907522fe34ed9c6b421b52be0b39
Andrew Clarkson <andrew@clarkson.mn>
2015-04-04 18:18:36 -0500

    Remove all references to unstable collections

--- word-count/example.rs
+++ word-count/example.rs
@@ -1,13 +1,20 @@
-#![feature(convert)]
-#![feature(collections)]
-
 use std::collections::HashMap;
 use std::collections::hash_map::Entry;
 
+fn lowercase(word: &str) -> String {
+    let mut lower = String::with_capacity(word.len());
+    for c in word.chars() {
+        for low in c.to_lowercase() {
+            lower.push(low);
+        }
+    }
+    lower
+}
+
 pub fn word_count(input: &str) -> HashMap<String, u32> {
     let mut map: HashMap<String, u32> = HashMap::new();
-    let norm = input.to_lowercase();
-    let slice: &str = norm.as_ref();
+    let lower = lowercase(input);
+    let slice: &str = lower.as_ref();
     for word in slice.split(|c: char| !c.is_alphanumeric()).filter(|s| !s.is_empty()) {
         match map.entry(word.to_string()) {
             Entry::Vacant(entry) => { entry.insert(1); },
