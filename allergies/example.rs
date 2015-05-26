8b12f86fd6893a1fd08dd6bfcfd9a0162521f204
Adolfo Silva <code@anomi.co>
2015-05-26 21:46:51 +0100

    Add implementation of allergies exercise

---
+++ allergies/example.rs
@@ -0,0 +1,22 @@
+pub struct Allergies(pub usize);
+
+#[derive(PartialEq, Debug)]
+pub enum Allergen { Eggs, Peanuts, Shellfish, Strawberries, Tomatoes, Chocolate, Pollen, Cats }
+
+impl Allergies {
+    pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
+        let allergens = Allergies::allergens();
+        let index = allergens.iter().position(|x: &Allergen| x == allergen).unwrap();
+        match self.0 & 1 << index { 0 => false, _ => true }
+    }
+
+    pub fn allergies(&self) -> Vec<Allergen> {
+        Allergies::allergens().into_iter().filter(|allergen| self.is_allergic_to(allergen)).collect()
+    }
+
+    fn allergens() -> Vec<Allergen> {
+        vec![Allergen::Eggs, Allergen::Peanuts, Allergen::Shellfish,
+            Allergen::Strawberries, Allergen::Tomatoes, Allergen::Chocolate,
+            Allergen::Pollen, Allergen::Cats]
+    }
+}
