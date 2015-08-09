dd1d196bcfa3e90076f37165ff493a27e40ef86e
Sébastien Duquette <ekse.0x@gmail.com>
2015-08-08 22:53:02 -0400

    handle error case when strings have different lengths in point-mutation (issue #45)

--- point-mutations/example.rs
+++ point-mutations/example.rs
@@ -1,3 +1,7 @@
-pub fn hamming_distance(a: &str, b: &str) -> usize {
-    a.chars().zip(b.chars()).filter(|&(a, b)| a != b).count()
+pub fn hamming_distance(a : &str, b: &str) -> Result<usize, &'static str> {
+    if a.len() != b.len() {
+        return Result::Err("inputs of different length");
+    }
+    
+    Result::Ok(a.chars().zip(b.chars()).filter(|&(a, b)| a != b).count())
 }
