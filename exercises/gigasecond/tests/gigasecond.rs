ecf88660b9d47d220a846553d88e104b6f9b9d86
Ian Whitney <whit0694@umn.edu>
2016-03-05 19:56:26 -0600

    Implement Gigasecond exercise
    Adding the [Gigasecond
    exercise](http://x.exercism.io/problems/gigasecond) for the Rust track.
    
    This exercise is a little odd in Rust as the core language has no library for
    dealing with time. There was the Time crate, but that was pulled from
    the standard library. Based on [this
    RFC](https://github.com/rust-lang/rfcs/issues/619) from last year, the
    Chrono crate appears to be the best current library for dealing with
    time. I've used Chrono here.
    
    Either because I'm still new to Rust, or because of an actual limitation
    in Chrono, the best way I could write the tests was to have everything
    be a UTC DateTime. This means that the tests that only use dates still
    require the slightly weird `.and_hms(0,0,0)` function at the end to make
    them DateTime-y.
    
    I did this because Chrono only seems to support Duration addition for
    DateTimes, not Dates.
    
    I'm following the Ruby track example and putting this exercise after
    Hamming.

---
+++ exercises/gigasecond/tests/gigasecond.rs
@@ -0,0 +1,39 @@
+extern crate gigasecond;
+
+//Note: Your code must also start with the following two lines.
+extern crate chrono;
+use chrono::*;
+
+#[test]
+fn test_date() {
+    let start_date = UTC.ymd(2011, 4, 25).and_hms(0,0,0);
+    assert_eq!(gigasecond::after(start_date), UTC.ymd(2043, 1, 1).and_hms(1,46,40));
+}
+
+#[test]
+#[ignore]
+fn test_another_date() {
+    let start_date = UTC.ymd(1977, 6, 13).and_hms(0,0,0);
+    assert_eq!(gigasecond::after(start_date), UTC.ymd(2009, 2, 19).and_hms(1,46,40));
+}
+
+#[test]
+#[ignore]
+fn test_third_date() {
+    let start_date = UTC.ymd(1959, 7, 19).and_hms(0,0,0);
+    assert_eq!(gigasecond::after(start_date), UTC.ymd(1991, 3, 27).and_hms(1,46,40));
+}
+
+#[test]
+#[ignore]
+fn test_datetime() {
+    let start_date = UTC.ymd(2015, 1, 24).and_hms(22,0,0);
+    assert_eq!(gigasecond::after(start_date), UTC.ymd(2046, 10, 2).and_hms(23,46,40));
+}
+
+#[test]
+#[ignore]
+fn test_another_datetime() {
+    let start_date = UTC.ymd(2015, 1, 24).and_hms(23,59,59);
+    assert_eq!(gigasecond::after(start_date), UTC.ymd(2046, 10, 3).and_hms(1,46,39));
+}
