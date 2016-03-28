b452b25fc3ec79c0e574927369c2adef63f2fafa
Ian Whitney <whit0694@umn.edu>
2016-03-28 13:58:25 -0600

    Move Result return to Queen::new
    Prompted by the discussion with @petertseng here:
    https://github.com/exercism/xrust/pull/89#issuecomment-202289566
    
    The standard tests show that Queen creation should throw an error, not
    the `can_attack` method. To follow that, I've switched `Queen::new` to
    return a `Result`.
    
    This has required a change to the tests, as each `Queen::new` now has to
    be unwrapped. I've reordered the tests to make that less weird
    (hopefully). The first test now exercises the creation of a Queen with a
    valid position, using `is_ok()` which should points students directly at
    the Result return type.
    
    We then test the invalid queen creation. Then we can move on to
    `can_attack`.
    
    And I've updated the Example implementation to match this new API, while
    also polishing it a bit.

--- exercises/queen-attack/example.rs
+++ exercises/queen-attack/example.rs
@@ -1,33 +1,71 @@
 #[derive(Debug, PartialEq)]
-pub struct Queen { position: (i8, i8) }
+pub struct Queen { position: ChessPosition }
 
 impl Queen {
-    pub fn new(position: (i8, i8)) -> Queen {
-        Queen { position: position }
-    }
-
-    pub fn can_attack(&self, piece: Queen) -> Result<bool, ()> {
-        if self.position_valid() && piece.position_valid() {
-            Ok(self.rank() == piece.rank() ||
-               self.file() == piece.file() ||
-               self.sum() == piece.sum() ||
-               self.difference() == piece.difference())
+    pub fn new(position: (i8, i8)) -> Result<Queen, ()> {
+        let position = ChessPosition::new(position);
+        if position.valid() {
+            Ok(Queen { position: position })
         } else {
             Err(())
         }
     }
 
-    fn position_valid(&self) -> bool {
+    pub fn can_attack(&self, piece: Queen) -> bool {
+        self.horizontal_from(&piece) ||
+            self.vertical_from(&piece) ||
+            self.diagonal_from(&piece)
+    }
+
+    fn horizontal_from(&self, piece: &Queen) -> bool {
+        self.rank() == piece.rank()
+    }
+
+    fn vertical_from(&self, piece: &Queen) -> bool {
+        self.file() == piece.file()
+    }
+
+    fn diagonal_from(&self, piece: &Queen) -> bool {
+        self.sum() == piece.sum() ||
+            self.difference() == piece.difference()
+    }
+
+    fn rank(&self) -> i8 {
+        self.position.rank()
+    }
+
+    fn file(&self) -> i8 {
+        self.position.file()
+    }
+
+    fn sum(&self) -> i8 {
+        self.position.sum()
+    }
+
+    fn difference(&self) -> i8 {
+        self.position.difference()
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct ChessPosition { coordinates: (i8, i8) }
+
+impl ChessPosition {
+    fn new(coordinates: (i8, i8)) -> ChessPosition {
+        ChessPosition {coordinates: coordinates}
+    }
+
+    fn valid(&self) -> bool {
         (self.rank() >= 0 && self.rank() <= 7) &&
         (self.file() >= 0 && self.file() <= 7)
     }
 
     fn rank(&self) -> i8 {
-        self.position.0
+        self.coordinates.0
     }
 
     fn file(&self) -> i8 {
-        self.position.1
+        self.coordinates.1
     }
 
     fn sum(&self) -> i8 {
--- exercises/queen-attack/tests/queen-attack.rs
+++ exercises/queen-attack/tests/queen-attack.rs
@@ -3,76 +3,79 @@ extern crate queen_attack;
 use queen_attack::*;
 
 #[test]
-fn test_can_not_attack() {
+fn test_queen_creation_with_valid_position() {
     let white_queen = Queen::new((2,4));
-    let black_queen = Queen::new((6,6));
-    assert_eq!(false, white_queen.can_attack(black_queen).unwrap());
+    assert!(white_queen.is_ok());
+}
+
+#[test]
+#[ignore]
+fn test_queen_creation_with_incorrect_positions() {
+    let white_queen = Queen::new((-1,2));
+    assert!(white_queen.is_err());
+
+    let white_queen = Queen::new((8,2));
+    assert!(white_queen.is_err());
+
+    let white_queen = Queen::new((5,-1));
+    assert!(white_queen.is_err());
+
+    let white_queen = Queen::new((5,8));
+    assert!(white_queen.is_err());
+}
+
+#[test]
+#[ignore]
+fn test_can_not_attack() {
+    let white_queen = Queen::new((2,4)).unwrap();
+    let black_queen = Queen::new((6,6)).unwrap();
+    assert_eq!(false, white_queen.can_attack(black_queen));
 }
 
 #[test]
 #[ignore]
 fn test_can_attack_on_same_rank() {
-    let white_queen = Queen::new((2,4));
-    let black_queen = Queen::new((2,6));
-    assert!(white_queen.can_attack(black_queen).unwrap());
+    let white_queen = Queen::new((2,4)).unwrap();
+    let black_queen = Queen::new((2,6)).unwrap();
+    assert!(white_queen.can_attack(black_queen));
 }
 
 #[test]
 #[ignore]
 fn test_can_attack_on_same_file() {
-    let white_queen = Queen::new((4,5));
-    let black_queen = Queen::new((3,5));
-    assert!(white_queen.can_attack(black_queen).unwrap());
+    let white_queen = Queen::new((4,5)).unwrap();
+    let black_queen = Queen::new((3,5)).unwrap();
+    assert!(white_queen.can_attack(black_queen));
 }
 
 #[test]
 #[ignore]
 fn test_can_attack_on_first_diagonal() {
-    let white_queen = Queen::new((2,2));
-    let black_queen = Queen::new((0,4));
-    assert!(white_queen.can_attack(black_queen).unwrap());
+    let white_queen = Queen::new((2,2)).unwrap();
+    let black_queen = Queen::new((0,4)).unwrap();
+    assert!(white_queen.can_attack(black_queen));
 }
 
 #[test]
 #[ignore]
 fn test_can_attack_on_second_diagonal() {
-    let white_queen = Queen::new((2,2));
-    let black_queen = Queen::new((3,1));
-    assert!(white_queen.can_attack(black_queen).unwrap());
+    let white_queen = Queen::new((2,2)).unwrap();
+    let black_queen = Queen::new((3,1)).unwrap();
+    assert!(white_queen.can_attack(black_queen));
 }
 
 #[test]
 #[ignore]
 fn test_can_attack_on_third_diagonal() {
-    let white_queen = Queen::new((2,2));
-    let black_queen = Queen::new((1,1));
-    assert!(white_queen.can_attack(black_queen).unwrap());
+    let white_queen = Queen::new((2,2)).unwrap();
+    let black_queen = Queen::new((1,1)).unwrap();
+    assert!(white_queen.can_attack(black_queen));
 }
 
 #[test]
 #[ignore]
 fn test_can_attack_on_fourth_diagonal() {
-    let white_queen = Queen::new((2,2));
-    let black_queen = Queen::new((5,5));
-    assert!(white_queen.can_attack(black_queen).unwrap());
-}
-
-#[test]
-#[ignore]
-fn test_queen_with_invalid_position() {
-    let white_queen = Queen::new((-1,2));
-    let black_queen = Queen::new((5,5));
-    assert!(white_queen.can_attack(black_queen).is_err());
-
-    let white_queen = Queen::new((8,2));
-    let black_queen = Queen::new((5,5));
-    assert!(white_queen.can_attack(black_queen).is_err());
-
-    let white_queen = Queen::new((2,2));
-    let black_queen = Queen::new((5,-1));
-    assert!(white_queen.can_attack(black_queen).is_err());
-
-    let white_queen = Queen::new((2,2));
-    let black_queen = Queen::new((5,8));
-    assert!(white_queen.can_attack(black_queen).is_err());
+    let white_queen = Queen::new((2,2)).unwrap();
+    let black_queen = Queen::new((5,5)).unwrap();
+    assert!(white_queen.can_attack(black_queen));
 }
