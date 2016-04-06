08e9023318d28daad5a7b55537b208858edf545f
Ian Whitney <whit0694@umn.edu>
2016-04-06 14:47:08 -0500

    Implement Sieve exercise
    Tests are from the sieve.json file in xcommon
    
    https://github.com/exercism/x-common/blob/master/sieve.json
    
    Though I have re-ordered them to follow a path I thought better. Start
    with no primes, then work up to the simplest results (1 and 2), then get
    more complex.
    
    In the config.json I've chosen to put this exercise before Allergies. I
    picked this spot for two reasons:
    
    1. It has some interesting iterator usage, which I thought step up after
    doing iterator work in Anagrams, and the folding in Difference of
    Squares.
    2. I think Allergies comes to early. Or, maybe it just came too early
    for me, as I found it baffling.
    
    But I'm flexible on placement. Looking at the other tracks it doesn't
    seem to have a consistent spot.
    
    The `Sieve::primes_up_to()` was chosen after I looked at the
    [Primal](https://crates.io/crates/primal) crate, which seems to be the
    top crate to use for primes. While it does have `Sieve::new(limit)`,
    most of its functions don't use the `new` function, so I've left it off
    here.

---
+++ exercises/sieve/example.rs
@@ -0,0 +1,14 @@
+pub struct Sieve;
+
+impl Sieve {
+    pub fn primes_up_to(limit: i32) -> Vec<i32> {
+        let mut integers = (1..limit).map(|x| x + 1).collect::<Vec<i32>>();
+        let mut p = Some(2);
+
+        while let Some(y) = p {
+            integers.retain(|&x| (x == y) || (x % y != 0));
+            p = integers.clone().into_iter().find(|x| *x > y);
+        }
+        integers
+    }
+}
