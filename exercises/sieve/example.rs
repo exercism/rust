1772fb21f18ccf0f627b7988b8079fab6092cc6a
Ian Whitney <whit0694@umn.edu>
2016-04-15 12:54:46 -0500

    Remove Sieve struct from API
    Fixes #102

--- exercises/sieve/example.rs
+++ exercises/sieve/example.rs
@@ -1,14 +1,10 @@
-pub struct Sieve;
+pub fn primes_up_to(limit: i32) -> Vec<i32> {
+    let mut integers = (1..limit).map(|x| x + 1).collect::<Vec<i32>>();
+    let mut p = Some(2);
 
-impl Sieve {
-    pub fn primes_up_to(limit: i32) -> Vec<i32> {
-        let mut integers = (1..limit).map(|x| x + 1).collect::<Vec<i32>>();
-        let mut p = Some(2);
-
-        while let Some(y) = p {
-            integers.retain(|&x| (x == y) || (x % y != 0));
-            p = integers.clone().into_iter().find(|x| *x > y);
-        }
-        integers
+    while let Some(y) = p {
+        integers.retain(|&x| (x == y) || (x % y != 0));
+        p = integers.clone().into_iter().find(|x| *x > y);
     }
+    integers
 }
