b3ba06d4f9759c261eec6309dac76c0582b32738
Ian Whitney <whit0694@umn.edu>
2016-04-15 12:50:08 -0500

    Update example to use Display
    As discussed here:
    https://github.com/exercism/xrust/pull/103#issuecomment-209962970
    
    I also ran the example file through rustfmt, so there's a bit of line
    noise in this commit.

--- exercises/roman-numerals/example.rs
+++ exercises/roman-numerals/example.rs
@@ -1,21 +1,21 @@
-static ROMAN_MAP: [(usize, &'static str); 13] = [
-    (1, "I"),
-    (4, "IV"),
-    (5, "V"),
-    (9, "IX"),
-    (10, "X"),
-    (40, "XL"),
-    (50, "L"),
-    (90, "XC"),
-    (100, "C"),
-    (400, "CD"),
-    (500, "D"),
-    (900, "CM"),
-    (1000, "M"),
-];
+use std::fmt;
+
+static ROMAN_MAP: [(usize, &'static str); 13] = [(1, "I"),
+                                                 (4, "IV"),
+                                                 (5, "V"),
+                                                 (9, "IX"),
+                                                 (10, "X"),
+                                                 (40, "XL"),
+                                                 (50, "L"),
+                                                 (90, "XC"),
+                                                 (100, "C"),
+                                                 (400, "CD"),
+                                                 (500, "D"),
+                                                 (900, "CM"),
+                                                 (1000, "M")];
 
 pub struct Roman {
-    num: usize
+    num: usize,
 }
 
 impl From<usize> for Roman {
@@ -24,8 +24,8 @@ impl From<usize> for Roman {
     }
 }
 
-impl ToString for Roman {
-    fn to_string(&self) -> String {
+impl fmt::Display for Roman {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut start = self.num.clone();
         let mut result = String::new();
         for &(numeric, roman_string) in ROMAN_MAP.into_iter().rev() {
@@ -34,14 +34,13 @@ impl ToString for Roman {
                 start = start - numeric;
             }
         }
-        result
+        write!(f, "{}", result)
     }
 }
 
+
 impl Roman {
     fn new(num: usize) -> Roman {
-        Roman {
-            num: num,
-        }
+        Roman { num: num }
     }
 }
