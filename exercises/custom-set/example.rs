ad99a31469fd83948598a37d41f62c3f0f2effec
Ian Whitney <whit0694@umn.edu>
2016-04-19 20:16:13 -0500

    Update Custom Set tests to follow standard, update API
    I'm addressing two things in this commit.
    
    Bringing Tests in line with the Standard
    ====
    
    Our suite of tests for Custom Set did not follow the [standard
    json file in
    x-common](https://github.com/exercism/x-common/blob/d6cd42db16fce5090bb486821a36d0fb1bfb7df2/custom-set.json)
    They tests have different function names, don't cover
    symmetric_difference and introduced some functions (`is_superset`) that aren't in the
    standard test file.
    
    I have not implemented the entire standard test suite as it covers sets
    of [mixed
    types](https://github.com/exercism/x-common/blob/d6cd42db16fce5090bb486821a36d0fb1bfb7df2/custom-set.json#L475)
    which I don't think is even possible in Rust. Vectors have to be
    homogeneous. Tuples can mix types, but can't change size. Mixed type
    sets may be possible, but implementing that seems way beyond the scope
    of this exercise.
    
    I think [this suite of tests is too
    long](https://github.com/exercism/discussions/issues/10). But until I
    get a PR in to shorten the standard test suite, I figured I might as
    well follow it.
    
    Updating the API
    ====
    
    I had a couple of problems with the current approach. First, the tests
    hide the actual API in a helper method. This was probably done to reduce
    duplication, but I find that it obscures the function under test. So I
    removed those.
    
    The tests also pushed towards a bunch of implementation that I found
    unnecessary, specifically the whole [Iter
    implementation](https://github.com/exercism/xrust/blob/04b665e989b578c4e46ff100b1b0a00d5df9bb0d/exercises/custom-set/example.rs#L118).
    There might be ways to get the current test suite passing without
    implementing Iter, but I got pretty stuck. My new implementation does
    not require this implementation, since equality comparisons are now
    handled by via PartialEq.

--- exercises/custom-set/example.rs
+++ exercises/custom-set/example.rs
@@ -1,149 +1,84 @@
-use std::iter::{IntoIterator, FromIterator};
-use std::slice;
-use std::option::Option;
-use std::vec::Vec;
-
 #[derive(Debug)]
 pub struct CustomSet<T> {
-    elements: Vec<T>
+    collection: Vec<T>,
 }
 
-/// Create an empty set.
-impl <T> CustomSet<T> where T: Eq + Clone {
-    pub fn new() -> CustomSet<T> {
-        CustomSet { elements: Vec::new() }
+impl<T: Ord + Clone> PartialEq for CustomSet<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.collection.iter().all(|x| other.contains(&x)) &&
+        other.collection.iter().all(|x| self.contains(&x))
+    }
+}
+
+impl<T: Ord + Clone> CustomSet<T> {
+    pub fn new(inputs: Vec<T>) -> CustomSet<T> {
+        let mut s = CustomSet { collection: Vec::new() };
+        for input in inputs {
+            s.add(input);
+        }
+        s
     }
 
-    pub fn len(&self) -> usize {
-        self.elements.len()
+    pub fn add(&mut self, element: T) {
+        if !self.contains(&element) {
+            self.collection.push(element)
+        }
+    }
+
+    pub fn delete(&mut self, element: &T) {
+        self.collection.retain(|x| x != element)
+    }
+
+    pub fn contains(&self, other: &T) -> bool {
+        self.collection.contains(other)
     }
 
     pub fn is_empty(&self) -> bool {
-        self.len() == 0
+        self.collection.is_empty()
     }
 
-    pub fn contains(&self, needle: &T) -> bool {
-        for element in self.elements.iter() {
-            if needle == element {
-                return true;
-            }
-        }
-        false
+    pub fn size(&self) -> usize {
+        self.collection.len()
     }
 
-    pub fn insert(&mut self, element: T) -> bool {
-        if !self.contains(&element) {
-            self.elements.push(element);
-            return true;
-        }
-        false
+    pub fn is_subset(&self, other: &Self) -> bool {
+        self.collection.iter().all(|x| other.contains(x))
     }
 
-    pub fn remove(&mut self, needle: &T) -> bool {
-        let mut index: Option<usize> = None;
-        for (i, element) in self.iter().enumerate() {
-            if needle == element {
-                index = Some(i);
-                break;
-            }
-        }
-        match index {
-            Some(index) => {
-                self.elements.remove(index);
-                true
-            }
-            None => false
-        }
-    }
-    
-    pub fn difference(&self, other: &CustomSet<T>) -> CustomSet<T> {
-        let mut difference: CustomSet<T> = CustomSet::new();
-        for element in self.elements.iter() {
-            if !other.contains(element) {
-                difference.insert(element.clone());
-            }
-        }
-        difference
+    pub fn is_disjoint(&self, other: &Self) -> bool {
+        !self.collection.iter().any(|x| other.contains(x))
     }
 
-    pub fn intersection(&self, other: &CustomSet<T>) -> CustomSet<T> {
-        let mut intersection: CustomSet<T> = CustomSet::new();
-        for element in self.elements.iter() {
-            if other.contains(element) {
-                intersection.insert(element.clone());
-            }
-        }
-        intersection
+    pub fn intersection(&self, other: &Self) -> CustomSet<T> {
+        CustomSet::new(self.collection
+                           .iter()
+                           .cloned()
+                           .filter(|c| other.contains(c))
+                           .collect())
     }
 
-    pub fn union(&self, other: &CustomSet<T>) -> CustomSet<T> {
-        let mut union = CustomSet::new();
-        for element in self.elements.iter().chain(other.elements.iter()) {
-            union.insert(element.clone());
-        }
-        union
+    pub fn union(&self, other: &Self) -> CustomSet<T> {
+        CustomSet::new(self.collection
+                           .iter()
+                           .cloned()
+                           .chain(other.collection.iter().cloned())
+                           .collect())
     }
 
-    pub fn is_disjoint(&self, other: &CustomSet<T>) -> bool {
-        for element in self.elements.iter() {
-            if other.contains(element) {
-                return false;
-            }
-        }
-        true
+    pub fn difference(&self, other: &Self) -> CustomSet<T> {
+        CustomSet::new(self.collection
+                           .iter()
+                           .cloned()
+                           .filter(|c| !other.contains(c))
+                           .collect())
     }
 
-    pub fn is_subset(&self, other: &CustomSet<T>) -> bool {
-        for element in self.elements.iter() {
-            if !other.contains(element) {
-                return false;
-            }
-        }
-        true
-    }
-    
-    pub fn is_superset(&self, other: &CustomSet<T>) -> bool {
-        other.is_subset(self)
-    }
-
-    pub fn clear(&mut self) {
-        self.elements.clear();
-    }
-
-    pub fn iter(&self) -> Iter<T>{
-        Iter { iter: self.elements.iter() }
-    }
-}
-
-pub struct Iter<'a, T: 'a> {
-    iter: slice::Iter<'a, T>
-}
-
-impl<'a, T> IntoIterator for &'a CustomSet<T> where T: Eq + Clone {
-    type Item = &'a T;
-    type IntoIter = Iter<'a, T>;
-
-    fn into_iter(self) -> Iter<'a, T> {
-        self.iter()
-    }
-}
-
-impl<'a, T> Iterator for Iter<'a, T> {
-    type Item = &'a T;
-
-    fn next(&mut self) -> Option<&'a T> {
-        self.iter.next()
-    }
-
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.iter.size_hint()
-    }
-}
-
-impl<T> FromIterator<T> for CustomSet<T> where T: Eq + Clone {
-    fn from_iter<I: IntoIterator<Item=T>>(iterable: I) -> CustomSet<T> {
-        let mut set = CustomSet::new();
-        set.elements = Vec::from_iter(iterable);
-        set
+    pub fn symmetric_difference(&self, other: &Self) -> CustomSet<T> {
+        CustomSet::new(self.difference(other)
+                           .collection
+                           .iter()
+                           .cloned()
+                           .chain(other.difference(self).collection.iter().cloned())
+                           .collect())
     }
 }
