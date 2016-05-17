a5ac22d4686fede69e5be51a71351139893913b6
Ian Whitney <whit0694@umn.edu>
2016-05-17 12:43:48 -0500

    Add Scrabble Score
    Implements the Scrabble Score exercise.
    
    As of right now there is no official test suite for Scrabble Score.
    There is a PR (https://github.com/exercism/x-common/pull/161), but it
    is not yet merged.
    
    I've put this exercise between the Nucleotides and Roman Numerals,
    figuring that it did similar character iteration and value mapping.
    
    I am following the test suite in that PR, with one exception. I thought
    it was likely that a common naive implementation would be to map through
    the chars, find the score and then simply unwrap the value. Like this
    
    ```
    word.chars().map(|c| values.get(&c).fold(0, |sum, v| sum + v.unwrap())
    ```
    
    So I have added a test that uses a non-English character that will cause this
    implementation to panic.
    
    My example implementation probably leaves a lot to be desired. I wanted
    to make the values hash a static, but you can't do that with a hash.
    ([Lazy
    Static](http://rust-lang-nursery.github.io/lazy-static.rs/lazy_static/index.html)
    does let you do this, but I'm not going to require a crate in my
    example).

---
+++ exercises/scrabble-score/example.rs
@@ -0,0 +1,40 @@
+use std::collections::HashMap;
+
+pub fn score(word: &str) -> u16 {
+    let values = dictionary();
+    word.to_lowercase()
+        .chars()
+        .map(|c| values.get(&c).clone())
+        .fold(0, |score, v| score + v.unwrap_or(&0))
+}
+
+fn dictionary() -> HashMap<char, u16> {
+    let mut values = HashMap::new();
+    values.insert('a', 1);
+    values.insert('b', 3);
+    values.insert('c', 3);
+    values.insert('d', 2);
+    values.insert('e', 1);
+    values.insert('f', 4);
+    values.insert('g', 2);
+    values.insert('h', 4);
+    values.insert('i', 1);
+    values.insert('j', 8);
+    values.insert('k', 5);
+    values.insert('l', 1);
+    values.insert('m', 3);
+    values.insert('n', 1);
+    values.insert('o', 1);
+    values.insert('p', 3);
+    values.insert('q', 10);
+    values.insert('r', 1);
+    values.insert('s', 1);
+    values.insert('t', 1);
+    values.insert('u', 1);
+    values.insert('v', 4);
+    values.insert('w', 4);
+    values.insert('x', 8);
+    values.insert('y', 4);
+    values.insert('z', 10);
+    values
+}
