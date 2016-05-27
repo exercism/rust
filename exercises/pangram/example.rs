adc92c53b5b30d446065ec1b69fa245f9c6033a9
Ian Whitney <whit0694@umn.edu>
2016-05-27 13:49:27 -0500

    Implement pangram
    Implements the Pangram problem and follows the current canonical test
    suite.
    
    I'm putting the problem after scrabble score. I'm guessing that
    Most implementations will use iter -> filter -> collect (though students
    constantly surprise me with their great ideas). The track places
    these iterator -> Higher Order Function problems around scrabble score &
    hamming.

---
+++ exercises/pangram/example.rs
@@ -0,0 +1,18 @@
+use std::collections::BTreeSet;
+use std::ascii::AsciiExt;
+use std::iter::FromIterator;
+
+pub fn is_pangram(sentence: &str) -> bool {
+    sentence.to_lowercase()
+            .chars()
+            .filter(|c| c.is_alphabetic())
+            .filter(|c| c.is_ascii())
+            .collect::<BTreeSet<char>>() == english_letter_set()
+
+}
+
+fn english_letter_set() -> BTreeSet<char> {
+    BTreeSet::from_iter(ENGLISH_ALPHABET.chars())
+}
+
+const ENGLISH_ALPHABET: &'static str = "abcdefghijklmnopqrstuvwxyz";
