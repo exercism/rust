540d14596ccba0b5e4c41e388bafa549018c180f
Tara <tbvanc@gmail.com>
2016-05-30 13:26:13 -0500

    Check that result contains expected allergens
    Fixes #130
    
    Testing for all allergies would fail if the allergens weren't in the
    same order as the test vector.
    
    This checks that the result contains all the expected allergens rather
    than expecting the result and test vector to be equal.

--- exercises/allergies/tests/allergies.rs
+++ exercises/allergies/tests/allergies.rs
@@ -40,10 +40,14 @@ fn test_just_to_peanuts() {
 #[test]
 #[ignore]
 fn test_allergic_to_everything() {
-    assert_eq!(vec![Allergen::Eggs, Allergen::Peanuts, Allergen::Shellfish,
-                    Allergen::Strawberries, Allergen::Tomatoes, Allergen::Chocolate,
-                    Allergen::Pollen, Allergen::Cats],
-        Allergies::new(255).allergies());
+    let all = vec![Allergen::Eggs, Allergen::Peanuts, Allergen::Shellfish,
+                   Allergen::Strawberries, Allergen::Tomatoes,
+                   Allergen::Chocolate, Allergen::Pollen, Allergen::Cats];
+    let allergies = Allergies::new(255).allergies();
+
+    for allergy in &all {
+        assert!(allergies.contains(&allergy));
+    }
 }
 
 #[test]
