9129c83803c384f0653486925ca8d55439069fc9
Ian Whitney <whit0694@umn.edu>
2016-06-27 10:05:30 -0500

    Implement Robot Simulator
    Tests come from
    https://github.com/exercism/x-common/blob/183934754b1847612809db410a8aeb640678f188/robot-simulator.json
    
    As discussed in https://github.com/exercism/xrust/issues/117 we are
    providing a stub implementation so that ignored tests do not fail when a
    student hasn't yet implemented the functionality they exercise.
    
    A lot of excellent feedback and help provided by
    
    - jonasbb
    - petertseng
    - steveklabnik
    - Ryman
    - Dr-Emann
    
    Full details of the design discussion at
    
    https://github.com/exercism/xrust/pull/146
    
    --
    
    Some details that people may care about:
    
    ----
    
    Robots are immutable because:
    
    - Immutability is the default in Rust
    - No other problem (that I know of) really features Rust's default immutability
    - Immutability is not my natural inclination
    
    I figure if I don't expect immutability then other programmers with an
    OO-focused background probably don't expect immutability. So a problem
    that forces immutability may make me (and them) think about
    immutability. Which I think is good.
    
    Immutability brings other benefits, though they aren't exposed by the
    tests. It would be very easy to trace your Robot's path, for example.
    Re-winding to a specific point on the path would be trivial. All very
    nice, but not part of the test suite.
    
    ----
    
    The example code has the `build` function because:
    
    There's still some awkwardness around the `new` function, since it takes
    x/y and all the internals use Position.
    
    I have to preserve the `new(x, y, direction)` function signature because
    of the tests (and because the tests should follow the example of Queen
    Attack, which also passes `x` and `y` in as separate parameters.
    
    But that function becomes a pain once I'm working inside the Robot and I
    have a Position.
    
    The new `build` function creates a robot using a Position and Direction,
    so it can be easily used by all of the Robot's internals. And `new` now
    just wraps around `build`.
    
    This is kind of the exact opposite way I'd normally do this. I'd expect
    a `build` function to be the public API, do the data manipulation and
    then call a private `new` function.
    
    But if I were to do that here then all of the tests would contain:
    
    ```
    Robot::build(x, y, &direction)
    ```
    
    Which I think would be non-idiomatic and weird to students.
    
    ----

---
+++ exercises/robot-simulator/tests/robot-simulator.rs
@@ -0,0 +1,147 @@
+extern crate robot_simulator;
+
+use robot_simulator::*;
+
+#[test]
+fn robots_are_created_with_position_and_direction() {
+    let robot = Robot::new(0, 0, Direction::North);
+    assert_eq!((0, 0), robot.position());
+    assert_eq!(&Direction::North, robot.direction());
+}
+
+#[test]
+#[ignore]
+fn positions_can_be_negative() {
+    let robot = Robot::new(-1, -1, Direction::South);
+    assert_eq!((-1, -1), robot.position());
+    assert_eq!(&Direction::South, robot.direction());
+}
+
+#[test]
+#[ignore]
+fn turning_right_does_not_change_position() {
+    let robot = Robot::new(0, 0, Direction::North).turn_right();
+    assert_eq!((0, 0), robot.position());
+}
+
+#[test]
+#[ignore]
+fn turning_right_from_north_points_the_robot_east() {
+    let robot = Robot::new(0, 0, Direction::North).turn_right();
+    assert_eq!(&Direction::East, robot.direction());
+}
+
+#[test]
+#[ignore]
+fn turning_right_from_east_points_the_robot_south() {
+    let robot = Robot::new(0, 0, Direction::East).turn_right();
+    assert_eq!(&Direction::South, robot.direction());
+}
+
+#[test]
+#[ignore]
+fn turning_right_from_south_points_the_robot_west() {
+    let robot = Robot::new(0, 0, Direction::South).turn_right();
+    assert_eq!(&Direction::West, robot.direction());
+}
+
+#[test]
+#[ignore]
+fn turning_right_from_west_points_the_robot_north() {
+    let robot = Robot::new(0, 0, Direction::West).turn_right();
+    assert_eq!(&Direction::North, robot.direction());
+}
+
+#[test]
+#[ignore]
+fn turning_left_does_not_change_position() {
+    let robot = Robot::new(0, 0, Direction::North).turn_left();
+    assert_eq!((0, 0), robot.position());
+}
+
+#[test]
+#[ignore]
+fn turning_left_from_north_points_the_robot_west() {
+    let robot = Robot::new(0, 0, Direction::North).turn_left();
+    assert_eq!(&Direction::West, robot.direction());
+}
+
+#[test]
+#[ignore]
+fn turning_left_from_west_points_the_robot_south() {
+    let robot = Robot::new(0, 0, Direction::West).turn_left();
+    assert_eq!(&Direction::South, robot.direction());
+}
+
+#[test]
+#[ignore]
+fn turning_left_from_south_points_the_robot_east() {
+    let robot = Robot::new(0, 0, Direction::South).turn_left();
+    assert_eq!(&Direction::East, robot.direction());
+}
+
+#[test]
+#[ignore]
+fn turning_left_from_east_points_the_robot_north() {
+    let robot = Robot::new(0, 0, Direction::East).turn_left();
+    assert_eq!(&Direction::North, robot.direction());
+}
+
+#[test]
+#[ignore]
+fn advance_does_not_change_the_direction() {
+    let robot = Robot::new(0, 0, Direction::North).advance();
+    assert_eq!(&Direction::North, robot.direction());
+}
+
+#[test]
+#[ignore]
+fn advance_increases_the_y_coordinate_by_one_when_facing_north() {
+    let robot = Robot::new(0, 0, Direction::North).advance();
+    assert_eq!((0, 1), robot.position());
+}
+
+#[test]
+#[ignore]
+fn advance_decreases_the_y_coordinate_by_one_when_facing_south() {
+    let robot = Robot::new(0, 0, Direction::South).advance();
+    assert_eq!((0, -1), robot.position());
+}
+
+#[test]
+#[ignore]
+fn advance_increases_the_x_coordinate_by_one_when_facing_east() {
+    let robot = Robot::new(0, 0, Direction::East).advance();
+    assert_eq!((1, 0), robot.position());
+}
+
+#[test]
+#[ignore]
+fn advance_decreases_the_x_coordinate_by_one_when_facing_west() {
+    let robot = Robot::new(0, 0, Direction::West).advance();
+    assert_eq!((-1, 0), robot.position());
+}
+
+#[test]
+#[ignore]
+fn follow_instructions_to_move_west_and_north() {
+    let robot = Robot::new(0, 0, Direction::North).instructions("LAAARALA");
+    assert_eq!((-4, 1), robot.position());
+    assert_eq!(&Direction::West, robot.direction());
+}
+
+#[test]
+#[ignore]
+fn follow_instructions_to_move_west_and_south() {
+    let robot = Robot::new(2, -7, Direction::East).instructions("RRAAAAALA");
+    assert_eq!((-3, -8), robot.position());
+    assert_eq!(&Direction::South, robot.direction());
+}
+
+#[test]
+#[ignore]
+fn follow_instructions_to_move_east_and_north() {
+    let robot = Robot::new(8, 4, Direction::South).instructions("LAAARRRALLLL");
+    assert_eq!((11, 5), robot.position());
+    assert_eq!(&Direction::North, robot.direction());
+}
