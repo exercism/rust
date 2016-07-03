8ebe85d41d508b1db8c7a1b9f352ca57f837e844
Peter Tseng <pht24@cornell.edu>
2016-07-03 04:01:32 -0700

    word-count: Use `Entry#or_insert` in `word_count`
    This drastically shortens the function, avoiding the need to `match` on
    the `Entry` cases.
    This seems good to make the example more idiomatic.

--- exercises/word-count/example.rs
+++ exercises/word-count/example.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::collections::hash_map::Entry;
 
 fn lowercase(word: &str) -> String {
     let mut lower = String::with_capacity(word.len());
@@ -16,12 +15,7 @@ pub fn word_count(input: &str) -> HashMap<String, u32> {
     let lower = lowercase(input);
     let slice: &str = lower.as_ref();
     for word in slice.split(|c: char| !c.is_alphanumeric()).filter(|s| !s.is_empty()) {
-        match map.entry(word.to_string()) {
-            Entry::Vacant(entry) => { entry.insert(1); },
-            Entry::Occupied(mut entry) => {
-                *entry.get_mut() += 1;
-            }
-        };
+        *map.entry(word.to_string()).or_insert(0) += 1;
     }
     map
 }
