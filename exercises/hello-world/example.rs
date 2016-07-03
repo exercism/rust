b1a2716649516e29ee8e359f66a7e01461f31b3e
Peter Tseng <pht24@cornell.edu>
2016-07-03 13:21:28 -0700

    hello-world: No need to use `ref` on a `&str`
    For an explanation of what the `ref` was doing:
    
    * https://doc.rust-lang.org/book/patterns.html#ref-and-ref-mut
    * https://play.rust-lang.org/?gist=53e77f95fdf28bd221b731efcc8f1f9d&version=stable
    
    From these two we can see that if we take `ref`, the type of `n` would
    be `&&str` and if we don't  it will be `&str`. It doesn't seem necessary
    to take a ref of a ref, so I recommend it be removed.
    
    Taking `ref` would matter if `n` were of a type that was not `Copy` and
    we wished to use it afterward. If both of those conditions were true, if
    we failed to use `ref` we would be unable to use `n` afterward since it
    was moved by not taking `ref`.

--- exercises/hello-world/example.rs
+++ exercises/hello-world/example.rs
@@ -1,6 +1,6 @@
 pub fn hello(name: Option<&str>) -> String {
     match name {
-        Some(ref n) => format!("Hello, {}!", n),
+        Some(n) => format!("Hello, {}!", n),
         None => "Hello, World!".to_string(),
     }
 }
