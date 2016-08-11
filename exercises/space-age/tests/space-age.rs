4dc0b58a63d77381c238ea84ecbf616ebd70bfd8
Ian Whitney <whit0694@umn.edu>
2016-08-10 21:43:16 -0500

    Implement Space Age
    This implementation follows the common test suite.
    
    We designed the tests to encourage (force?) students to implement
    solutions using Traits, and for them to use the From trait to convert
    numbers to Durations.
    
    In a "Real World" implementation of a Planetary Calendar you might not
    implement this code in this same way. That's fine! Exercism isn't here
    to model Real Production Code. It's here to help students learn about
    language, design and tradeoffs.
    
    By focusing on Traits this problem will hopefully illustrate how Rust
    handles design -- no Inheritance, just Composition via Traits -- and
    minimizing code duplication.
    
    We've placed this problem after Queen Attack and Roman Numerals. If the
    student has gone through the problems in order then they've covered
    Traits (Roman Numerals, and maybe in Queen Attack).  Custom traits may
    be new, depending on how they handled Queen Attack.
    
    I think providing a default implementation for a trait function will be
    new, but the stub file makes it pretty clear what's going on there.
    
    I don't think it's a big leap to add a trait function that will be
    implemented differently for each implementation of Planet. But we'll see
    what students make of it.
    
    We're also trying something new, adding a topics.md file that will list
    what parts of Rust the problem & solution focus on
    
    Lengthy discussion of how this problem was implemented can be found at
    https://github.com/exercism/xrust/pull/177

---
+++ exercises/space-age/tests/space-age.rs
@@ -0,0 +1,69 @@
+extern crate space_age;
+
+use space_age::*;
+
+fn assert_in_delta(expected: f64, actual: f64) {
+    let diff: f64 = expected - actual.abs();
+    let delta: f64 = 0.01;
+    if diff > delta {
+        panic!("Your result of {} should be within {} of the expected result {}",
+               actual,
+               delta,
+               expected)
+    }
+}
+
+#[test]
+fn earth_age() {
+    let duration = Duration::from(1_000_000_000);
+    assert_in_delta(31.69, Earth::years_during(&duration));
+}
+
+#[test]
+#[ignore]
+fn mercury_age() {
+    let duration = Duration::from(2_134_835_688);
+    assert_in_delta(280.88, Mercury::years_during(&duration));
+}
+
+#[test]
+#[ignore]
+fn venus_age() {
+    let duration = Duration::from(189_839_836);
+    assert_in_delta(9.78, Venus::years_during(&duration));
+}
+
+#[test]
+#[ignore]
+fn mars_age() {
+    let duration = Duration::from(2_329_871_239);
+    assert_in_delta(39.25, Mars::years_during(&duration));
+}
+
+#[test]
+#[ignore]
+fn jupiter_age() {
+    let duration = Duration::from(901_876_382);
+    assert_in_delta(2.41, Jupiter::years_during(&duration));
+}
+
+#[test]
+#[ignore]
+fn saturn_age() {
+    let duration = Duration::from(3_000_000_000);
+    assert_in_delta(3.23, Saturn::years_during(&duration));
+}
+
+#[test]
+#[ignore]
+fn uranus_age() {
+    let duration = Duration::from(3_210_123_456);
+    assert_in_delta(1.21, Uranus::years_during(&duration));
+}
+
+#[test]
+#[ignore]
+fn neptune_age() {
+    let duration = Duration::from(8_210_123_456);
+    assert_in_delta(1.58, Neptune::years_during(&duration));
+}
---
+++ exercises/space-age/topics.md
@@ -0,0 +1,6 @@
+# Topics
+
+Some Rust topics you may want to read about while solving this problem:
+
+- Traits, both the From trait and implementing your own traits
+- Default method implementations for traits
