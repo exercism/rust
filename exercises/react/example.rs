ccda60d2eb9c6cd5ae73dcb78d992c7e57c59301
Peter Tseng <pht24@cornell.edu>
2016-08-31 06:39:33 -0700

    Revert "let's have cells and epochs and the cell caches a value...!"
    This reverts commit 88f2463cd1edc36db97fe7c3ceadc7ffb038c479.

--- exercises/react/example.rs
+++ exercises/react/example.rs
@@ -1,26 +1,20 @@
 // TODO: For now, lib is symlinked to example to ease local development.
 // But the final plan is to provide a stub file once we know what the interface will be.
 
-
-use std::cell::Cell as MutCell;
-
 pub trait Cell<T> {
     fn value(&self) -> &T;
-    fn epoch(&self) -> usize;
 }
 
 pub struct Reactor;
 
 pub struct InputCell<T> {
     val: T,
-    epoch: usize,
 }
 
-pub struct Compute1Cell<'a, T: 'a, U: Copy, F: Fn(&T) -> U> {
+pub struct Compute1Cell<'a, T: 'a, U, F: Fn(&T) -> U> {
     compute: F,
     cell: &'a Cell<T>,
-    epoch: MutCell<usize>,
-    val: MutCell<U>,
+    val: U,
 }
 
 impl Reactor {
@@ -31,15 +25,14 @@ impl Reactor {
     pub fn create_input<T>(&self, initial: T) -> InputCell<T> {
         InputCell {
             val: initial,
-            epoch: 0,
         }
     }
 
-    pub fn create_compute1<'a, T, U: Copy, F: Fn(&T) -> U>(&self, cell: &'a Cell<T>, compute: F) -> Compute1Cell<'a, T, U, F> {
+    pub fn create_compute1<'a, T, U, F>(&self, cell: &'a Cell<T>, compute: F) -> Compute1Cell<'a, T, U, F>
+        where F: Fn(&T) -> U {
         Compute1Cell {
-            val: MutCell::new(compute(cell.value())),
+            val: compute(cell.value()),
             cell: cell,
-            epoch: MutCell::new(cell.epoch()),
             compute: compute,
         }
     }
@@ -49,10 +42,6 @@ impl <T> Cell<T> for InputCell<T> {
     fn value(&self) -> &T {
         &self.val
     }
-
-    fn epoch(&self) -> usize {
-        self.epoch
-    }
 }
 
 impl <T> InputCell<T> {
@@ -61,16 +50,8 @@ impl <T> InputCell<T> {
     }
 }
 
-impl <'a, T, U: Copy, F: Fn(&T) -> U> Cell<U> for Compute1Cell<'a, T, U, F> {
+impl <'a, T, U, F: Fn(&T) -> U> Cell<U> for Compute1Cell<'a, T, U, F> {
     fn value(&self) -> &U {
-        if self.epoch() < self.cell.epoch() {
-            self.epoch.set(self.cell.epoch());
-            self.val.set((self.compute)(self.cell.value()));
-        }
-        &self.val.get().clone()
-    }
-
-    fn epoch(&self) -> usize {
-        self.epoch.get()
+        &self.val
     }
 }
