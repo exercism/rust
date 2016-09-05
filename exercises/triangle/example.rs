3e280892cb252e7fc3ea21b4b918dac3999152c2
Ian Whitney <whit0694@umn.edu>
2016-09-04 20:23:37 -0500

    Implement Triangle
    Test suite mostly follows the standard. I've started off with a couple
    of Result-checking tests, just to establish that we expect a Result
    back.
    
    Floating-point tests are included as optional, and an example that
    passes those tests is included. Making a function generic over Ints &
    Floats is tricky, and we decided to not make it a requirement.
    
    Hints file points to some optional ways of implementing a solution. A
    single `Triangle` struct gets the test passing fastest, but other
    designs can be explored.
    
    Since the single struct approach will be accessible to students by the
    time they reach rna-transcription, we've put it there. If we added tests
    that forced an Enum/Trait/Etc. impl, then we'd move it. But tests like
    that seemed too proscriptive.
    
    Full discussion of the implementation of this exercise is at
    https://github.com/exercism/xrust/pull/197

---
+++ exercises/triangle/example.rs
@@ -0,0 +1,38 @@
+use std::iter::FromIterator;
+use std::collections::BTreeSet;
+
+pub struct Triangle {
+    sides: [u16; 3],
+}
+
+impl Triangle {
+    #[cfg_attr(rustfmt, rustfmt_skip)]
+    fn valid_sides(&self) -> bool {
+        (self.sides.iter().all(|&s| s > 0)) &&
+            (self.sides[0] + self.sides[1] >= self.sides[2]) &&
+            (self.sides[1] + self.sides[2] >= self.sides[0]) &&
+            (self.sides[2] + self.sides[0] >= self.sides[1])
+    }
+
+    pub fn build(sides: [u16; 3]) -> Result<Self, ()> {
+        let t = Triangle { sides: sides };
+
+        if t.valid_sides() {
+            Ok(t)
+        } else {
+            Err(())
+        }
+    }
+
+    pub fn is_equilateral(&self) -> bool {
+        BTreeSet::from_iter(self.sides.iter()).len() == 1
+    }
+
+    pub fn is_isosceles(&self) -> bool {
+        BTreeSet::from_iter(self.sides.iter()).len() == 2
+    }
+
+    pub fn is_scalene(&self) -> bool {
+        BTreeSet::from_iter(self.sides.iter()).len() == 3
+    }
+}
