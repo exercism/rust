327c14387ed5d32c0bd24c3e5d7381f41ba1ceda
Greg Cline <gregrcline@gmail.com>
2016-09-14 12:23:29 -0400

    Add example and tests for Acronym

---
+++ exercises/acronym/tests/acronym.rs
@@ -0,0 +1,37 @@
+extern crate acronym;
+
+#[test]
+fn basic() {
+  assert_eq!(acronym::abbreviate("Portable Network Graphics"), "PNG");
+}
+
+#[test]
+#[ignore]
+fn lowercase_words() {
+  assert_eq!(acronym::abbreviate("Ruby on Rails"), "ROR");
+}
+
+#[test]
+#[ignore]
+fn camelcase() {
+    assert_eq!(acronym::abbreviate("HyperText Markup Language"), "HTML");
+}
+
+#[test]
+#[ignore]
+fn punctuation() {
+    assert_eq!(acronym::abbreviate("First In, First Out"), "FIFO");
+}
+
+#[test]
+#[ignore]
+fn all_caps_words() {
+    assert_eq!(acronym::abbreviate("PHP: Hypertext Preprocessor"), "PHP");
+}
+
+#[test]
+#[ignore]
+fn hyphenated() {
+    assert_eq!(acronym::abbreviate("Complementary metal-oxide semiconductor"), 
+               "CMOS");
+}
