acb00ecbb44b061640067e017e0111f21ca5638b
Greg Cline <gregrcline@gmail.com>
2016-09-17 08:59:25 -0400

    Implement Acronym (#204)
    * Add example and tests for Acronym
    * Added Acronym to config.json and problems.md

---
+++ exercises/acronym/example.rs
@@ -0,0 +1,20 @@
+pub fn abbreviate(phrase: &str) -> String {
+    phrase.split(|c: char| c.is_whitespace() || !c.is_alphanumeric())
+          .flat_map(|word| split_camel(word))
+          .filter_map(|word| word.chars().next())
+          .collect::<String>()
+          .to_uppercase()
+}
+
+fn split_camel(phrase: &str) -> Vec<String> {
+    let chars: Vec<char> = phrase.chars().collect();
+    let mut words: Vec<String> = Vec::new();
+    let mut word_start: usize = 0;
+    for (i, c) in chars.iter().enumerate() {
+        if i == chars.len() - 1 || c.is_lowercase() && chars[i + 1].is_uppercase() {
+            words.push(chars[word_start..i + 1].iter().cloned().collect());
+            word_start = i + 1;
+        }
+    }
+    words
+}
