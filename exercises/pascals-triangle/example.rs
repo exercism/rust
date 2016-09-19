8ab67453e36ea92941c852204264c125ef7da63c
Ian Whitney <ian@ianwhitney.com>
2016-09-19 09:12:09 -0500

    Implement Pascals Triangle (#201)
    Implement Pascals Triangle
    
    Follows official tests
    
    Discussion of implementation and placement at https://github.com/exercism/xrust/pull/201/files

---
+++ exercises/pascals-triangle/example.rs
@@ -0,0 +1,24 @@
+pub struct PascalsTriangle {
+    row_count: u32,
+}
+
+impl PascalsTriangle {
+    pub fn new(row_count: u32) -> Self {
+        PascalsTriangle { row_count: row_count }
+    }
+
+    pub fn rows(&self) -> Vec<Vec<u32>> {
+        (0..self.row_count).map(|row| PascalsTriangle::row(row)).collect()
+    }
+
+    pub fn row(number: u32) -> Vec<u32> {
+        let mut r = vec![1];
+
+        for p in 1..(number + 1) {
+            if let Some(&last) = r.last() {
+                r.push((last * (number + 1 - p)) / p)
+            }
+        }
+        r
+    }
+}
---
+++ exercises/pascals-triangle/tests/pascals-triangle.rs
@@ -0,0 +1,42 @@
+extern crate pascals_triangle;
+
+use pascals_triangle::*;
+
+#[test]
+fn no_rows() {
+    let pt = PascalsTriangle::new(0);
+    let expected: Vec<Vec<u32>> = Vec::new();
+    assert_eq!(expected, pt.rows());
+}
+
+#[test]
+#[ignore]
+fn one_row() {
+    let pt = PascalsTriangle::new(1);
+    let expected: Vec<Vec<u32>> = vec![vec![1]];
+    assert_eq!(expected, pt.rows());
+}
+
+#[test]
+#[ignore]
+fn two_rows() {
+    let pt = PascalsTriangle::new(2);
+    let expected: Vec<Vec<u32>> = vec![vec![1], vec![1, 1]];
+    assert_eq!(expected, pt.rows());
+}
+
+#[test]
+#[ignore]
+fn three_rows() {
+    let pt = PascalsTriangle::new(3);
+    let expected: Vec<Vec<u32>> = vec![vec![1], vec![1, 1], vec![1, 2, 1]];
+    assert_eq!(expected, pt.rows());
+}
+
+#[test]
+#[ignore]
+fn last_of_four_rows() {
+    let pt = PascalsTriangle::new(4);
+    let expected: Vec<u32> = vec![1, 3, 3, 1];
+    assert_eq!(expected, pt.rows().pop().unwrap());
+}
