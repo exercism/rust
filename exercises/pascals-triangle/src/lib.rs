dfabf85227cfeb82764f8489242571b7d462289a
Ian Whitney <whit0694@umn.edu>
2016-09-19 09:15:08 -0500

    Stub file for pascals-triangle

---
+++ exercises/pascals-triangle/src/lib.rs
@@ -0,0 +1,11 @@
+pub struct PascalsTriangle;
+
+impl PascalsTriangle {
+    pub fn new(row_count: u32) -> Self {
+        unimplemented!();
+    }
+
+    pub fn rows(&self) -> Vec<Vec<u32>> {
+        unimplemented!();
+    }
+}
