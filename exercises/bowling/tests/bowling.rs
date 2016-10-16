4644815b62ce62dda593b6f94b749c97d6775860
Ian Whitney <whit0694@umn.edu>
2016-10-16 10:40:30 -0500

    Handle results, reorder tests, fix errors
    Errors
    ----
    
    I think I caught all of the logical errors in my use of ranges. A second
    set of eyes checking that will be much appreciated
    
    Ordering
    ----
    
    This mostly follows the new canonical_data.json file. I've moved some of
    the exception tests up front so that students can quickly handle the
    broad edge cases of the problem
    
    - Rolls return results
    - Score return results
    - A single roll can't be more than 10
    - A game needs 10 frames to be OK
    
    The weirder edge cases are saved until the end as I don't think they
    need to be handled up front.
    
    Handle results
    ----
    
    Based on the conversation here
    https://github.com/exercism/xrust/pull/213#issuecomment-253384376
    
    I think a bare `is_ok` is fine for all of the rolls that we expect be
    ok. We could wrap it in `assert!`, but I think that is noisy as it
    indicates we're testing something that we're not actually testing.

--- exercises/bowling/tests/bowling.rs
+++ exercises/bowling/tests/bowling.rs
@@ -1,45 +1,91 @@
 #[test]
-fn twenty_zero_pin_rolls_is_zero() {
+fn roll_returns_a_result() {
+    let game = BowlingGame::new();
+    assert!(game.roll(0).is_ok());
+}
+
+#[test]
+fn you_can_not_roll_more_than_ten_pins_in_a_single_roll() {
+    let game = BowlingGame::new();
+
+    assert!(game.roll(11).is_err());
+}
+
+#[test]
+fn a_game_score_is_ok_if_ten_frames_have_been_rolled() {
+    let game = BowlingGame::new();
+
+    for x in (0..10) {
+        game.roll(0).is_ok();
+        game.roll(0).is_ok();
+    }
+
+    assert!(game.score().is_ok());
+}
+
+#[test]
+fn you_can_not_score_a_game_with_no_rolls() {
+    let game = BowlingGame::new();
+
+    assert!(game.score().is_err());
+}
+
+#[test]
+fn a_game_score_is_err_if_fewer_than_ten_frames_have_been_rolled() {
+    let game = BowlingGame::new();
+
+    for x in (0..9) {
+        game.roll(0).is_ok();
+        game.roll(0).is_ok();
+    }
+
+    assert!(game.score().is_err());
+}
+
+#[test]
+fn a_game_score_is_err_if_more_than_ten_frames_have_been_rolled() {
+    let game = BowlingGame::new();
+
+    for x in (0..11) {
+        game.roll(0).is_ok();
+        game.roll(0).is_ok();
+    }
+
+    assert!(game.score().is_err());
+}
+
+#[test]
+fn twenty_zero_pin_rolls_scores_zero() {
     let game = BowlingGame::new();
 
     for x in (0..20) {
-        game.roll(0);
+        game.roll(0).is_ok();
     }
 
     assert_eq!(game.score().unwrap(), 0);
 }
 
 #[test]
-fn twenty_one_pin_rolls_is_twenty() {
-    let game = BowlingGame::new();
-
-    for x in (0..20) {
-        game.roll(1);
-    }
-
-    assert_eq!(game.score().unwrap(), 20);
-}
-
-#[test]
 fn ten_frames_without_a_strike_or_spare() {
     let game = BowlingGame::new();
 
-    for x in (0..20) {
-        game.roll(4);
+    for x in (0..10) {
+        game.roll(3).is_ok();
+        game.roll(6).is_ok();
     }
 
-    assert_eq!(game.score().unwrap(), 80);
+    assert_eq!(game.score().unwrap(), 90);
 }
 
 #[test]
 fn spare_in_the_first_frame_followed_by_zeros() {
     let game = BowlingGame::new();
 
-    game.roll(4);
-    game.roll(6);
+    game.roll(6).is_ok();
+    game.roll(4).is_ok();
 
     for x in (0..18) {
-        game.roll(0);
+        game.roll(0).is_ok();
     }
 
     assert_eq!(game.score().unwrap(), 10);
@@ -49,29 +95,29 @@ fn spare_in_the_first_frame_followed_by_zeros() {
 fn points_scored_in_the_roll_after_a_spare_are_counted_twice_as_a_bonus() {
     let game = BowlingGame::new();
 
-    game.roll(5);
-    game.roll(5);
-    game.roll(3);
+    game.roll(6).is_ok();
+    game.roll(4).is_ok();
+    game.roll(3).is_ok();
 
     for x in (0..17) {
-        game.roll(0);
+        game.roll(0).is_ok();
     }
 
     assert_eq!(game.score().unwrap(), 16);
 }
 
 #[test]
-fn consecutive_spares_each_get_their_bonus_points() {
+fn consecutive_spares_each_get_a_one_roll_bonus() {
     let game = BowlingGame::new();
 
-    game.roll(5);
-    game.roll(5);
-    game.roll(3);
-    game.roll(7);
-    game.roll(4);
+    game.roll(5).is_ok();
+    game.roll(5).is_ok();
+    game.roll(3).is_ok();
+    game.roll(7).is_ok();
+    game.roll(4).is_ok();
 
     for x in (0..15) {
-        game.roll(0);
+        game.roll(0).is_ok();
     }
 
     assert_eq!(game.score().unwrap(), 31);
@@ -81,13 +127,13 @@ fn consecutive_spares_each_get_their_bonus_points() {
 fn if_the_last_frame_is_a_spare_you_get_one_extra_roll_that_is_scored_once() {
     let game = BowlingGame::new();
 
-    for x in (0..17) {
-        game.roll(0);
+    for x in (0..18) {
+        game.roll(0).is_ok();
     }
 
-    game.roll(5);
-    game.roll(5);
-    game.roll(7);
+    game.roll(5).is_ok();
+    game.roll(5).is_ok();
+    game.roll(7).is_ok();
 
     assert_eq!(game.score().unwrap(), 17);
 }
@@ -96,167 +142,175 @@ fn if_the_last_frame_is_a_spare_you_get_one_extra_roll_that_is_scored_once() {
 fn a_strike_earns_ten_points_in_a_frame_with_a_single_roll() {
     let game = BowlingGame::new();
 
-    game.roll(10);
+    game.roll(10).is_ok();
 
-    for x in (0..17) {
-        game.roll(0);
+    for x in (0..18) {
+        game.roll(0).is_ok();
     }
 
-    assert_eq!(game.score().unwrap(), 0);
+    assert_eq!(game.score().unwrap(), 10);
 }
 
 #[test]
 fn points_scored_in_the_two_rolls_after_a_strike_are_counted_twice_as_a_bonus() {
     let game = BowlingGame::new();
 
-    game.roll(10);
-    game.roll(5);
-    game.roll(3);
+    game.roll(10).is_ok();
+    game.roll(5).is_ok();
+    game.roll(3).is_ok();
 
-    for x in (0..15) {
-        game.roll(0);
+    for x in (0..16) {
+        game.roll(0).is_ok();
     }
 
     assert_eq!(game.score().unwrap(), 26);
 }
 
 #[test]
-fn points_scored_in_the_two_rolls_after_a_strike_are_counted_twice_with_consecutive_strikes() {
+fn consecutive_strikes_each_get_the_two_roll_bonus() {
     let game = BowlingGame::new();
 
-    game.roll(10);
-    game.roll(10);
-    game.roll(10);
-    game.roll(5);
-    game.roll(3);
+    game.roll(10).is_ok();
+    game.roll(10).is_ok();
+    game.roll(10).is_ok();
+    game.roll(5).is_ok();
+    game.roll(3).is_ok();
 
-    for x in (0..11) {
-        game.roll(0);
+    for x in (0..12) {
+        game.roll(0).is_ok();
     }
 
     assert_eq!(game.score().unwrap(), 81);
 }
 
 #[test]
-fn if_the_last_frame_is_a_strike_you_get_two_extra_rolls() {
+fn a_strike_in_the_last_frame_earns_a_two_roll_bonus_that_is_counted_once() {
     let game = BowlingGame::new();
 
-    for x in (0..17) {
-        game.roll(0);
+    for x in (0..18) {
+        game.roll(0).is_ok();
     }
 
-    game.roll(10);
-    game.roll(7);
-    game.roll(1);
+    game.roll(10).is_ok();
+    game.roll(7).is_ok();
+    game.roll(1).is_ok();
 
     assert_eq!(game.score().unwrap(), 18);
 }
 
 #[test]
-fn strikes_in_extra_rolls_after_a_strike_in_the_final_frame_do_not_get_the_bonus() {
+fn a_spare_with_the_two_roll_bonus_does_not_get_a_bonus_roll() {
     let game = BowlingGame::new();
 
-    for x in (0..17) {
-        game.roll(0);
+    for x in (0..18) {
+        game.roll(0).is_ok();
     }
 
-    game.roll(10);
-    game.roll(10);
-    game.roll(10);
+    game.roll(10).is_ok();
+    game.roll(7).is_ok();
+    game.roll(3).is_ok();
+
+    assert_eq!(game.score().unwrap(), 20);
+}
+
+#[test]
+fn strikes_with_the_two_roll_bonus_do_not_get_a_bonus_roll() {
+    let game = BowlingGame::new();
+
+    for x in (0..18) {
+        game.roll(0).is_ok();
+    }
+
+    game.roll(10).is_ok();
+    game.roll(10).is_ok();
+    game.roll(10).is_ok();
 
     assert_eq!(game.score().unwrap(), 30);
 }
 
+#[test]
+fn a_strike_with_the_one_roll_bonus_after_a_spare_in_the_last_fram_does_not_get_a_bonus() {
+    let game = BowlingGame::new();
+
+    for x in (0..18) {
+        game.roll(0).is_ok();
+    }
+
+    game.roll(7).is_ok();
+    game.roll(3).is_ok();
+    game.roll(10).is_ok();
+
+    assert_eq!(game.score().unwrap(), 20);
+}
+
+#[test]
 fn all_strikes_is_a_perfect_score_of_300() {
     let game = BowlingGame::new();
 
-    for x in (0..11) {
-        game.roll(10);
+    for x in (0..12) {
+        game.roll(10).is_ok();
     }
 
     assert_eq!(game.score().unwrap(), 300);
 }
 
-fn you_can_not_roll_more_than_ten_pins_in_a_single_roll() {
-    let game = BowlingGame::new();
-
-    for x in (0..19) {
-        game.roll(0);
-    }
-
-    game.roll(11);
-
-    assert!(game.score().is_err());
-}
-
+#[test]
 fn you_can_not_roll_more_than_ten_pins_in_a_single_frame() {
     let game = BowlingGame::new();
 
-    for x in (0..18) {
-        game.roll(0);
-    }
-
-    game.roll(5);
-    game.roll(6);
-
-    assert!(game.score().is_err());
+    assert!(game.roll(5).is_ok());
+    assert!(game.roll(6).is_err());
 }
 
-fn you_can_not_score_a_game_with_no_rolls() {
-    let game = BowlingGame::new();
-
-    assert!(game.score().is_err());
-}
-
-fn you_can_not_score_an_incomplete_game() {
+#[test]
+fn you_can_not_roll_more_than_ten_pins_with_the_two_bonus_rolls_after_a_final_strike() {
     let game = BowlingGame::new();
 
     for x in (0..18) {
-        game.roll(0);
+        game.roll(0).is_ok();
     }
 
-    assert!(game.score().is_err());
+    game.roll(10).is_ok();
+
+    assert!(game.roll(5).is_ok());
+    assert!(game.roll(6).is_err());
 }
 
-fn you_can_not_score_a_game_with_more_than_ten_frames() {
+#[test]
+fn if_the_last_frame_is_a_strike_you_can_not_score_before_the_extra_rolls_are_taken() {
     let game = BowlingGame::new();
 
-    for x in (0..21) {
-        game.roll(0);
+    for x in (0..18) {
+        game.roll(0).is_ok();
     }
 
+    game.roll(10).is_ok();
+
     assert!(game.score().is_err());
+
+    game.roll(10).is_ok();
+
+    assert!(game.score().is_err());
+
+    game.roll(10).is_ok();
+
+    assert!(game.score().is_ok());
 }
 
+#[test]
 fn if_the_last_frame_is_a_spare_you_can_not_create_a_score_before_extra_roll_is_taken() {
     let game = BowlingGame::new();
 
-    for x in (0..17) {
-        game.roll(0);
+    for x in (0..18) {
+        game.roll(0).is_ok();
     }
 
-    game.roll(5);
-    game.roll(5);
-
-    assert!(game.score().is_err());
-}
-
-fn if_the_last_frame_is_a_strike_you_can_not_create_a_score_before_extra_rolls_are_taken() {
-    let game = BowlingGame::new();
-
-    for x in (0..17) {
-        game.roll(0);
-    }
-
-    game.roll(10);
+    game.roll(5).is_ok();
+    game.roll(5).is_ok();
 
     assert!(game.score().is_err());
 
-    game.roll(10);
-
-    assert!(game.score().is_err());
-
-    game.roll(10);
+    game.roll(10).is_ok();
 
     assert!(game.score().is_ok());
 }
