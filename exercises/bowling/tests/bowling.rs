e3d3c207635b2d0e67038def4b4ff70393676fa2
Ian Whitney <whit0694@umn.edu>
2016-10-16 12:53:43 -0500

    Include crate, stylistic fixes
    The changes to the for loops resolve warnings thrown by the compiler.

--- exercises/bowling/tests/bowling.rs
+++ exercises/bowling/tests/bowling.rs
@@ -1,3 +1,7 @@
+extern crate bowling;
+
+use bowling::*;
+
 #[test]
 fn roll_returns_a_result() {
     let game = BowlingGame::new();
@@ -15,7 +19,7 @@ fn you_can_not_roll_more_than_ten_pins_in_a_single_roll() {
 fn a_game_score_is_ok_if_ten_frames_have_been_rolled() {
     let game = BowlingGame::new();
 
-    for x in (0..10) {
+    for _ in 0..10 {
         game.roll(0).is_ok();
         game.roll(0).is_ok();
     }
@@ -34,7 +38,7 @@ fn you_can_not_score_a_game_with_no_rolls() {
 fn a_game_score_is_err_if_fewer_than_ten_frames_have_been_rolled() {
     let game = BowlingGame::new();
 
-    for x in (0..9) {
+    for _ in 0..9 {
         game.roll(0).is_ok();
         game.roll(0).is_ok();
     }
@@ -46,7 +50,7 @@ fn a_game_score_is_err_if_fewer_than_ten_frames_have_been_rolled() {
 fn a_game_score_is_err_if_more_than_ten_frames_have_been_rolled() {
     let game = BowlingGame::new();
 
-    for x in (0..11) {
+    for _ in 0..11 {
         game.roll(0).is_ok();
         game.roll(0).is_ok();
     }
@@ -58,7 +62,7 @@ fn a_game_score_is_err_if_more_than_ten_frames_have_been_rolled() {
 fn twenty_zero_pin_rolls_scores_zero() {
     let game = BowlingGame::new();
 
-    for x in (0..20) {
+    for _ in 0..20 {
         game.roll(0).is_ok();
     }
 
@@ -69,7 +73,7 @@ fn twenty_zero_pin_rolls_scores_zero() {
 fn ten_frames_without_a_strike_or_spare() {
     let game = BowlingGame::new();
 
-    for x in (0..10) {
+    for _ in 0..10 {
         game.roll(3).is_ok();
         game.roll(6).is_ok();
     }
@@ -84,7 +88,7 @@ fn spare_in_the_first_frame_followed_by_zeros() {
     game.roll(6).is_ok();
     game.roll(4).is_ok();
 
-    for x in (0..18) {
+    for _ in 0..18 {
         game.roll(0).is_ok();
     }
 
@@ -99,7 +103,7 @@ fn points_scored_in_the_roll_after_a_spare_are_counted_twice_as_a_bonus() {
     game.roll(4).is_ok();
     game.roll(3).is_ok();
 
-    for x in (0..17) {
+    for _ in 0..17 {
         game.roll(0).is_ok();
     }
 
@@ -116,7 +120,7 @@ fn consecutive_spares_each_get_a_one_roll_bonus() {
     game.roll(7).is_ok();
     game.roll(4).is_ok();
 
-    for x in (0..15) {
+    for _ in 0..15 {
         game.roll(0).is_ok();
     }
 
@@ -127,7 +131,7 @@ fn consecutive_spares_each_get_a_one_roll_bonus() {
 fn if_the_last_frame_is_a_spare_you_get_one_extra_roll_that_is_scored_once() {
     let game = BowlingGame::new();
 
-    for x in (0..18) {
+    for _ in 0..18 {
         game.roll(0).is_ok();
     }
 
@@ -144,7 +148,7 @@ fn a_strike_earns_ten_points_in_a_frame_with_a_single_roll() {
 
     game.roll(10).is_ok();
 
-    for x in (0..18) {
+    for _ in 0..18 {
         game.roll(0).is_ok();
     }
 
@@ -159,7 +163,7 @@ fn points_scored_in_the_two_rolls_after_a_strike_are_counted_twice_as_a_bonus() 
     game.roll(5).is_ok();
     game.roll(3).is_ok();
 
-    for x in (0..16) {
+    for _ in 0..16 {
         game.roll(0).is_ok();
     }
 
@@ -176,7 +180,7 @@ fn consecutive_strikes_each_get_the_two_roll_bonus() {
     game.roll(5).is_ok();
     game.roll(3).is_ok();
 
-    for x in (0..12) {
+    for _ in 0..12 {
         game.roll(0).is_ok();
     }
 
@@ -187,7 +191,7 @@ fn consecutive_strikes_each_get_the_two_roll_bonus() {
 fn a_strike_in_the_last_frame_earns_a_two_roll_bonus_that_is_counted_once() {
     let game = BowlingGame::new();
 
-    for x in (0..18) {
+    for _ in 0..18 {
         game.roll(0).is_ok();
     }
 
@@ -202,7 +206,7 @@ fn a_strike_in_the_last_frame_earns_a_two_roll_bonus_that_is_counted_once() {
 fn a_spare_with_the_two_roll_bonus_does_not_get_a_bonus_roll() {
     let game = BowlingGame::new();
 
-    for x in (0..18) {
+    for _ in 0..18 {
         game.roll(0).is_ok();
     }
 
@@ -217,7 +221,7 @@ fn a_spare_with_the_two_roll_bonus_does_not_get_a_bonus_roll() {
 fn strikes_with_the_two_roll_bonus_do_not_get_a_bonus_roll() {
     let game = BowlingGame::new();
 
-    for x in (0..18) {
+    for _ in 0..18 {
         game.roll(0).is_ok();
     }
 
@@ -232,7 +236,7 @@ fn strikes_with_the_two_roll_bonus_do_not_get_a_bonus_roll() {
 fn a_strike_with_the_one_roll_bonus_after_a_spare_in_the_last_fram_does_not_get_a_bonus() {
     let game = BowlingGame::new();
 
-    for x in (0..18) {
+    for _ in 0..18 {
         game.roll(0).is_ok();
     }
 
@@ -247,7 +251,7 @@ fn a_strike_with_the_one_roll_bonus_after_a_spare_in_the_last_fram_does_not_get_
 fn all_strikes_is_a_perfect_score_of_300() {
     let game = BowlingGame::new();
 
-    for x in (0..12) {
+    for _ in 0..12 {
         game.roll(10).is_ok();
     }
 
@@ -266,7 +270,7 @@ fn you_can_not_roll_more_than_ten_pins_in_a_single_frame() {
 fn you_can_not_roll_more_than_ten_pins_with_the_two_bonus_rolls_after_a_final_strike() {
     let game = BowlingGame::new();
 
-    for x in (0..18) {
+    for _ in 0..18 {
         game.roll(0).is_ok();
     }
 
@@ -280,7 +284,7 @@ fn you_can_not_roll_more_than_ten_pins_with_the_two_bonus_rolls_after_a_final_st
 fn if_the_last_frame_is_a_strike_you_can_not_score_before_the_extra_rolls_are_taken() {
     let game = BowlingGame::new();
 
-    for x in (0..18) {
+    for _ in 0..18 {
         game.roll(0).is_ok();
     }
 
@@ -301,7 +305,7 @@ fn if_the_last_frame_is_a_strike_you_can_not_score_before_the_extra_rolls_are_ta
 fn if_the_last_frame_is_a_spare_you_can_not_create_a_score_before_extra_roll_is_taken() {
     let game = BowlingGame::new();
 
-    for x in (0..18) {
+    for _ in 0..18 {
         game.roll(0).is_ok();
     }
 
