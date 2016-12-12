18ce2b8c923dc57a44c8972773adb3832cc2c32e
Ian Whitney <whit0694@umn.edu>
2016-12-11 20:08:50 -0600

    Implement Largest Series Product
    Follows the canonical test suite as it stands in this commit
    
    https://github.com/exercism/x-common/blob/107c4416d3674d1ae10ba0a912aad9d1c4f3e909/exercises/largest-series-product/canonical-data.json
    
    Though I have started with a rust-specific test that shows students that `lsp`
    returns a Result.
    
    Placement is early in the Getting Rusty section. Results have been
    introduced by this point, as have some Higher Order Functions. The
    window iterator will be new. I've pointed students to it in the HINTS.md
    file.

---
+++ exercises/largest-series-product/HINTS.md
@@ -0,0 +1,6 @@
+# Largest Series Product in Rust
+
+These iterators may be useful, depending on your approach
+
+- [Windows](https://doc.rust-lang.org/std/slice/struct.Windows.html)
+- [Product](https://doc.rust-lang.org/std/iter/trait.Product.html)
---
+++ exercises/largest-series-product/tests/largest-series-product.rs
@@ -0,0 +1,105 @@
+extern crate largest_series_product;
+
+use largest_series_product::*;
+
+#[test]
+fn return_is_a_result() {
+    assert!(lsp("29", 2).is_ok());
+}
+
+#[test]
+#[ignore]
+fn find_the_largest_product_when_span_equals_length() {
+    assert_eq!(18, lsp("29", 2).unwrap());
+}
+
+#[test]
+#[ignore]
+fn find_the_largest_product_of_two_with_numbers_in_order() {
+    assert_eq!(72, lsp("0123456789", 2).unwrap());
+}
+
+#[test]
+#[ignore]
+fn find_the_largest_product_of_two_with_numbers_not_in_order() {
+    assert_eq!(48, lsp("576802143", 2).unwrap());
+}
+
+#[test]
+#[ignore]
+fn find_the_largest_product_of_three_with_numbers_in_order() {
+    assert_eq!(504, lsp("0123456789", 3).unwrap());
+}
+
+#[test]
+#[ignore]
+fn find_the_largest_product_of_three_with_numbers_not_in_order() {
+    assert_eq!(270, lsp("1027839564", 3).unwrap());
+}
+
+#[test]
+#[ignore]
+fn find_the_largest_product_of_five_with_numbers_in_order() {
+    assert_eq!(15120, lsp("0123456789", 5).unwrap());
+}
+
+#[test]
+#[ignore]
+fn span_of_six_in_a_large_number() {
+    assert_eq!(23520,
+               lsp("73167176531330624919225119674426574742355349194934", 6).unwrap());
+}
+
+#[test]
+#[ignore]
+fn returns_zero_if_number_is_zeros() {
+    assert_eq!(0, lsp("0000", 2).unwrap());
+}
+
+#[test]
+#[ignore]
+fn returns_zero_if_all_products_are_zero() {
+    assert_eq!(0, lsp("99099", 3).unwrap());
+}
+
+#[test]
+#[ignore]
+fn returns_an_error_if_span_is_longer_than_number() {
+    assert!(lsp("123", 4).is_err());
+}
+
+// There may be some confusion about whether this should be 1 or error.
+// The reasoning for it being 1 is this:
+// There is one 0-character string contained in the empty string.
+// That's the empty string itself.
+// The empty product is 1 (the identity for multiplication).
+// Therefore LSP('', 0) is 1.
+// It's NOT the case that LSP('', 0) takes max of an empty list.
+// So there is no error.
+// Compare against LSP('123', 4):
+// There are zero 4-character strings in '123'.
+// So LSP('123', 4) really DOES take the max of an empty list.
+// So LSP('123', 4) errors and LSP('', 0) does NOT.
+#[test]
+#[ignore]
+fn an_empty_string_and_no_span_returns_one() {
+    assert_eq!(1, lsp("", 0).unwrap());
+}
+
+#[test]
+#[ignore]
+fn a_non_empty_string_and_no_span_returns_one() {
+    assert_eq!(1, lsp("123", 0).unwrap());
+}
+
+#[test]
+#[ignore]
+fn empty_string_and_non_zero_span_is_an_error() {
+    assert!(lsp("", 1).is_err());
+}
+
+#[test]
+#[ignore]
+fn panics_if_string_contains_non_digits() {
+    assert!(lsp("1234a5", 2).is_err());
+}
