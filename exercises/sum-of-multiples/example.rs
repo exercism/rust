560ce72332455bbe5223aae72d5cca1688e3ba28
Ian Whitney <whit0694@umn.edu>
2016-12-19 08:21:25 -0600

    Update example solution
    https://github.com/exercism/xrust/pull/235#discussion_r92960308
    https://github.com/exercism/xrust/pull/235#discussion_r92960584

--- exercises/sum-of-multiples/example.rs
+++ exercises/sum-of-multiples/example.rs
@@ -1,11 +1,11 @@
 use std::collections::BTreeSet;
 
-pub fn sum_of_multiples(limit: u32, factors: &Vec<u32>) -> u32 {
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     let mut multiples: BTreeSet<u32> = BTreeSet::new();
 
-    for f in factors {
+    for &f in factors {
         let mut multiplier = 2;
-        let mut x = f.clone();
+        let mut x = f;
         while x < limit {
             multiples.insert(x);
             x = f * multiplier;
