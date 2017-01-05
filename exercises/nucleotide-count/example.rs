cf669e02f97e9f82079eeaed00db95aa6a292749
Peter Tseng <pht24@cornell.edu>
2017-01-04 22:55:18 -0800

    nucleotide-count: Use if-let rather than Entry

--- exercises/nucleotide-count/example.rs
+++ exercises/nucleotide-count/example.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::collections::hash_map::Entry;
 
 static VALID_NUCLEOTIDES: &'static str = "ACGT";
 
@@ -15,11 +14,10 @@ pub fn count(nucleotide: char, input: &str) -> Result<usize, char> {
 pub fn nucleotide_counts(input: &str) -> Result<HashMap<char, usize>, char> {
     let mut map: HashMap<char, usize> = VALID_NUCLEOTIDES.chars().map(|c| (c, 0)).collect();
     for nucleotide in input.chars() {
-        match map.entry(nucleotide) {
-            Entry::Vacant(_) => return Err(nucleotide),
-            Entry::Occupied(mut view) => {
-                *view.get_mut() += 1;
-            }
+        if let Some(n) = map.get_mut(&nucleotide) {
+            *n += 1;
+        } else {
+            return Err(nucleotide);
         }
     }
     Ok(map)
