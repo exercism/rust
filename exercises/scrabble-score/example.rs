739a00b040fd055930a91b8d70ea96f8eed92d08
Jules Kerssemakers <jules.kerssemakers@gmail.com>
2017-01-24 20:56:06 +0100

    Redo the scrabble-score example for the double-letter-value extension.
    Multiplying a letter value with a modifying token introduces dependency
    between characters of the word. This inter-character dependency makes it
    hard to keep the old map+fold method, which, in all its glorious
    functional-ness, doesn't like state between its iterations.
    
    We switch to a for-loop, which mutates state kept in the 'score' and
    'previous_char_value' variables.
    Idiomatic rust prefers for-loops over map() if the primary effect of
    the iteration is the side-effect, as is now the case.
    
    Unfortunately, the inter-loop dependency sacrifices (potential) iteration
    parallelism. An alternative approach that could conserve parallelism would
    be to replace any "X:double" with "XX" and "X:triple" with "XXX".
    However, that capability is beyond std's String::replace(), and
    introducing the (admittedly awesome) regex-crate just for this is
    probably overkill..
    
    The previous HashMap dictionary() is no longer suitable, as the value of
    some tokens ('2' and '3') now dynamically depends on the previous char.
    Also, Rust's match() is precisely 102% cooler than a HashMap, so use that!

--- exercises/scrabble-score/example.rs
+++ exercises/scrabble-score/example.rs
@@ -1,40 +1,35 @@
-use std::collections::HashMap;
-
 pub fn score(word: &str) -> u16 {
-    let values = dictionary();
-    word.to_lowercase()
-        .chars()
-        .map(|c| values.get(&c).clone())
-        .fold(0, |score, v| score + v.unwrap_or(&0))
+    let mut score: u16 = 0;
+    let mut previous_char_value: u16 = 0;
+
+    // normalise the word to lowercase, for case-insensitivity, also, for easier iteration,
+    // reduce the multi-character value multipliers :double and :triple to a single char.
+    let tokenised_word = word.to_lowercase().replace(":double", "2").replace(":triple", "3");
+
+    for a_char in tokenised_word.chars() {
+        let value = char_value(a_char, previous_char_value);
+        score += value;
+        previous_char_value = value;
+    }
+
+    score
 }
 
-fn dictionary() -> HashMap<char, u16> {
-    let mut values = HashMap::new();
-    values.insert('a', 1);
-    values.insert('b', 3);
-    values.insert('c', 3);
-    values.insert('d', 2);
-    values.insert('e', 1);
-    values.insert('f', 4);
-    values.insert('g', 2);
-    values.insert('h', 4);
-    values.insert('i', 1);
-    values.insert('j', 8);
-    values.insert('k', 5);
-    values.insert('l', 1);
-    values.insert('m', 3);
-    values.insert('n', 1);
-    values.insert('o', 1);
-    values.insert('p', 3);
-    values.insert('q', 10);
-    values.insert('r', 1);
-    values.insert('s', 1);
-    values.insert('t', 1);
-    values.insert('u', 1);
-    values.insert('v', 4);
-    values.insert('w', 4);
-    values.insert('x', 8);
-    values.insert('y', 4);
-    values.insert('z', 10);
-    values
+/// Calculates the value of the current character.
+///
+/// The previous characters' value is required in case the current
+/// token is a multiplier, which affects the score of the previous char.
+fn char_value(the_char: char, prev_value: u16) -> u16 {
+    match the_char {
+        'a'|'e'|'i'|'o'|'u'|'l'|'n'|'r'|'s'|'t' => 1,
+        'd'|'g' => 2,
+        'b'|'c'|'m'|'p' => 3,
+        'f'|'h'|'v'|'w'|'y' => 4,
+        'k' => 5,
+        'j'|'x' => 8,
+        'q'|'z' => 10,
+        '2' =>   prev_value, // repeat the previous value, effectively doubling the previous value
+        '3' => 2*prev_value, // repeat the previous value twice, effectively multiplying it by 3x
+        _   => 0, // 'funny' characters, such as accented ones or foreign alphabets, count for 0
+    }
 }
