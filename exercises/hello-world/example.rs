ef5056e632bb4090a0f32b4542af5e18bbc173ec
Peter Tseng <pht24@cornell.edu>
2017-01-31 19:10:17 -0800

    hello-world: simplify to unconditional "Hello, World!"
    The introductory exercise should require as little Rust knowledge as
    necessary. We'd like to introduce `cargo test` here since it will be
    used for all exercises, but introducing Option is too much.
    
    We add a stub in case it is not clear how to return a string. That way,
    the student only has to change the string provided.
    
    https://github.com/exercism/x-common/issues/520

--- exercises/hello-world/example.rs
+++ exercises/hello-world/example.rs
@@ -1,6 +1,3 @@
-pub fn hello(name: Option<&str>) -> String {
-    match name {
-        Some(n) => format!("Hello, {}!", n),
-        None => "Hello, World!".to_string(),
-    }
+pub fn hello() -> &'static str {
+    "Hello, World!"
 }
