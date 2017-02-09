f259c370b7220b8e72b24d52e91cbf74f549182e
Gibson Fahnestock <gibfahn@gmail.com>
2017-02-10 07:19:40 +0800

    luhn: Update example and add more tests (#253)
    The luhn test suite didn't cover a lot of cases, including:
     - Strings of zeros
     - Non-alphabetic characters
     - SINs which are only valid if you remember to reverse the string
     - A 9 in a doubled position (9 * 2 - 9 should equal 9 not 0)
     - " 0" (invalid as there's only 1 digit)
    
    This adds those tests and updates the example to properly reject symbols.

--- exercises/luhn/tests/luhn.rs
+++ exercises/luhn/tests/luhn.rs
@@ -15,6 +15,12 @@ fn single_zero_string_is_invalid() {
 
 #[test]
 #[ignore]
+fn simple_valid_sin() {
+    assert!(is_valid(" 5 9 "));
+}
+
+#[test]
+#[ignore]
 fn valid_canadian_sin_is_valid() {
     assert!(is_valid("046 454 286"));
 }
@@ -35,4 +41,40 @@ fn invalid_credit_card_is_invalid() {
 #[ignore]
 fn strings_that_contain_non_digits_are_invalid() {
     assert!(!is_valid("046a 454 286"));
+}
+
+#[test]
+#[ignore]
+fn punctuation_is_invalid() {
+    assert!(!is_valid("055-444-285"));
+}
+
+#[test]
+#[ignore]
+fn symbols_are_invalid() {
+    assert!(!is_valid("055£ 444$ 285"));
+}
+
+#[test]
+#[ignore]
+fn single_digit_with_space_is_invalid() {
+    assert!(!is_valid(" 0"));
+}
+
+#[test]
+#[ignore]
+fn lots_of_zeros_are_valid() {
+    assert!(is_valid(" 00000"));
+}
+
+#[test]
+#[ignore]
+fn another_valid_sin() {
+    assert!(is_valid("055 444 285"));
+}
+
+#[test]
+#[ignore]
+fn nine_doubled_is_nine() {
+    assert!(is_valid("091"));
 }
