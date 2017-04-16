8d2c0a4536a1b752ff036e46486b779fd47ea752
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2017-04-16 17:44:09 +0200

    Added more test cases

--- exercises/pascals-triangle/tests/pascals-triangle.rs
+++ exercises/pascals-triangle/tests/pascals-triangle.rs
@@ -40,3 +40,19 @@ fn last_of_four_rows() {
     let expected: Vec<u32> = vec![1, 3, 3, 1];
     assert_eq!(expected, pt.rows().pop().unwrap());
 }
+
+#[test]
+#[ignore]
+fn last_of_seven_rows() {
+    let pt = PascalsTriangle::new(7);
+    let expected: Vec<u32> = vec![1, 6, 15, 20, 15, 6, 1];
+    assert_eq!(expected, pt.rows().pop().unwrap());
+}
+
+#[test]
+#[ignore]
+fn last_of_twenty_rows() {
+    let pt = PascalsTriangle::new(21);
+    let expected: Vec<u32> = vec![1,20,190,1140,4845,15504,38760,77520,125970,167960,184756,167960,125970,77520,38760,15504,4845,1140,190,20,1];
+    assert_eq!(expected, pt.rows().pop().unwrap());
+}
