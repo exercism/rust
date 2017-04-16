e6f783a23b37fd05136733e9a955c7cd4e9fbf18
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2017-04-16 21:46:15 +0200

    Update pascals-triangle.rs
    typo

--- exercises/pascals-triangle/tests/pascals-triangle.rs
+++ exercises/pascals-triangle/tests/pascals-triangle.rs
@@ -51,7 +51,7 @@ fn last_of_seven_rows() {
 
 #[test]
 #[ignore]
-fn last_of_twenty_rows() {
+fn last_of_twentyone_rows() {
     let pt = PascalsTriangle::new(21);
     let expected: Vec<u32> = vec![1,20,190,1140,4845,15504,38760,77520,125970,167960,184756,167960,125970,77520,38760,15504,4845,1140,190,20,1];
     assert_eq!(expected, pt.rows().pop().unwrap());
