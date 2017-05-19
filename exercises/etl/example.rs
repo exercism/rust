1efb7d3d181bb14325b53e0235f489156217b2c3
Peter Tseng <pht24@cornell.edu>
2017-05-18 22:23:32 -0700

    etl 1.0.0: use single characters only
    https://github.com/exercism/x-common/blob/master/exercises/etl/description.md
    tells us that we are only working with single letters. In Rust, we would
    represent these as characters.
    
    However, ever since #33 the Rust track has used strings.
    
    It doesn't seem right to stray from the standard description. In using
    characters, we don't lose too much except perhaps deal with String
    lifetimes.

--- exercises/etl/example.rs
+++ exercises/etl/example.rs
@@ -1,8 +1,8 @@
 use std::ascii::AsciiExt;
 use std::collections::BTreeMap;
 
-pub fn transform(input: &BTreeMap<i32, Vec<String>>) -> BTreeMap<String, i32> {
+pub fn transform(input: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
     input.iter().flat_map(|(&n, vec)| {
-        vec.iter().map(move |s| (s.to_ascii_lowercase(), n))
+        vec.iter().map(move |c| (c.to_ascii_lowercase(), n))
     }).collect()
 }
