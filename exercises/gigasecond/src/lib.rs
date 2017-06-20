0eef6ff76c73fdfeca05e1ef2837937db9cd75c2
Peter Tseng <pht24@cornell.edu>
2017-06-19 19:35:44 -0700

    move up

--- exercises/gigasecond/src/lib.rs
+++ exercises/gigasecond/src/lib.rs
@@ -1,6 +1,7 @@
 extern crate chrono;
 use chrono::*;
 
+// Returns a UTC DateTime one billion seconds after start.
 pub fn after(start: DateTime<UTC>) -> DateTime<UTC> {
-    // Return a UTC DateTime one billion seconds after start.
+    unimplemented!()
 }
