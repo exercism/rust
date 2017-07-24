40a4ef936f2aae6853ff192c2c62bdce5ae327eb
sacherjj <sacherjj@gmail.com>
2017-07-24 12:27:08 -0400

    prime-factors: Add exercise

---
+++ exercises/prime-factors/example.rs
@@ -0,0 +1,13 @@
+pub fn factors(n: u64) -> Vec<u64> {
+    let mut val = n;
+    let mut out: Vec<u64> = vec![];
+    let mut possible:u64 = 2;
+    while val > 1 {
+        while val % possible == 0 {
+            out.push(possible);
+            val /= possible;
+        }
+        possible += 1;
+    }
+    out
+}
