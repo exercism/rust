b329135b3307294595d1d3162b9428e208baf0d0
sacherjj <sacherjj@gmail.com>
2017-07-25 10:18:53 -0400

    accumulate: Add exercise

---
+++ exercises/accumulate/example.rs
@@ -0,0 +1,15 @@
+pub fn map_function(values: Vec<i32>, f: &Fn(i32) -> i32) -> Vec<i32> {
+    let mut out: Vec<i32> = vec![];
+    for val in values {
+        out.push(f(val))
+    }
+    out
+}
+
+pub fn map_closure<F>(values: Vec<i32>, f: F) -> Vec<i32> where F: Fn(i32) -> i32 {
+        let mut out: Vec<i32> = vec![];
+        for val in values {
+            out.push(f(val))
+    }
+    out
+}   
