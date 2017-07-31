3d7198d47bfb212c1f961cd6b1a84a8ad3e1b1d4
Joe Sacher <sacherjj@gmail.com>
2017-07-31 03:48:38 -0400

    clock: Add exercise (#328)

---
+++ exercises/clock/.meta/hints.md
@@ -0,0 +1,10 @@
+## Rust Traits for .to_string()
+
+Did you implement .to_string() for the Clock struct?
+
+If so, try implementing the
+[Display trait](https://doc.rust-lang.org/std/fmt/trait.Display.html) for Clock instead.
+
+Traits allow for a common way to implement functionality for various types.
+
+For additional learning, how would you implement String::from for the Clock type?
---
+++ exercises/clock/example.rs
@@ -0,0 +1,32 @@
+use std::fmt;
+
+#[derive(Eq,PartialEq,Debug)]
+pub struct Clock {
+    minutes: i32
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let hours = self.minutes / 60;
+        let mins = self.minutes % 60;
+        write!(f, "{:02}:{:02}", hours, mins)
+    }
+}
+
+impl Clock {
+    pub fn new(hour: i32, minute: i32) -> Self {
+        Clock::build(hour * 60 + minute)
+    }
+
+    fn build(minutes: i32) -> Self {
+        let mut mins = minutes;
+        while mins < 0 {
+            mins += 1440;
+        }
+        Clock { minutes: mins % 1440 }
+    }
+
+    pub fn add_minutes(&mut self, minutes: i32) -> Self {
+        Clock::build(self.minutes + minutes)
+    }
+}
