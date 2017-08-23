db1e74f14f29e5955e19e2ebd36c1d4e73efe1cd
matbesancon <mathieu.besancon@gmail.com>
2017-08-23 13:47:45 -0400

    basic structure

---
+++ exercises/perfect-numbers/tests/perfect-numbers.rs
@@ -0,0 +1,30 @@
+extern crate perfect_numbers;
+
+use perfect_numbers::{Classification, classify};
+
+#[test]
+fn basic() {
+  assert_eq!(classify(0).unwrap_err(), "Number must be positive");
+}
+
+#[test]
+#[ignore]
+fn test_all() {
+  struct TestClassification {
+    num : u64,
+    result : perfect_numbers::Classification
+  }
+  let test_table: Vec<TestClassification> = vec![
+    TestClassification{num : 1, result : Classification::Deficient},
+    TestClassification{num : 13, result : Classification::Deficient},
+    TestClassification{num : 12, result : Classification::Abundant},
+    TestClassification{num : 6, result : Classification::Perfect},
+    TestClassification{num : 28, result : Classification::Perfect},
+    TestClassification{num : 496, result : Classification::Perfect},
+    TestClassification{num : 8128, result : Classification::Perfect}
+  ];
+  for t in test_table {
+    assert_eq!(classify(t.num).unwrap(), t.result);
+  }
+}
+
