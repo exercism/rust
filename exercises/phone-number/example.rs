d5c158a62b91b5a177c8af00f2154714752cce7f
Vincent Ho <vinmaster99@gmail.com>
2017-10-08 01:04:37 -0700

    Conform phone number unit test to NANP and updated track to 1.2.0

--- exercises/phone-number/example.rs
+++ exercises/phone-number/example.rs
@@ -1,15 +1,22 @@
 pub fn number(s: &str) -> Option<String> {
-    let digits: String = s
-        .chars()
-        .filter(|&c| c.is_digit(10))
-        .collect();
+    let digits: String = s.chars().filter(|&c| c.is_digit(10)).collect();
     match digits.len() {
-        10 => Some(digits),
-        11 => match digits.chars().nth(0) {
-            Some('1') => Some(digits[1..].to_string()),
-            _   => None
-        },
-        _  => None
+        10 => {
+            match (digits.chars().nth(0), digits.chars().nth(3)) {
+                (Some('0'), _) => None,
+                (Some('1'), _) => None,
+                (_, Some('0')) => None,
+                (_, Some('1')) => None,
+                _ => Some(digits),
+            }
+        }
+        11 => {
+            match digits.chars().nth(0) {
+                Some('1') => Some(digits[1..].to_string()),
+                _ => None,
+            }
+        }
+        _ => None,
     }
 }
 
@@ -18,10 +25,14 @@ pub fn area_code(s: &str) -> Option<String> {
 }
 
 pub fn pretty_print(s: &str) -> String {
-    number(s).map(|n|
-        format!("({area}) {prefix}-{exchange}",
-                area=&n[..3],
-                prefix=&n[3..6],
-                exchange=&n[6..])
-    ).unwrap_or("invalid".to_string())
+    number(s)
+        .map(|n| {
+            format!(
+                "({area}) {prefix}-{exchange}",
+                area = &n[..3],
+                prefix = &n[3..6],
+                exchange = &n[6..]
+            )
+        })
+        .unwrap_or("invalid".to_string())
 }
--- exercises/phone-number/tests/phone-number.rs
+++ exercises/phone-number/tests/phone-number.rs
@@ -5,90 +5,70 @@ fn to_some_string(s: &str) -> Option<String> {
 }
 
 #[test]
-fn test_number_cleans() {
-    assert_eq!(phone::number("(123) 456-7890"), to_some_string("1234567890"));
+fn test_cleans_the_number() {
+    assert_eq!(
+        phone::number("(223) 456-7890"),
+        to_some_string("2234567890")
+    );
 }
 
 #[test]
-#[ignore]
-fn test_number_cleans_with_dots() {
-    assert_eq!(phone::number("123.456.7890"), to_some_string("1234567890"));
+fn test_cleans_numbers_with_dots() {
+    assert_eq!(phone::number("223.456.7890"), to_some_string("2234567890"));
 }
 
 #[test]
-#[ignore]
-fn test_valid_when_11_digits_and_first_is_1() {
-    assert_eq!(phone::number("11234567890"), to_some_string("1234567890"));
+fn test_cleans_numbers_with_multiple_spaces() {
+    assert_eq!(
+        phone::number("223 456   7890   "),
+        to_some_string("2234567890")
+    );
 }
 
 #[test]
-#[ignore]
-fn test_invalid_when_11_digits() {
-    assert_eq!(phone::number("21234567890"), None);
-}
-
-#[test]
-#[ignore]
 fn test_invalid_when_9_digits() {
     assert_eq!(phone::number("123456789"), None);
 }
 
 #[test]
-#[ignore]
-fn test_invalid_when_empty() {
-    assert_eq!(phone::number(""), None);
+fn test_invalid_when_11_digits_does_not_start_with_a_1() {
+    assert_eq!(phone::number("22234567890"), None);
 }
 
 #[test]
-#[ignore]
-fn test_invalid_when_no_digits_present() {
-    assert_eq!(phone::number(" (-) "), None);
+fn test_valid_when_11_digits_and_starting_with_1() {
+    assert_eq!(phone::number("12234567890"), to_some_string("2234567890"));
 }
 
 #[test]
-#[ignore]
-fn test_valid_with_leading_characters() {
-    assert_eq!(phone::number("my number is 123 456 7890"), to_some_string("1234567890"));
+fn test_valid_when_11_digits_and_starting_with_1_even_with_punctuation() {
+    assert_eq!(
+        phone::number("+1 (223) 456-7890"),
+        to_some_string("2234567890")
+    );
 }
 
 #[test]
-#[ignore]
-fn test_valid_with_trailing_characters() {
-    assert_eq!(phone::number("123 456 7890 - bob"), to_some_string("1234567890"));
+fn test_invalid_when_more_than_11_digits() {
+    assert_eq!(phone::number("321234567890"), None);
 }
 
 #[test]
-#[ignore]
-fn test_area_code() {
-    assert_eq!(phone::area_code("1234567890"), to_some_string("123"));
+fn test_invalid_with_letters() {
+    assert_eq!(phone::number("123-abc-7890"), None);
 }
 
 #[test]
-#[ignore]
-fn test_area_code_with_full_us_phone_number() {
-    assert_eq!(phone::area_code("18234567890"), to_some_string("823"));
+fn test_invalid_with_punctuations() {
+    assert_eq!(phone::number("123-@:!-7890"), None);
 }
 
 #[test]
-#[ignore]
-fn test_area_code_with_invalid() {
-    assert_eq!(phone::area_code("1234161567890"), None);
+fn test_invalid_if_area_code_does_not_start_with_2_9() {
+    assert_eq!(phone::number("(123) 456-7890"), None);
 }
 
 #[test]
-#[ignore]
-fn test_pretty_print() {
-    assert_eq!(phone::pretty_print("1234567890"), "(123) 456-7890");
-}
-
-#[test]
-#[ignore]
-fn test_pretty_print_with_full_us_phone_number() {
-    assert_eq!(phone::pretty_print("11234567890"), "(123) 456-7890");
-}
-
-#[test]
-#[ignore]
-fn test_pretty_print_with_invalid() {
-    assert_eq!(phone::pretty_print("1186234567890"), "invalid");
+fn test_invalid_if_exchange_code_does_not_start_with_2_9() {
+    assert_eq!(phone::number("(223) 056-7890"), None);
 }
