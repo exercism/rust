8c735207a10a83e184193c94e16664e203a5f918
Vincent Ho <vinmaster99@gmail.com>
2017-10-09 20:03:44 -0700

    Remove code not being tested. Add back in the ignores

--- exercises/phone-number/example.rs
+++ exercises/phone-number/example.rs
@@ -19,20 +19,3 @@ pub fn number(s: &str) -> Option<String> {
         _ => None,
     }
 }
-
-pub fn area_code(s: &str) -> Option<String> {
-    number(s).map(|n| n[..3].to_string())
-}
-
-pub fn pretty_print(s: &str) -> String {
-    number(s)
-        .map(|n| {
-            format!(
-                "({area}) {prefix}-{exchange}",
-                area = &n[..3],
-                prefix = &n[3..6],
-                exchange = &n[6..]
-            )
-        })
-        .unwrap_or("invalid".to_string())
-}
--- exercises/phone-number/tests/phone-number.rs
+++ exercises/phone-number/tests/phone-number.rs
@@ -13,11 +13,13 @@ fn test_cleans_the_number() {
 }
 
 #[test]
+#[ignore]
 fn test_cleans_numbers_with_dots() {
     assert_eq!(phone::number("223.456.7890"), to_some_string("2234567890"));
 }
 
 #[test]
+#[ignore]
 fn test_cleans_numbers_with_multiple_spaces() {
     assert_eq!(
         phone::number("223 456   7890   "),
@@ -26,21 +28,25 @@ fn test_cleans_numbers_with_multiple_spaces() {
 }
 
 #[test]
+#[ignore]
 fn test_invalid_when_9_digits() {
     assert_eq!(phone::number("123456789"), None);
 }
 
 #[test]
+#[ignore]
 fn test_invalid_when_11_digits_does_not_start_with_a_1() {
     assert_eq!(phone::number("22234567890"), None);
 }
 
 #[test]
+#[ignore]
 fn test_valid_when_11_digits_and_starting_with_1() {
     assert_eq!(phone::number("12234567890"), to_some_string("2234567890"));
 }
 
 #[test]
+#[ignore]
 fn test_valid_when_11_digits_and_starting_with_1_even_with_punctuation() {
     assert_eq!(
         phone::number("+1 (223) 456-7890"),
@@ -49,26 +55,31 @@ fn test_valid_when_11_digits_and_starting_with_1_even_with_punctuation() {
 }
 
 #[test]
+#[ignore]
 fn test_invalid_when_more_than_11_digits() {
     assert_eq!(phone::number("321234567890"), None);
 }
 
 #[test]
+#[ignore]
 fn test_invalid_with_letters() {
     assert_eq!(phone::number("123-abc-7890"), None);
 }
 
 #[test]
+#[ignore]
 fn test_invalid_with_punctuations() {
     assert_eq!(phone::number("123-@:!-7890"), None);
 }
 
 #[test]
+#[ignore]
 fn test_invalid_if_area_code_does_not_start_with_2_9() {
     assert_eq!(phone::number("(123) 456-7890"), None);
 }
 
 #[test]
+#[ignore]
 fn test_invalid_if_exchange_code_does_not_start_with_2_9() {
     assert_eq!(phone::number("(223) 056-7890"), None);
 }
