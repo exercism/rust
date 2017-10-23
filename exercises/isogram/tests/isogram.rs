649a93e5e18f9c4bef01c2815abcef00b029d671
hekrause <hekrause@htwg-konstanz.de>
2017-10-23 13:34:55 +0200

    Added ignore macros

--- exercises/isogram/tests/isogram.rs
+++ exercises/isogram/tests/isogram.rs
@@ -8,45 +8,53 @@ fn empty_string() {
 }
 
 #[test]
+#[ignore]
 fn only_lower_case_characters() {
     assert_eq!(check("isogram"), true, "An \"isogram\" should be an isogram.")
 }
 
 #[test]
+#[ignore]
 fn one_duplicated_character() {
     assert_eq!(check("eleven"), false, "\"eleven\" has more than one \'e\'")
 }
 
 #[test]
+#[ignore]
 fn longest_reported_english_isogram() {
     assert_eq!(check("subdermatoglyphic"), true,
             "\"subdermatoglyphic\" should be an isogram.")
 }
 
 #[test]
+#[ignore]
 fn one_duplicated_character_mixed_case() {
     assert_eq!(check("Alphabet"), false, "\"Alphabet\" has more than one \'a\'")
 }
 
 #[test]
+#[ignore]
 fn hypothetical_isogramic_word_with_hyphen() {
     assert_eq!(check("thumbscrew-japingly"), true,
             "\"thumbscrew-japingly\" should be an isogram.")
 }
 
 #[test]
+#[ignore]
 fn isogram_with_duplicated_hyphen() {
     assert_eq!(check("six-year-old"), true,
             "\"six-year-old\" should be an isogram.")
 }
 
 #[test]
+#[ignore]
 fn made_up_name_that_is_an_isogram() {
     assert_eq!(check("Emily Jung Schwartzkopf"), true,
             "\"Emily Jung Schwartzkopf\" should be an isogram.")
 }
 
 #[test]
+#[ignore]
 fn duplicated_character_in_the_middle() {
     assert_eq!(check("accentor"), false, "\"accentor\" has more than one \'c\'")
 }
