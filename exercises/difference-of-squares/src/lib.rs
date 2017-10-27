bb86c0aadd9a616ea0bf53838d5e5a4f2339a75f
Peter Tseng <pht24@cornell.edu>
2017-10-27 16:08:46 -0700

    add unimplemented to stubs
    Otherwise, the actual return type of the stubs do not match their
    declared return type and thus the stub does not compile.

--- exercises/difference-of-squares/src/lib.rs
+++ exercises/difference-of-squares/src/lib.rs
@@ -1,11 +1,11 @@
 pub fn square_of_sum(n: usize) -> usize {
-
+    unimplemented!()
 }
 
 pub fn sum_of_squares(n: usize) -> usize {
-
+    unimplemented!()
 }
 
 pub fn difference(n: usize) -> usize {
-
+    unimplemented!()
 }
--- exercises/raindrops/src/lib.rs
+++ exercises/raindrops/src/lib.rs
@@ -1,3 +1,3 @@
 pub fn raindrops(n: usize) -> String {
-
+    unimplemented!()
 }
