59c4316f9c722986b688a46ea10d03e7b0e5f26c
Peter Tseng <pht24@cornell.edu>
2017-10-28 15:57:18 -0700

    isogram: rm trailing whitespace

--- exercises/isogram/example.rs
+++ exercises/isogram/example.rs
@@ -3,7 +3,7 @@ pub fn check(word: &str) -> bool {
     // Filter all non-Alphabetic character out and collect them in a new String
     let normalized_string: String = word.to_lowercase().chars().filter(|c| c.is_alphabetic()).collect();
 
-    /* Find the char element from back and front and compare the index. 
+    /* Find the char element from back and front and compare the index.
        If it is the same unique char the index will be the same.*/
     let is_unique = |x: char, word: &str| word.find(x).unwrap() == word.rfind(x).unwrap();
 
