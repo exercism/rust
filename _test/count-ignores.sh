a4c02a0d60418a3722849d587dd24091d8e1bd7d
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-11-14 12:29:42 +0100

    Conditionally disable count-ignores.sh; disable for perfect-numbers

--- _test/count-ignores.sh
+++ _test/count-ignores.sh
@@ -3,14 +3,21 @@
 repo=$(cd "$(dirname "$0")/.." && pwd)
 exitcode=0
 
-for t in $repo/exercises/*/tests/*.rs; do
-  tests=$(grep "^\s*\#\[test\]" $t | wc -l | tr -d '[:space:]')
-  ignores=$(grep "^\s*\#\[ignore\]" $t | wc -l | tr -d '[:space:]')
-  want_ignores=$(expr $tests - 1)
-  if [ "$ignores" != "$want_ignores" ]; then
-    echo "\033[1;31m$t: Has $tests tests and $ignores ignores (should be $want_ignores)\033[0m"
-    exitcode=1
-  fi
+for e in $repo/exercises/*; do
+   if [ -f "$e/.meta/ignore-count-ignores" ]; then
+      continue
+   fi
+   if [ -d "$e/tests" ]; then
+      for t in $e/tests/*.rs; do
+        tests=$(grep "^\s*\#\[test\]" $t | wc -l | tr -d '[:space:]')
+        ignores=$(grep "^\s*\#\[ignore\]" $t | wc -l | tr -d '[:space:]')
+        want_ignores=$(expr $tests - 1)
+        if [ "$ignores" != "$want_ignores" ]; then
+          echo "\033[1;31m$t: Has $tests tests and $ignores ignores (should be $want_ignores)\033[0m"
+          exitcode=1
+        fi
+      done
+   fi
 done
 
 exit $exitcode
