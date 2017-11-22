666c366fc0d39d9f6fd8e837facea8afe2153ae2
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-11-22 23:12:10 +0100

    Add macro implementation exercise
    Closes #397.
    
    This exercise requires that students implement a `hashmap!` macro
    to simplify creation of hashmap literals. The intent is to gently
    introduce macros-by-example by having them implement a macro which
    is both simple but useful.
    
    The [maplit crate](https://crates.io/crates/maplit) which inspired
    this exercise uses the [MIT license](https://opensource.org/licenses/MIT),
    which as far as I can tell permits this derivative use of its code.
    
    Used first edition book link in the readme because second edition's
    macros chapter is not yet written.

---
+++ exercises/macros/example.rs
@@ -0,0 +1,16 @@
+// Ignoring the README's injunction, this is heavily based on the implementation from maplit.
+// Original source is at https://github.com/bluss/maplit/blob/master/src/lib.rs#L27-L60
+
+#[macro_export]
+macro_rules! hashmap {
+    ($($key:expr => $value:expr,)+) => { hashmap!($($key => $value),+) };
+    ($($key:expr => $value:expr),*) => {
+        {
+            let mut _map = ::std::collections::HashMap::new();
+            $(
+                _map.insert($key, $value);
+            )*
+            _map
+        }
+    };
+}
