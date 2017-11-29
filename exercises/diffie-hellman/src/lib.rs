978895367508dc5a063d25295aabaf261392f0ce
hekrause <hekrause@htwg-konstanz.de>
2017-11-29 13:52:17 +0100

    Add underscores to variable names.

--- exercises/diffie-hellman/src/lib.rs
+++ exercises/diffie-hellman/src/lib.rs
@@ -1,12 +1,12 @@
 
-pub fn private_key(p: u64) -> u64 {
+pub fn private_key(_p: u64) -> u64 {
     unimplemented!()
 }
 
-pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
+pub fn public_key(_p: u64, _g: u64, _a: u64) -> u64 {
     unimplemented!()
 }
 
-pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
+pub fn secret(_p: u64, _b_pub: u64, _a: u64) -> u64 {
     unimplemented!()
 }
