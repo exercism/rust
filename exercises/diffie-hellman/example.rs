c0571ece33faecfcd66dbf98d10c524dbe0eb951
hekrause <hekrause@htwg-konstanz.de>
2017-11-29 13:20:46 +0100

    Make reqeusted changes.

--- exercises/diffie-hellman/example.rs
+++ exercises/diffie-hellman/example.rs
@@ -3,16 +3,13 @@ extern crate rand;
 use rand::distributions::{IndependentSample, Range};
 
 pub fn private_key(p: u64) -> u64 {
-    assert!(p < u64::max_value());
-    Range::new(2, p).ind_sample(&mut rand::thread_rng())
+    Range::new(1, p).ind_sample(&mut rand::thread_rng())
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    assert_ne!(g.checked_mul(a), None);
-    (g * a) % p
+    g.pow(a as u32) % p
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    assert_ne!(b_pub.checked_mul(a), None);
-    (b_pub * a) % p
+    b_pub.pow(a as u32) % p
 }
--- exercises/diffie-hellman/tests/diffie-hellman.rs
+++ exercises/diffie-hellman/tests/diffie-hellman.rs
@@ -4,46 +4,44 @@ use diffie_hellman::*;
 
 #[test]
 fn test_private_key_in_range_key() {
-    let primes: Vec<u64> = vec![5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 773, 9091,
-                                38321, 6131329];
+    let primes: Vec<u64> = vec![5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 773, 967,
+                                3461, 6131];
     let private_keys: Vec<u64> = primes.iter().map(|x| private_key(*x)).collect();
 
     for i in 0..primes.len() {
-        assert!(1 < private_keys[i] && private_keys[i] < primes[i]);
+        assert!(1 <= private_keys[i] && private_keys[i] < primes[i]);
     }
 }
 
 #[test]
-#[ignore]
 fn test_public_key_correct() {
     let p: u64 = 23;
     let g: u64 = 5;
 
     let private_key: u64 = 6;
-    let expected: u64 = 7;
+    let expected: u64 = 8;
 
     assert_eq!(public_key(p, g, private_key), expected);
 }
 
-#[test]
-#[ignore]
-fn test_secret_key_correct() {
-    let p: u64 = 92233987;
-    let g: u64 = 5371081;
 
-    let private_key = 75233987;
+#[test]
+fn test_secret_key_correct() {
+    let p: u64 = 11;
+    let g: u64 = 7;
+
+    let private_key = 7;
     let public_key = public_key(p, g, private_key);
     let secret = secret(p, public_key, private_key);
-    let expected = 76945309;
+    let expected = 8;
 
     assert_eq!(secret, expected);
 }
 
 #[test]
-#[ignore]
 fn test_changed_secret_key() {
-    let p: u64 = 92233987;
-    let g: u64 = 5371081;
+    let p: u64 = 13;
+    let g: u64 = 11;
 
     let private_key_a = private_key(p);
     let private_key_b = private_key(p);
@@ -51,6 +49,7 @@ fn test_changed_secret_key() {
     let public_key_a = public_key(p, g, private_key_a);
     let public_key_b = public_key(p, g, private_key_b);
 
+    // Key exchange
     let secret_a = secret(p, public_key_b, private_key_a);
     let secret_b = secret(p, public_key_a, private_key_b);
 
