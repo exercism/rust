4fdf058790e55b3b2186d05270d7e85c6e224bdd
Shing Tak Lam <shingtaklam1324@gmail.com>
2017-12-04 21:58:22 +0800

    Edited the 'rna-transcription' exercise to 'rna' and 'dna'

--- exercises/rna-transcription/example.rs
+++ exercises/rna-transcription/example.rs
@@ -1,17 +1,19 @@
 #[derive(PartialEq, Eq, Debug)]
-pub struct RibonucleicAcid {
-    nucleotides: String
+pub struct RNA {
+    nucleotides: String,
 }
 
-impl RibonucleicAcid {
-    pub fn new(nucleotides: &str) -> RibonucleicAcid {
-        RibonucleicAcid { nucleotides: nucleotides.to_string() }
+impl RNA {
+    pub fn new(nucleotides: &str) -> RNA {
+        RNA {
+            nucleotides: nucleotides.to_string(),
+        }
     }
 }
 
 #[derive(PartialEq, Eq, Debug)]
-pub struct DeoxyribonucleicAcid {
-    nucleotides: String
+pub struct DNA {
+    nucleotides: String,
 }
 
 fn transcribe_dna_rna(c: char) -> Option<char> {
@@ -20,19 +22,26 @@ fn transcribe_dna_rna(c: char) -> Option<char> {
         'G' => Some('C'),
         'A' => Some('U'),
         'T' => Some('A'),
-        _   => None
+        _ => None,
     }
 }
 
-impl DeoxyribonucleicAcid {
-    pub fn new(nucleotides: &str) -> DeoxyribonucleicAcid {
-        DeoxyribonucleicAcid { nucleotides: nucleotides.to_string() }
+impl DNA {
+    pub fn new(nucleotides: &str) -> DNA {
+        DNA {
+            nucleotides: nucleotides.to_string(),
+        }
     }
 
-    pub fn to_rna(&self) -> Result<RibonucleicAcid, ()> {
-        let rna_nucleotides: String = self.nucleotides.chars().filter_map(transcribe_dna_rna).collect();
+    pub fn to_rna(&self) -> Result<RNA, ()> {
+        let rna_nucleotides: String = self.nucleotides
+            .chars()
+            .filter_map(transcribe_dna_rna)
+            .collect();
         if rna_nucleotides.len() == self.nucleotides.len() {
-            Ok(RibonucleicAcid { nucleotides: rna_nucleotides })
+            Ok(RNA {
+                nucleotides: rna_nucleotides,
+            })
         } else {
             Err(())
         }
