587c185c869497658d64db52afbf3f57aef7760c
Richard Dodd <richard.dodd@itp-group.co.uk>
2018-02-02 14:52:36 +0000

    rna-transcription: Make DNA and RNA validate input on creation.
    Closes #423.
    
    The tests are re-ordered because it is natural to test validation before
    `to_rna` using the changed data structures.

--- exercises/rna-transcription/README.md
+++ exercises/rna-transcription/README.md
@@ -18,6 +18,17 @@ each nucleotide with its complement:
 * `T` -> `A`
 * `A` -> `U`
 
+## Notes on Rust implementation
+
+By using private fields in structs with public `new` functions returning
+`Option` or `Result` (as here with `DNA::new` & `RNA::new`), we can guarantee
+that the internal representation of `DNA` is correct. Because every valid DNA
+string has a valid RNA string, we don't need to return a `Result`/`Option` from
+`to_rna`.
+
+This explains the type signatures you will see in the tests.
+
+
 ## Rust Installation
 
 Refer to the [exercism help page][help-page] for Rust installation and learning
--- exercises/rna-transcription/tests/rna-transcription.rs
+++ exercises/rna-transcription/tests/rna-transcription.rs
@@ -1,55 +1,88 @@
 extern crate rna_transcription as dna;
 
 #[test]
+fn test_valid_dna_input() {
+    assert!(dna::DNA::new("GCTA").is_ok());
+}
+
+#[test]
+#[ignore]
+fn test_valid_rna_input() {
+    assert!(dna::RNA::new("CGAU").is_ok());
+}
+
+#[test]
+#[ignore]
+fn test_invalid_dna_input() {
+    // Invalid character
+    assert!(dna::DNA::new("X").is_err());
+    // Valid nucleotide, but invalid in context
+    assert!(dna::DNA::new("U").is_err());
+    // Longer string with contained errors
+    assert!(dna::DNA::new("ACGTUXXCTTAA").is_err());
+}
+
+#[test]
+#[ignore]
+fn test_invalid_rna_input() {
+    // Invalid character
+    assert!(dna::RNA::new("X").is_err());
+    // Valid nucleotide, but invalid in context
+    assert!(dna::RNA::new("T").is_err());
+    // Longer string with contained errors
+    assert!(dna::RNA::new("ACGUTTXCUUAA").is_err());
+}
+
+#[test]
+#[ignore]
 fn test_acid_equals_acid() {
-    assert_eq!(dna::RNA::new("CGA"), dna::RNA::new("CGA"));
-    assert_ne!(dna::RNA::new("CGA"), dna::RNA::new("AGC"));
+    assert_eq!(dna::DNA::new("CGA").unwrap(), dna::DNA::new("CGA").unwrap());
+    assert_ne!(dna::DNA::new("CGA").unwrap(), dna::DNA::new("AGC").unwrap());
+    assert_eq!(dna::RNA::new("CGA").unwrap(), dna::RNA::new("CGA").unwrap());
+    assert_ne!(dna::RNA::new("CGA").unwrap(), dna::RNA::new("AGC").unwrap());
 }
 
 #[test]
 #[ignore]
 fn test_transcribes_cytosine_guanine() {
-    assert_eq!(Ok(dna::RNA::new("G")), dna::DNA::new("C").to_rna());
+    assert_eq!(
+        dna::RNA::new("G").unwrap(),
+        dna::DNA::new("C").unwrap().to_rna()
+    );
 }
 
 #[test]
 #[ignore]
 fn test_transcribes_guanine_cytosine() {
-    assert_eq!(Ok(dna::RNA::new("C")), dna::DNA::new("G").to_rna());
+    assert_eq!(
+        dna::RNA::new("C").unwrap(),
+        dna::DNA::new("G").unwrap().to_rna()
+    );
 }
 
 #[test]
 #[ignore]
 fn test_transcribes_adenine_uracil() {
-    assert_eq!(Ok(dna::RNA::new("U")), dna::DNA::new("A").to_rna());
+    assert_eq!(
+        dna::RNA::new("U").unwrap(),
+        dna::DNA::new("A").unwrap().to_rna()
+    );
 }
 
 #[test]
 #[ignore]
 fn test_transcribes_thymine_to_adenine() {
-    assert_eq!(Ok(dna::RNA::new("A")), dna::DNA::new("T").to_rna());
+    assert_eq!(
+        dna::RNA::new("A").unwrap(),
+        dna::DNA::new("T").unwrap().to_rna()
+    );
 }
 
 #[test]
 #[ignore]
 fn test_transcribes_all_dna_to_rna() {
-    assert_eq!(Ok(dna::RNA::new("UGCACCAGAAUU")), dna::DNA::new("ACGTGGTCTTAA").to_rna())
-}
-
-#[test]
-#[ignore]
-fn handles_invalid_input() {
-    assert!(dna::DNA::new("U").to_rna().is_err());
-}
-
-#[test]
-#[ignore]
-fn handles_completely_invalid_input() {
-    assert!(dna::DNA::new("XXX").to_rna().is_err());
-}
-
-#[test]
-#[ignore]
-fn handles_partially_invalid_input() {
-    assert!(dna::DNA::new("ACGTXXXCTTAA").to_rna().is_err());
+    assert_eq!(
+        dna::RNA::new("UGCACCAGAAUU").unwrap(),
+        dna::DNA::new("ACGTGGTCTTAA").unwrap().to_rna()
+    )
 }
