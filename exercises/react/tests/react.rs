85c5f56e4672b9e3befffac74647e5f3e4e0ef7f
Peter Tseng <pht24@cornell.edu>
2018-03-10 11:09:45 +0000

    react: Use `&[...]` instead of `&vec![...]`
    Clippy suggested:
    useless use of `vec!`
    
    https://rust-lang-nursery.github.io/rust-clippy/master/index.html#useless_vec

--- exercises/react/tests/react.rs
+++ exercises/react/tests/react.rs
@@ -31,7 +31,7 @@ fn error_setting_a_nonexistent_input_cell() {
 fn compute_cells_calculate_initial_value() {
     let mut reactor = Reactor::new();
     let input = reactor.create_input(1);
-    let output = reactor.create_compute(&vec![input], |v| v[0] + 1).unwrap();
+    let output = reactor.create_compute(&[input], |v| v[0] + 1).unwrap();
     assert_eq!(reactor.value(output), Some(2));
 }
 
@@ -41,7 +41,7 @@ fn compute_cells_take_inputs_in_the_right_order() {
     let mut reactor = Reactor::new();
     let one = reactor.create_input(1);
     let two = reactor.create_input(2);
-    let output = reactor.create_compute(&vec![one, two], |v| v[0] + v[1] * 10).unwrap();
+    let output = reactor.create_compute(&[one, two], |v| v[0] + v[1] * 10).unwrap();
     assert_eq!(reactor.value(output), Some(21));
 }
 
@@ -50,7 +50,7 @@ fn compute_cells_take_inputs_in_the_right_order() {
 fn error_creating_compute_cell_if_input_doesnt_exist() {
     let mut dummy_reactor = Reactor::new();
     let input = dummy_reactor.create_input(1);
-    assert!(Reactor::new().create_compute(&vec![input], |_| 0).is_err());
+    assert!(Reactor::new().create_compute(&[input], |_| 0).is_err());
 }
 
 #[test]
@@ -61,7 +61,7 @@ fn do_not_break_cell_if_creating_compute_cell_with_valid_and_invalid_input() {
     let dummy_cell = dummy_reactor.create_input(2);
     let mut reactor = Reactor::new();
     let input = reactor.create_input(1);
-    assert!(reactor.create_compute(&vec![input, dummy_cell], |_| 0).is_err());
+    assert!(reactor.create_compute(&[input, dummy_cell], |_| 0).is_err());
     assert!(reactor.set_value(input, 5).is_ok());
     assert_eq!(reactor.value(input), Some(5));
 }
@@ -71,7 +71,7 @@ fn do_not_break_cell_if_creating_compute_cell_with_valid_and_invalid_input() {
 fn compute_cells_update_value_when_dependencies_are_changed() {
     let mut reactor = Reactor::new();
     let input = reactor.create_input(1);
-    let output = reactor.create_compute(&vec![input], |v| v[0] + 1).unwrap();
+    let output = reactor.create_compute(&[input], |v| v[0] + 1).unwrap();
     assert_eq!(reactor.value(output), Some(2));
     assert!(reactor.set_value(input, 3).is_ok());
     assert_eq!(reactor.value(output), Some(4));
@@ -82,9 +82,9 @@ fn compute_cells_update_value_when_dependencies_are_changed() {
 fn compute_cells_can_depend_on_other_compute_cells() {
     let mut reactor = Reactor::new();
     let input = reactor.create_input(1);
-    let times_two = reactor.create_compute(&vec![input], |v| v[0] * 2).unwrap();
-    let times_thirty = reactor.create_compute(&vec![input], |v| v[0] * 30).unwrap();
-    let output = reactor.create_compute(&vec![times_two, times_thirty], |v| v[0] + v[1]).unwrap();
+    let times_two = reactor.create_compute(&[input], |v| v[0] * 2).unwrap();
+    let times_thirty = reactor.create_compute(&[input], |v| v[0] * 30).unwrap();
+    let output = reactor.create_compute(&[times_two, times_thirty], |v| v[0] + v[1]).unwrap();
     assert_eq!(reactor.value(output), Some(32));
     assert!(reactor.set_value(input, 3).is_ok());
     assert_eq!(reactor.value(output), Some(96));
@@ -95,7 +95,7 @@ fn compute_cells_can_depend_on_other_compute_cells() {
 fn error_setting_a_compute_cell() {
     let mut reactor = Reactor::new();
     let input = reactor.create_input(1);
-    let output = reactor.create_compute(&vec![input], |_| 0).unwrap();
+    let output = reactor.create_compute(&[input], |_| 0).unwrap();
     assert!(reactor.set_value(output, 3).is_err());
 }
 
@@ -108,7 +108,7 @@ fn compute_cells_fire_callbacks() {
     {
         let mut reactor = Reactor::new();
         let input = reactor.create_input(1);
-        let output = reactor.create_compute(&vec![input], |v| v[0] + 1).unwrap();
+        let output = reactor.create_compute(&[input], |v| v[0] + 1).unwrap();
         assert!(reactor.add_callback(output, |v| values.push(v)).is_ok());
         assert!(reactor.set_value(input, 3).is_ok());
     }
@@ -120,7 +120,7 @@ fn compute_cells_fire_callbacks() {
 fn error_adding_callback_to_nonexistent_cell() {
     let mut dummy_reactor = Reactor::new();
     let input = dummy_reactor.create_input(1);
-    let output = dummy_reactor.create_compute(&vec![input], |_| 0).unwrap();
+    let output = dummy_reactor.create_compute(&[input], |_| 0).unwrap();
     assert!(Reactor::new().add_callback(output, |_: usize| println!("hi")).is_err());
 }
 
@@ -131,7 +131,7 @@ fn callbacks_only_fire_on_change() {
     {
         let mut reactor = Reactor::new();
         let input = reactor.create_input(1);
-        let output = reactor.create_compute(&vec![input], |v| if v[0] < 3 { 111 } else { 222 }).unwrap();
+        let output = reactor.create_compute(&[input], |v| if v[0] < 3 { 111 } else { 222 }).unwrap();
         assert!(reactor.add_callback(output, |v| values.push(v)).is_ok());
         assert!(reactor.set_value(input, 2).is_ok());
         assert!(reactor.set_value(input, 4).is_ok());
@@ -148,7 +148,7 @@ fn callbacks_can_be_added_and_removed() {
     {
         let mut reactor = Reactor::new();
         let input = reactor.create_input(11);
-        let output = reactor.create_compute(&vec![input], |v| v[0] + 1).unwrap();
+        let output = reactor.create_compute(&[input], |v| v[0] + 1).unwrap();
         let callback = reactor.add_callback(output, |v| values1.push(v)).unwrap();
         assert!(reactor.add_callback(output, |v| values2.push(v)).is_ok());
         assert!(reactor.set_value(input, 31).is_ok());
@@ -169,7 +169,7 @@ fn removing_a_callback_multiple_times_doesnt_interfere_with_other_callbacks() {
     {
         let mut reactor = Reactor::new();
         let input = reactor.create_input(1);
-        let output = reactor.create_compute(&vec![input], |v| v[0] + 1).unwrap();
+        let output = reactor.create_compute(&[input], |v| v[0] + 1).unwrap();
         let callback = reactor.add_callback(output, |v| values1.push(v)).unwrap();
         assert!(reactor.add_callback(output, |v| values2.push(v)).is_ok());
         // We want the first remove to be Ok, but we don't care about the others.
@@ -190,10 +190,10 @@ fn callbacks_should_only_be_called_once_even_if_multiple_dependencies_change() {
     {
         let mut reactor = Reactor::new();
         let input = reactor.create_input(1);
-        let plus_one = reactor.create_compute(&vec![input], |v| v[0] + 1).unwrap();
-        let minus_one1 = reactor.create_compute(&vec![input], |v| v[0] - 1).unwrap();
-        let minus_one2 = reactor.create_compute(&vec![minus_one1], |v| v[0] - 1).unwrap();
-        let output = reactor.create_compute(&vec![plus_one, minus_one2], |v| v[0] * v[1]).unwrap();
+        let plus_one = reactor.create_compute(&[input], |v| v[0] + 1).unwrap();
+        let minus_one1 = reactor.create_compute(&[input], |v| v[0] - 1).unwrap();
+        let minus_one2 = reactor.create_compute(&[minus_one1], |v| v[0] - 1).unwrap();
+        let output = reactor.create_compute(&[plus_one, minus_one2], |v| v[0] * v[1]).unwrap();
         assert!(reactor.add_callback(output, |v| values.push(v)).is_ok());
         assert!(reactor.set_value(input, 4).is_ok());
     }
@@ -207,9 +207,9 @@ fn callbacks_should_not_be_called_if_dependencies_change_but_output_value_doesnt
     {
         let mut reactor = Reactor::new();
         let input = reactor.create_input(1);
-        let plus_one = reactor.create_compute(&vec![input], |v| v[0] + 1).unwrap();
-        let minus_one = reactor.create_compute(&vec![input], |v| v[0] - 1).unwrap();
-        let always_two = reactor.create_compute(&vec![plus_one, minus_one], |v| v[0] - v[1]).unwrap();
+        let plus_one = reactor.create_compute(&[input], |v| v[0] + 1).unwrap();
+        let minus_one = reactor.create_compute(&[input], |v| v[0] - 1).unwrap();
+        let always_two = reactor.create_compute(&[plus_one, minus_one], |v| v[0] - v[1]).unwrap();
         assert!(reactor.add_callback(always_two, |v| values.push(v)).is_ok());
         for i in 2..5 {
             assert!(reactor.set_value(input, i).is_ok());
@@ -228,12 +228,12 @@ fn test_adder_with_boolean_values() {
     let b = reactor.create_input(false);
     let carry_in = reactor.create_input(false);
 
-    let a_xor_b = reactor.create_compute(&vec![a, b], |v| v[0] ^ v[1]).unwrap();
-    let sum = reactor.create_compute(&vec![a_xor_b, carry_in], |v| v[0] ^ v[1]).unwrap();
+    let a_xor_b = reactor.create_compute(&[a, b], |v| v[0] ^ v[1]).unwrap();
+    let sum = reactor.create_compute(&[a_xor_b, carry_in], |v| v[0] ^ v[1]).unwrap();
 
-    let a_xor_b_and_cin = reactor.create_compute(&vec![a_xor_b, carry_in], |v| v[0] && v[1]).unwrap();
-    let a_and_b = reactor.create_compute(&vec![a, b], |v| v[0] && v[1]).unwrap();
-    let carry_out = reactor.create_compute(&vec![a_xor_b_and_cin, a_and_b], |v| v[0] || v[1]).unwrap();
+    let a_xor_b_and_cin = reactor.create_compute(&[a_xor_b, carry_in], |v| v[0] && v[1]).unwrap();
+    let a_and_b = reactor.create_compute(&[a, b], |v| v[0] && v[1]).unwrap();
+    let carry_out = reactor.create_compute(&[a_xor_b_and_cin, a_and_b], |v| v[0] || v[1]).unwrap();
 
     let tests = vec![
         (false, false, false, false, false),
