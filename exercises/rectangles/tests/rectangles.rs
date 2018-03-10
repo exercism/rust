ccf6d69845af03577e8e0e4b89daa9d42031b594
Peter Tseng <pht24@cornell.edu>
2018-03-10 12:10:54 +0000

    rectangles: Use &[T] instead of &Vec<T> for input lines
    The tests create a vector and immediately take a reference to it, doing
    nothing else to the vector.
    If this were on a single line, Clippy would suggest:
    useless use of `vec!`
    
    https://rust-lang-nursery.github.io/rust-clippy/master/index.html#useless_vec
    
    Since these vectors are constructed strictly to express the input lines
    and are never expected to grow/shrink, it is reasonable to simply use a
    slice.

--- exercises/rectangles/tests/rectangles.rs
+++ exercises/rectangles/tests/rectangles.rs
@@ -4,135 +4,135 @@ use rectangles::count;
 
 #[test]
 fn test_zero_area_1() {
-    let lines = vec![];
-    assert_eq!(0, count(&lines))
+    let lines = &[];
+    assert_eq!(0, count(lines))
 }
 
 #[test]
 #[ignore]
 fn test_zero_area_2() {
-    let lines = vec![""];
-    assert_eq!(0, count(&lines))
+    let lines = &[""];
+    assert_eq!(0, count(lines))
 }
 
 #[test]
 #[ignore]
 fn test_empty_area() {
-    let lines = vec![" "];
-    assert_eq!(0, count(&lines))
+    let lines = &[" "];
+    assert_eq!(0, count(lines))
 }
 
 #[test]
 #[ignore]
 fn test_one_rectangle() {
-    let lines = vec![
+    let lines = &[
         "+-+",
         "| |",
         "+-+",
         ];
-    assert_eq!(1, count(&lines))
+    assert_eq!(1, count(lines))
 }
 
 #[test]
 #[ignore]
 fn test_two_rectangles_no_shared_parts() {
-    let lines = vec![
+    let lines = &[
         "  +-+",
         "  | |",
         "+-+-+",
         "| |  ",
         "+-+  "
         ];
-    assert_eq!(2, count(&lines))
+    assert_eq!(2, count(lines))
 }
 
 #[test]
 #[ignore]
 fn test_five_rectangles_three_regions() {
-    let lines = vec![
+    let lines = &[
         "  +-+",
         "  | |",
         "+-+-+",
         "| | |",
         "+-+-+"
         ];
-    assert_eq!(5, count(&lines))
+    assert_eq!(5, count(lines))
 }
 
 #[test]
 #[ignore]
 fn rectangle_of_height_1() {
-    let lines = vec![
+    let lines = &[
         "+--+",
         "+--+"
         ];
-    assert_eq!(1, count(&lines))
+    assert_eq!(1, count(lines))
 }
 
 #[test]
 #[ignore]
 fn rectangle_of_width_1() {
-    let lines = vec![
+    let lines = &[
         "++",
         "||",
         "++"
         ];
-    assert_eq!(1, count(&lines))
+    assert_eq!(1, count(lines))
 }
 
 #[test]
 #[ignore]
 fn unit_equare() {
-    let lines = vec![
+    let lines = &[
         "++",
         "++"
         ];
-    assert_eq!(1, count(&lines))
+    assert_eq!(1, count(lines))
 }
 
 #[test]
 #[ignore]
 fn test_incomplete_rectangles() {
-    let lines = vec![
+    let lines = &[
         "  +-+",
         "    |",
         "+-+-+",
         "| | -",
         "+-+-+"
         ];
-    assert_eq!(1, count(&lines))
+    assert_eq!(1, count(lines))
 }
 
 #[test]
 #[ignore]
 fn test_complicated() {
-    let lines = vec![
+    let lines = &[
         "+------+----+",
         "|      |    |",
         "+---+--+    |",
         "|   |       |",
         "+---+-------+"
         ];
-    assert_eq!(3, count(&lines))
+    assert_eq!(3, count(lines))
 }
 
 #[test]
 #[ignore]
 fn test_not_so_complicated() {
-    let lines = vec![
+    let lines = &[
         "+------+----+",
         "|      |    |",
         "+------+    |",
         "|   |       |",
         "+---+-------+"
         ];
-    assert_eq!(2, count(&lines))
+    assert_eq!(2, count(lines))
 }
 
 #[test]
 #[ignore]
 fn test_large_input_with_many_rectangles() {
-    let lines = vec![
+    let lines = &[
         "+---+--+----+",
         "|   +--+----+",
         "+---+--+    |",
@@ -142,5 +142,5 @@ fn test_large_input_with_many_rectangles() {
         "+------+  | |",
         "          +-+"
         ];
-    assert_eq!(60, count(&lines))
+    assert_eq!(60, count(lines))
 }
