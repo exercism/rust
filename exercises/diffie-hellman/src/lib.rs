e085173fd302ec3f894756acc0f935c0aafe783f
Baelyk <b43lyk@gmail.com>
2018-03-19 12:56:47 -0500

    Remove leading underscore from stub variables (#477)
    Resolves #476
    
    Remove the underscore and include the variable in the `unimplemented!()`
    macro to get around unused variable warnings.

--- exercises/diffie-hellman/src/lib.rs
+++ exercises/diffie-hellman/src/lib.rs
@@ -1,11 +1,11 @@
-pub fn private_key(_p: u64) -> u64 {
-    unimplemented!()
+pub fn private_key(p: u64) -> u64 {
+    unimplemented!("Pick a private key greater than 1 and less than {}", p)
 }
 
-pub fn public_key(_p: u64, _g: u64, _a: u64) -> u64 {
-    unimplemented!()
+pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
+    unimplemented!("Calculate public key using prime numbers {} and {}, and private key {}", p, g, a)
 }
 
-pub fn secret(_p: u64, _b_pub: u64, _a: u64) -> u64 {
-    unimplemented!()
+pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
+    unimplemented!("Calculate secret key using prime number {}, public key {}, and private key {}", p, b_pub, a)
 }
