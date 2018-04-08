1418849a21fa173083d50b4c240ff74db4773a89
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-04-08 16:43:57 +0200

    variable-length-quantity: eliminate stub warnings

--- exercises/variable-length-quantity/src/lib.rs
+++ exercises/variable-length-quantity/src/lib.rs
@@ -6,10 +6,10 @@ pub enum Error {
 
 /// Convert a list of numbers to a stream of bytes encoded with variable length encoding.
 pub fn to_bytes(values: &[u32]) -> Vec<u8> {
-    unimplemented!()
+    unimplemented!("Convert the values {:?} to a list of bytes", values)
 }
 
 /// Given a stream of bytes, extract all numbers which are encoded in there.
 pub fn from_bytes(bytes: &[u8]) -> Result<Vec<u32>, Error> {
-    unimplemented!()
+    unimplemented!("Convert the list of bytes {:?} to a list of numbers", bytes)
 }
