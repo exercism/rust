1dba2bd9aeea0e2646203f77077e67bd248daa3c
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-04-08 16:25:05 +0200

    pascals-triangle: eliminate stub warnings

--- exercises/pascals-triangle/src/lib.rs
+++ exercises/pascals-triangle/src/lib.rs
@@ -2,7 +2,7 @@ pub struct PascalsTriangle;
 
 impl PascalsTriangle {
     pub fn new(row_count: u32) -> Self {
-        unimplemented!();
+        unimplemented!("create Pascal's triangle with {} rows", row_count);
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
