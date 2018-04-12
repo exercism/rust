ba4508f5989b057db5c7551a957ddc210511607f
Kirill Meng <kirill.meng@hotmail.de>
2018-04-12 10:34:45 +0200

    add empty example.rs file
    	- pass 3 range failure tests by if statement in lib.rs

--- exercises/palindrome-products/src/lib.rs
+++ exercises/palindrome-products/src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[derive(Debug, PartialEq)]
+pub enum Error{
+    Empty,
+    RangeFailure,
+}
+
+pub fn get_palindrome_product(min: i32, max: i32) -> Result<i32, Error>{
+    if min > max{
+        return Err(Error::RangeFailure);
     }
+    Ok(1)
 }
