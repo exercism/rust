d684347f1e30c5d96cd345174eb3db0a9692a125
Kirill Meng <kimeng@htwg-konstanz.de>
2018-04-15 18:47:57 +0200

    add factors check in tests
    	- add exmaple.rs ref. solution

--- exercises/palindrome-products/src/lib.rs
+++ exercises/palindrome-products/src/lib.rs
@@ -4,9 +4,92 @@ pub enum Error{
     RangeFailure,
 }
 
-pub fn get_palindrome_product(min: i32, max: i32) -> Result<i32, Error>{
+#[allow(dead_code)]
+#[derive(Debug, PartialEq)]
+pub struct ReturnVals{
+    pub result: i32,
+    pub factors: Vec<(i32, i32)>,
+}
+
+pub fn get_smallest_palindrome_product(min: i32, max: i32) -> Result<ReturnVals, Error>{
     if min > max{
         return Err(Error::RangeFailure);
     }
-    Ok(1)
+
+    let palindrome: Result<i32, Error> = get_smallest_palindrome(min, max);
+    if palindrome.is_err(){
+        return Err(palindrome.unwrap_err());
+    }
+    let p = palindrome.unwrap();
+    Ok(ReturnVals{
+        result: p,
+        factors: get_factors(p, min, max),
+    })
+}
+
+pub fn get_largest_palindrome_product(min: i32, max: i32) -> Result<ReturnVals, Error>{
+    if min > max{
+        return Err(Error::RangeFailure);
+    }
+
+    let palindrome: Result<i32, Error> = get_largest_palindrome(min, max);
+    if palindrome.is_err(){
+        return Err(palindrome.unwrap_err());
+    }
+    let p = palindrome.unwrap();
+    Ok(ReturnVals{
+        result: p,
+        factors: get_factors(p, min, max),
+    })
+}
+
+fn get_factors(n: i32, min: i32, max:i32)-> Vec<(i32, i32)>{
+    let mut factors = Vec::new();
+
+    for number in min .. max{
+        let div = n/number;
+        if n % number == 0 &&  div <= max && div >= min && !factors.contains(&(div, number)){
+            factors.push((number, n/number));
+        }
+    }
+    factors
+}
+
+fn get_smallest_palindrome(min: i32, max:i32)-> Result<i32, Error>{
+    let l:Vec<i32> = (min*min .. max*max).collect();
+        let filtered: Vec<i32> = l.iter()
+        .cloned()
+        .filter(|n| is_palindrome(*n) && has_factors(*n, min, max))
+        .collect::<Vec<i32>>();
+        if filtered.is_empty(){
+            return Err(Error::Empty);
+        } else{
+            Ok(*filtered.iter().min().unwrap())
+        }
+}
+
+fn get_largest_palindrome(min: i32, max:i32)-> Result<i32, Error>{
+        let l:Vec<i32> = (min*min .. max*max).collect();
+        let filtered: Vec<i32> = l.iter()
+        .cloned()
+        .filter(|n| is_palindrome(*n) && has_factors(*n, min, max))
+        .collect::<Vec<i32>>();
+        if filtered.is_empty(){
+            return Err(Error::Empty);
+        } else{
+            Ok(*filtered.iter().max().unwrap())
+        }
+        
+}
+ 
+fn has_factors(n: i32, min:i32, max:i32)->bool{
+    let fac = get_factors(n, min, max);
+    !fac.is_empty()
+}
+
+fn is_palindrome(s: i32)->bool{
+    let s1 = s.to_string();
+    let s2 = s1.chars().rev().collect::<String>();
+
+    s1 == s2
 }
--- exercises/palindrome-products/tests/palindrome-products.rs
+++ exercises/palindrome-products/tests/palindrome-products.rs
@@ -1,62 +1,73 @@
 extern crate palindrome_products;
 use palindrome_products::*;
 
+enum GET{
+    Smallest,
+    Largest,
+}
+
+fn test(e: GET, (min, max): (i32, i32))->Result<ReturnVals, Error>{
+    match e{
+        GET::Smallest => return get_smallest_palindrome_product(min, max),
+        GET::Largest => return get_largest_palindrome_product(min, max),
+    }
+}
 #[test]
 fn smallest_palindrome_single_digits(){
-    assert_eq!(get_palindrome_product(1,9), Ok(1));
+    assert_eq!(test(GET::Smallest, (1,9)), Ok(ReturnVals{result: 1, factors: vec!((1,1))}));
 }
 
 #[test]
 fn largest_palindrome_single_digits(){
-    assert_eq!(get_palindrome_product(1,9), Ok(9));
+    assert_eq!(test(GET::Largest, (1,9)), Ok(ReturnVals{result: 9, factors: vec!((1,9), (3,3))}));
 }
 
 #[test]
 fn smallest_palindrome_double_digits(){
-    assert_eq!(get_palindrome_product(10,99), Ok(121));
+    assert_eq!(test(GET::Smallest, (10,99)), Ok(ReturnVals{result: 121, factors: vec!((11,11))}))
 }
 
 #[test]
 fn largest_palindrome_double_digits(){
-    assert_eq!(get_palindrome_product(10,99), Ok(9009));
+    assert_eq!(test(GET::Largest, (10,99)), Ok(ReturnVals{result: 9009, factors: vec!((91,99))}))
 }
 
 #[test]
 fn smallest_palindrome_triple_digits(){
-    assert_eq!(get_palindrome_product(100,999), Ok(10201));
+    assert_eq!(test(GET::Smallest, (100,999)), Ok(ReturnVals{result: 10201, factors: vec!((101,101))}))
 }
 
 #[test]
 fn largest_palindrome_triple_digits(){
-    assert_eq!(get_palindrome_product(100,999), Ok(906609));
+    assert_eq!(test(GET::Largest, (100,999)), Ok(ReturnVals{result: 906609, factors: vec!((913,993))}))
 }
 
 #[test]
 fn smallest_palindrome_four_digits(){
-    assert_eq!(get_palindrome_product(1000,9999), Ok(1002001));
+    assert_eq!(test(GET::Smallest, (1000,9999)), Ok(ReturnVals{result: 1002001, factors: vec!((1001,1001))}))
 }
 
 #[test]
 fn largest_palindrome_four_digits(){
-    assert_eq!(get_palindrome_product(1000,9999), Ok(99000099));
+    assert_eq!(test(GET::Largest, (1000,9999)), Ok(ReturnVals{result: 99000099, factors: vec!((9901,9999))}))
 }
 
 #[test]
 fn empty_result_for_smallest_palindrome(){
-    assert_eq!(get_palindrome_product(1002, 1003), Err(Error::Empty));
+    assert_eq!(test(GET::Smallest, (1002,1003)), Err(Error::Empty));
 }
 
 #[test]
 fn empty_result_for_largest_palindrome(){
-    assert_eq!(get_palindrome_product(15, 15), Err(Error::Empty));
+    assert_eq!(test(GET::Largest, (15,15)), Err(Error::Empty));
 }
 
 #[test]
 fn error_smallest_palindrome_when_min_bt_max(){
-    assert_eq!(get_palindrome_product(1000, 1), Err(Error::RangeFailure));
+    assert_eq!(test(GET::Smallest, (1000,1)), Err(Error::RangeFailure));
 }
 
 #[test]
 fn error_largest_palindrome_when_min_bt_max(){
-    assert_eq!(get_palindrome_product(2, 1), Err(Error::RangeFailure));
+    assert_eq!(test(GET::Largest, (2,1)), Err(Error::RangeFailure));
 }
