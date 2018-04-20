b21b3101589058c615eb2853f8cc8b558c7c5d7c
Kirill Meng <kimeng@htwg-konstanz.de>
2018-04-20 22:03:43 +0200

    apply rustfmt
    	- put the first running test on the top

--- exercises/palindrome-products/example.rs
+++ exercises/palindrome-products/example.rs
@@ -1,25 +1,25 @@
 pub type Palindrome = u64;
-pub fn get_palindrome_products(min: u64, max: u64) -> Vec<Palindrome>{
-    let mut palindromes:Vec<u64> = Vec::new();
-    for i in min..max+1{
-        for j in min..max+1{
-            if is_palindrome(i*j){
-                palindromes.push(i*j);
+pub fn get_palindrome_products(min: u64, max: u64) -> Vec<Palindrome> {
+    let mut palindromes: Vec<u64> = Vec::new();
+    for i in min..max + 1 {
+        for j in min..max + 1 {
+            if is_palindrome(i * j) {
+                palindromes.push(i * j);
             }
         }
     }
     palindromes
 }
 
-pub fn min(palindromes: &[Palindrome])->Option<Palindrome>{
+pub fn min(palindromes: &[Palindrome]) -> Option<Palindrome> {
     palindromes.iter().min().map(|n| n.clone())
 }
 
-pub fn max(palindromes: &[Palindrome])->Option<Palindrome>{
+pub fn max(palindromes: &[Palindrome]) -> Option<Palindrome> {
     palindromes.iter().max().map(|n| n.clone())
 }
 
-fn is_palindrome(n: u64)->bool{
+fn is_palindrome(n: u64) -> bool {
     let s = n.to_string().into_bytes();
     s.iter().zip(s.iter().rev()).all(|(c1, c2)| c1 == c2)
 }
--- exercises/palindrome-products/tests/palindrome-products.rs
+++ exercises/palindrome-products/tests/palindrome-products.rs
@@ -2,32 +2,7 @@ extern crate palindrome_products;
 use palindrome_products::*;
 
 #[test]
-#[ignore]
-fn empty_result_for_smallest_palindrome(){
-    assert_eq!(min(&get_palindrome_products(1002, 1003)), None);
-}
-
-#[test]
-#[ignore]
-fn empty_result_for_largest_palindrome(){
-    assert_eq!(max(&get_palindrome_products(15, 15)), None);
-}
-
-#[test]
-#[ignore]
-fn error_smallest_palindrome_when_min_gt_max(){
-    assert_eq!(min(&get_palindrome_products(1000, 1)), None);
-}
-
-#[test]
-#[ignore]
-fn error_largest_palindrome_when_min_st_max(){
-    assert_eq!(max(&get_palindrome_products(2, 1)), None);
-}
-
-
-#[test]
-fn single_digits(){
+fn single_digits() {
     let palindromes = get_palindrome_products(1, 9);
     assert_eq!(min(&palindromes), Some(1));
     assert_eq!(max(&palindromes), Some(9));
@@ -35,7 +10,7 @@ fn single_digits(){
 
 #[test]
 #[ignore]
-fn double_digits(){
+fn double_digits() {
     let palindromes = get_palindrome_products(10, 99);
     assert_eq!(min(&palindromes), Some(121));
     assert_eq!(max(&palindromes), Some(9009));
@@ -43,7 +18,7 @@ fn double_digits(){
 
 #[test]
 #[ignore]
-fn triple_digits(){
+fn triple_digits() {
     let palindromes = get_palindrome_products(100, 999);
     assert_eq!(min(&palindromes), Some(10201));
     assert_eq!(max(&palindromes), Some(906609));
@@ -51,8 +26,32 @@ fn triple_digits(){
 
 #[test]
 #[ignore]
-fn four_digits(){
+fn four_digits() {
     let palindromes = get_palindrome_products(1000, 9999);
     assert_eq!(min(&palindromes), Some(1002001));
     assert_eq!(max(&palindromes), Some(99000099));
+}
+
+#[test]
+#[ignore]
+fn empty_result_for_smallest_palindrome() {
+    assert_eq!(min(&get_palindrome_products(1002, 1003)), None);
+}
+
+#[test]
+#[ignore]
+fn empty_result_for_largest_palindrome() {
+    assert_eq!(max(&get_palindrome_products(15, 15)), None);
+}
+
+#[test]
+#[ignore]
+fn error_smallest_palindrome_when_min_gt_max() {
+    assert_eq!(min(&get_palindrome_products(1000, 1)), None);
+}
+
+#[test]
+#[ignore]
+fn error_largest_palindrome_when_min_st_max() {
+    assert_eq!(max(&get_palindrome_products(2, 1)), None);
 }
