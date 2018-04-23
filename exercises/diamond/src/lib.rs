d17563f414aecd4211c9c9ebe9d834df0e862e19
Kirill Meng <kimeng@htwg-konstanz.de>
2018-04-23 21:15:06 +0200

    remove '+' form cargo.toml
    	- add tests
    	- add stub

---
+++ exercises/diamond/src/lib.rs
@@ -0,0 +1,3 @@
+pub fn get_diamond(c: char) -> Vec<String>{
+    unimplemented!("Return the vector of strings which represent the diamond with particular char {}", c);
+}
---
+++ exercises/diamond/test/diamond.rs
@@ -0,0 +1,94 @@
+extern crate diamond;
+
+#[test]
+fn test_A(){
+    assert_eq!(get_diamond("A"), vec!["A"]);
+}
+
+#[test]
+#[ignore]
+fn test_B(){
+    assert_eq!(get_diamond("B"), vec![" A ", 
+                                      "B B",
+                                      " A " ]);
+}
+
+#[test]
+#[ignore]
+fn test_C(){
+    assert_eq!(get_diamond('C'), vec!["  A  ",
+                                      " B B ",
+                                      "C   C",
+                                      " B B ",
+                                      "  A  "]);
+}
+
+#[test]
+#[ignore]
+fn test_D(){
+    assert_eq!(get_diamond('D'), vec!["   A   ",
+                                      "  B B  ",
+                                      " C   C ",
+                                      "D     D",
+                                      " C   C ",
+                                      "  B B  ",
+                                      "   A   ",
+}
+
+#[test]
+#[ignore]
+fn test_Z(){
+    assert_eq!(get_diamond('Z'), vec![
+                "                         A                         ",
+                "                        B B                        ",
+                "                       C   C                       ",
+                "                      D     D                      ",
+                "                     E       E                     ",
+                "                    F         F                    ",
+                "                   G           G                   ",
+                "                  H             H                  ",
+                "                 I               I                 ",
+                "                J                 J                ",
+                "               K                   K               ",
+                "              L                     L              ",
+                "             M                       M             ",
+                "            N                         N            ",
+                "           O                           O           ",
+                "          P                             P          ",
+                "         Q                               Q         ",
+                "        R                                 R        ",
+                "       S                                   S       ",
+                "      T                                     T      ",
+                "     U                                       U     ",
+                "    V                                         V    ",
+                "   W                                           W   ",
+                "  X                                             X  ",
+                " Y                                               Y ",
+                "Z                                                 Z",
+                " Y                                               Y ",
+                "  X                                             X  ",
+                "   W                                           W   ",
+                "    V                                         V    ",
+                "     U                                       U     ",
+                "      T                                     T      ",
+                "       S                                   S       ",
+                "        R                                 R        ",
+                "         Q                               Q         ",
+                "          P                             P          ",
+                "           O                           O           ",
+                "            N                         N            ",
+                "             M                       M             ",
+                "              L                     L              ",
+                "               K                   K               ",
+                "                J                 J                ",
+                "                 I               I                 ",
+                "                  H             H                  ",
+                "                   G           G                   ",
+                "                    F         F                    ",
+                "                     E       E                     ",
+                "                      D     D                      ",
+                "                       C   C                       ",
+                "                        B B                        ",
+                "                         A                         "
+    ]);
+}
