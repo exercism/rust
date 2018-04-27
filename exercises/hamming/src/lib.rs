94d7265bd9cbdeeaff739299f108c7a72e3752b8
AvasDream <vincegrixc@gmail.com>
2018-04-27 20:32:55 +0200

    added templates to 9 exercises (#517)
    * added templates to 9 exercises
    
    * fixed typo

---
+++ exercises/hamming/src/lib.rs
@@ -0,0 +1,6 @@
+/// Return the Hamming distance between the strings,
+/// or an error if the lengths are mismatched.
+pub fn hamming_distance(s1: &str, s2: &str) -> Result<u64, ()> {
+    unimplemented!("What is the Hamming Distance between {:?} and {:?}",
+                   s1, s2);
+}
