53c30297fc730a7fba8f4cbaa38185c01023d550
Kirill Meng <kimeng@htwg-konstanz.de>
2018-04-28 12:03:40 +0200

    add tests

---
+++ exercises/spiral-matrix/tests/spiral-matrix.rs
@@ -0,0 +1,55 @@
+extern crate spiral_matrix;
+use spiral_matrix::*;
+
+#[test]
+fn empty_spiral() {
+    let expected: Vec<Vec<usize>> = Vec::new();
+    assert_eq!(get_spiral(0), expected);
+}
+
+#[test]
+#[ignore]
+fn size_one_spiral() {
+    let expected: Vec<Vec<usize>> = Vec::new();
+    expected.push(vec![1]);
+    assert_eq!(get_spiral(1), expected);
+}
+#[test]
+#[ignore]
+fn size_two_spiral() {
+    let expected: Vec<Vec<usize>> = Vec::new();
+    expected.push(vec![1, 2]);
+    expected.push(vec![4, 3]);
+    assert_eq!(get_spiral(2), expected);
+}
+
+#[test]
+#[ignore]
+fn size_three_spiral() {
+    let expected: Vec<Vec<usize>> = Vec::new();
+    expected.push(vec![1, 2, 3]);
+    expected.push(vec![8, 9, 4]);
+    expected.push(vec![7, 6, 5]);
+    assert_eq!(get_spiral(3), expected);
+}
+#[test]
+#[ignore]
+fn size_four_spiral() {
+    let expected: Vec<Vec<usize>> = Vec::new();
+    expected.push(vec![1, 2, 3, 4]);
+    expected.push(vec![12, 13, 14, 5]);
+    expected.push(vec![11, 16, 15, 6]);
+    expected.push(vec![10, 9, 8, 7]);
+    assert_eq!(get_spiral(4), expected);
+}
+#[test]
+#[ignore]
+fn size_five_spiral() {
+    let expected: Vec<Vec<usize>> = Vec::new();
+    expected.push(vec![1, 2, 3, 4, 5]);
+    expected.push(vec![16, 17, 18, 19, 6]);
+    expected.push(vec![15, 24, 25, 20, 7]);
+    expected.push(vec![14, 23, 22, 21, 8]);
+    expected.push(vec![13, 12, 11, 10, 9]);
+    assert_eq!(get_spiral(5), expected);
+}
