13419bc03720e12fb077574e293a5bba784d842e
Kirill Meng <kirill.meng@hotmail.de>
2018-04-29 23:38:36 +0200

    ad return type in stub

--- exercises/spiral-matrix/src/lib.rs
+++ exercises/spiral-matrix/src/lib.rs
@@ -1,4 +1,4 @@
-pub fn get_spiral(size: usize) {
+pub fn get_spiral(size: usize) -> Vec<Vec<usize>> {
     unimplemented!(
         "Function that returns the spiral matrix of square size {}",
         size
