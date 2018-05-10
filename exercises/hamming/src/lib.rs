5638873396d9ed0522d8117e2840d380b9305036
Austin Kiekintveld <me@akiek.com>
2018-05-10 17:38:46 -0400

    hamming: Replace `Result<T, ()>` antipattern with `Option`

--- exercises/hamming/src/lib.rs
+++ exercises/hamming/src/lib.rs
@@ -1,6 +1,6 @@
 /// Return the Hamming distance between the strings,
-/// or an error if the lengths are mismatched.
-pub fn hamming_distance(s1: &str, s2: &str) -> Result<usize, ()> {
+/// or None if the lengths are mismatched.
+pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
     unimplemented!("What is the Hamming Distance between {:?} and {:?}",
                    s1, s2);
 }
