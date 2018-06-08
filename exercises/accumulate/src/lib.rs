8173ccd9c4c47d47cd51a78074ff7341caeb42e7
Nathaniel Knight <nknight@cfenet.ubc.ca>
2018-06-08 14:27:16 -0700

    accumulate: Fix type of closure argument in template

--- exercises/accumulate/src/lib.rs
+++ exercises/accumulate/src/lib.rs
@@ -2,6 +2,9 @@ pub fn map_function<T, R>(xs: Vec<T>, f: &Fn(T) -> R) -> Vec<R> {
     panic!("Implement your solution here")
 }
 
-pub fn map_closure<T, R>(xs: Vec<T>, f: &Fn(T) -> R) -> Vec<R> {
+pub fn map_closure<F, T, R>(xs: Vec<T>, f: F) -> Vec<R>
+where
+    F: Fn(T) -> R,
+{
     panic!("Implement your solution here")
 }
