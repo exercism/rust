79a4f5d339f0326e34b3f63ba6b2ac6d910f4227
Nathaniel Knight <nknight@cfenet.ubc.ca>
2018-06-08 14:10:12 -0700

    Add a template to the `accumulate` exercise
    With an empty `src/lib.rs` the test suite won't compile until a
    learner has written a more or less complete solution. This means they
    can't pass the first test until they've understood the whole suite.
    
    This commit adds stub implementations of the `map_function` and
    `map_closure` functions so that the test suite compiles as each test
    is passed.

---
+++ exercises/accumulate/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn map_function<T, R>(xs: Vec<T>, f: &Fn(T) -> R) -> Vec<R> {
+    panic!("Implement your solution here")
+}
+
+pub fn map_closure<T, R>(xs: Vec<T>, f: &Fn(T) -> R) -> Vec<R> {
+    panic!("Implement your solution here")
+}
