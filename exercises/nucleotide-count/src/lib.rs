d75b9adfbc038d25cd1629f51c527ff243bab6a8
ZapAnton <wc3ft2@gmail.com>
2018-07-03 02:03:58 +0300

    nucleotide-count: Added template to stub file

--- exercises/nucleotide-count/src/lib.rs
+++ exercises/nucleotide-count/src/lib.rs
@@ -1,1 +1,16 @@
+use std::collections::HashMap;
 
+pub fn count(nucleotide: char, dna: &str) -> Result<usize, ()> {
+    unimplemented!(
+        "How much of nucleotide type '{}' is contained inside DNA string '{}'?",
+        nucleotide,
+        dna
+    );
+}
+
+pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
+    unimplemented!(
+        "How much of every nucleotide type is contained inside DNA string '{}'?",
+        dna
+    );
+}
