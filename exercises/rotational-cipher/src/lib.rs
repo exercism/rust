d8abff14774b5fb4a3e969c0354be68a43aedbcd
ZapAnton <wc3ft2@gmail.com>
2018-07-04 10:09:26 +0300

    rotational-cipher: Added template to stub file

--- exercises/rotational-cipher/src/lib.rs
+++ exercises/rotational-cipher/src/lib.rs
@@ -1,1 +1,7 @@
-
+pub fn rotate(input: &str, key: u8) -> String {
+    unimplemented!(
+        "How would input text '{}' transform when every letter is shifted using key '{}'?",
+        input,
+        key
+    );
+}
