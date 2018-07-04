1aedb76846f954efc98271818e3c5f372f467caf
ZapAnton <wc3ft2@gmail.com>
2018-07-04 14:03:38 +0300

    rotational-cipher: Changed key type from u8 to i8 in the rotate function template

--- exercises/rotational-cipher/src/lib.rs
+++ exercises/rotational-cipher/src/lib.rs
@@ -1,4 +1,4 @@
-pub fn rotate(input: &str, key: u8) -> String {
+pub fn rotate(input: &str, key: i8) -> String {
     unimplemented!(
         "How would input text '{}' transform when every letter is shifted using key '{}'?",
         input,
