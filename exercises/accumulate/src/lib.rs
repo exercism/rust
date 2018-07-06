d7ad45695ae040180d3cca1edcfc15010c5b7685
ZapAnton <wc3ft2@gmail.com>
2018-07-06 19:31:58 +0300

    accumulate: Added template to stub file

--- exercises/accumulate/src/lib.rs
+++ exercises/accumulate/src/lib.rs
@@ -1,1 +1,7 @@
+pub fn map_function(input: Vec<i32>, _function: impl Fn(i32) -> i32) -> Vec<i32> {
+    unimplemented!("Transform input vector {:?} using passed function", input);
+}
 
+pub fn map_closure(input: Vec<i32>, _closure: impl Fn(i32) -> i32) -> Vec<i32> {
+    unimplemented!("Transform input vector {:?} using passed closure", input);
+}
