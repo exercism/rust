d349cc994f5b910b596d3edbfa9bc62050c2d451
ZapAnton <wc3ft2@gmail.com>
2018-07-09 20:38:50 +0300

    triangle: Added template to stub file

--- exercises/triangle/src/lib.rs
+++ exercises/triangle/src/lib.rs
@@ -1,1 +1,19 @@
+pub struct Triangle;
 
+impl Triangle {
+    pub fn build(sides: [u64; 3]) -> Result<Triangle, ()> {
+        unimplemented!("Construct new Triangle from following sides: {:?}. Return Error if the sides are invalid.", sides);
+    }
+
+    pub fn is_equilateral(&self) -> bool {
+        unimplemented!("Determine if the Triangle is equilateral.");
+    }
+
+    pub fn is_scalene(&self) -> bool {
+        unimplemented!("Determine if the Triangle is scalene.");
+    }
+
+    pub fn is_isosceles(&self) -> bool {
+        unimplemented!("Determine if the Triangle is isosceles.");
+    }
+}
