aaad0caef28f626dd687d9afc627a8f764243641
ZapAnton <wc3ft2@gmail.com>
2018-07-13 17:48:34 +0300

    grep: Added template to stub file

--- exercises/grep/src/lib.rs
+++ exercises/grep/src/lib.rs
@@ -1,1 +1,8 @@
-
+pub fn grep(pattern: &str, flags: &[&str], files: &[&str]) -> Vec<String> {
+    unimplemented!(
+        "Search the files '{:?}' for '{}' pattern and save the matches in a vector. Your search logic should be aware of the given flags '{:?}'",
+        files,
+        pattern,
+        flags
+    );
+}
