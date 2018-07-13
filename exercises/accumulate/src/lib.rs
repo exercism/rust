78d1f295acd0a77c867a4860d8a44f2513eda244
ZapAnton <wc3ft2@gmail.com>
2018-07-13 11:11:28 +0300

    accumulate: Removed types for _closure and _function arguments from template

--- exercises/accumulate/src/lib.rs
+++ exercises/accumulate/src/lib.rs
@@ -1,7 +1,9 @@
-pub fn map_function(input: Vec<i32>, _function: impl Fn(i32) -> i32) -> Vec<i32> {
+/// What should the type of _function be?
+pub fn map_function(input: Vec<i32>, _function: ???) -> Vec<i32> {
     unimplemented!("Transform input vector {:?} using passed function", input);
 }
 
-pub fn map_closure(input: Vec<i32>, _closure: impl Fn(i32) -> i32) -> Vec<i32> {
+/// What should the type of _closure be?
+pub fn map_closure(input: Vec<i32>, _closure: ???) -> Vec<i32> {
     unimplemented!("Transform input vector {:?} using passed closure", input);
 }
