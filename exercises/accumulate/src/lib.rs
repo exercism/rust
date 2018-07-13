346b755d5db63df61655dac00a8fdda29f13e82b
Zapolsky Anton <wc3ft2@gmail.com>
2018-07-13 11:21:54 +0300

    accumulate: Added template to stub file (#571)
    * accumulate: Added template to stub file
    
    * accumulate: Removed types for _closure and _function arguments from template

--- exercises/accumulate/src/lib.rs
+++ exercises/accumulate/src/lib.rs
@@ -1,1 +1,9 @@
+/// What should the type of _function be?
+pub fn map_function(input: Vec<i32>, _function: ???) -> Vec<i32> {
+    unimplemented!("Transform input vector {:?} using passed function", input);
+}
 
+/// What should the type of _closure be?
+pub fn map_closure(input: Vec<i32>, _closure: ???) -> Vec<i32> {
+    unimplemented!("Transform input vector {:?} using passed closure", input);
+}
