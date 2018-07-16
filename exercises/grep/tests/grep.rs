d2eaad29fa753a5ec3237ce470c65b2543954372
ZapAnton <wc3ft2@gmail.com>
2018-07-16 15:31:02 +0300

    grep: Every test case now handles it's own set of files

--- exercises/grep/tests/grep.rs
+++ exercises/grep/tests/grep.rs
@@ -2,9 +2,7 @@ extern crate grep;
 
 use grep::grep;
 
-use std::{
-    fs, sync::{Once, ONCE_INIT},
-};
+use std::fs;
 
 static ILIAD_CONTENT: &'static str = "Achilles sing, O Goddess! Peleus' son;
 His wrath pernicious, who ten thousand woes
@@ -50,34 +48,41 @@ static IN_THE_WHITE_NIGHT_CONTENT: &'static str = "Белой ночью мес�
 Красный месяц, тихий шум?..
 ";
 
-struct Fixture;
+struct Fixture<'a> {
+    file_names: &'a [&'a str],
+}
 
-impl Fixture {
+impl<'a> Fixture<'a> {
+    fn new(file_names: &'a [&'a str]) -> Self {
+        Fixture { file_names }
+    }
+
     fn set_up(&self) {
-        static SETUP: Once = ONCE_INIT;
+        let file_name_content_pairs = self.file_names
+            .iter()
+            .cloned()
+            .map(|file_name| {
+                if file_name.ends_with("iliad.txt") {
+                    (file_name, ILIAD_CONTENT)
+                } else if file_name.ends_with("midsummer_night.txt") {
+                    (file_name, MIDSUMMER_NIGHT_CONTENT)
+                } else if file_name.ends_with("paradise_lost.txt") {
+                    (file_name, PARADISE_LOST_CONTENT)
+                } else {
+                    (file_name, IN_THE_WHITE_NIGHT_CONTENT)
+                }
+            })
+            .collect::<Vec<(&str, &str)>>();
 
-        SETUP.call_once(|| {
-            set_up_files(&vec![
-                ("iliad.txt", ILIAD_CONTENT),
-                ("midsummer_night.txt", MIDSUMMER_NIGHT_CONTENT),
-                ("paradise_lost.txt", PARADISE_LOST_CONTENT),
-                ("in_the_white_night.txt", IN_THE_WHITE_NIGHT_CONTENT),
-            ]);
-        });
+        set_up_files(&file_name_content_pairs);
     }
 }
 
-impl Drop for Fixture {
+impl<'a> Drop for Fixture<'a> {
     fn drop(&mut self) {
-        /*tear_down_files(&vec![
-            "iliad.txt",
-            "midsummer_night.txt",
-            "paradise_lost.txt",
-        ]);*/
+        tear_down_files(self.file_names);
     }
 }
-
-const TEST_FIXTURE: Fixture = Fixture;
 
 fn set_up_files(files: &[(&str, &str)]) {
     for (file_name, file_content) in files {
@@ -90,12 +95,14 @@ fn set_up_files(files: &[(&str, &str)]) {
 
 fn tear_down_files(files: &[&str]) {
     for file_name in files {
-        fs::remove_file(file_name).expect(&format!("Could not delete {}", file_name));
+        fs::remove_file(file_name).expect(&format!("Could not delete file '{}'", file_name));
     }
 }
 
 fn process_grep_case(pattern: &str, flags: &[&str], files: &[&str], expected: &[&str]) {
-    TEST_FIXTURE.set_up();
+    let test_fixture = Fixture::new(files);
+
+    test_fixture.set_up();
 
     let grep_result = grep(pattern, flags, files);
 
@@ -111,7 +118,7 @@ fn test_one_file_one_match_no_flags() {
 
     let flags = vec![];
 
-    let files = vec!["iliad.txt"];
+    let files = vec!["test_one_file_one_match_no_flags_iliad.txt"];
 
     let expected = vec!["Of Atreus, Agamemnon, King of men."];
 
@@ -126,7 +133,7 @@ fn test_one_file_one_match_print_line_numbers_flag() {
 
     let flags = vec!["-n"];
 
-    let files = vec!["paradise_lost.txt"];
+    let files = vec!["test_one_file_one_match_print_line_numbers_flag_paradise_lost.txt"];
 
     let expected = vec!["2:Of that Forbidden Tree, whose mortal tast"];
 
@@ -141,7 +148,7 @@ fn test_one_file_one_match_caseinsensitive_flag() {
 
     let flags = vec!["-i"];
 
-    let files = vec!["paradise_lost.txt"];
+    let files = vec!["test_one_file_one_match_caseinsensitive_flag_paradise_lost.txt"];
 
     let expected = vec!["Of that Forbidden Tree, whose mortal tast"];
 
@@ -156,9 +163,9 @@ fn test_one_file_one_match_print_file_names_flag() {
 
     let flags = vec!["-l"];
 
-    let files = vec!["paradise_lost.txt"];
+    let files = vec!["test_one_file_one_match_print_file_names_flag_paradise_lost.txt"];
 
-    let expected = vec!["paradise_lost.txt"];
+    let expected = vec!["test_one_file_one_match_print_file_names_flag_paradise_lost.txt"];
 
     process_grep_case(&pattern, &flags, &files, &expected);
 }
@@ -171,7 +178,7 @@ fn test_one_file_one_match_match_entire_lines_flag() {
 
     let flags = vec!["-x"];
 
-    let files = vec!["paradise_lost.txt"];
+    let files = vec!["test_one_file_one_match_match_entire_lines_flag_paradise_lost.txt"];
 
     let expected = vec!["With loss of Eden, till one greater Man"];
 
@@ -186,7 +193,7 @@ fn test_one_file_one_match_multiple_flags() {
 
     let flags = vec!["-x", "-i", "-n"];
 
-    let files = vec!["iliad.txt"];
+    let files = vec!["test_one_file_one_match_multiple_flags_iliad.txt"];
 
     let expected = vec!["9:Of Atreus, Agamemnon, King of men."];
 
@@ -201,7 +208,7 @@ fn test_one_file_several_matches_no_flags() {
 
     let flags = vec![];
 
-    let files = vec!["midsummer_night.txt"];
+    let files = vec!["test_one_file_several_matches_no_flags_midsummer_night.txt"];
 
     let expected = vec![
         "Nor how it may concern my modesty,",
@@ -220,7 +227,7 @@ fn test_one_file_several_matches_print_line_numbers_flag() {
 
     let flags = vec!["-n"];
 
-    let files = vec!["midsummer_night.txt"];
+    let files = vec!["test_one_file_several_matches_print_line_numbers_flag_midsummer_night.txt"];
 
     let expected = vec![
         "3:Nor how it may concern my modesty,",
@@ -239,7 +246,7 @@ fn test_one_file_several_matches_match_entire_lines_flag() {
 
     let flags = vec!["-x"];
 
-    let files = vec!["midsummer_night.txt"];
+    let files = vec!["test_one_file_several_matches_match_entire_lines_flag_midsummer_night.txt"];
 
     let expected = vec![];
 
@@ -254,7 +261,7 @@ fn test_one_file_several_matches_caseinsensitive_flag() {
 
     let flags = vec!["-i"];
 
-    let files = vec!["iliad.txt"];
+    let files = vec!["test_one_file_several_matches_caseinsensitive_flag_iliad.txt"];
 
     let expected = vec![
         "Achilles sing, O Goddess! Peleus' son;",
@@ -272,7 +279,7 @@ fn test_one_file_several_matches_inverted_flag() {
 
     let flags = vec!["-v"];
 
-    let files = vec!["paradise_lost.txt"];
+    let files = vec!["test_one_file_several_matches_inverted_flag_paradise_lost.txt"];
 
     let expected = vec![
         "Brought Death into the World, and all our woe,",
@@ -293,7 +300,7 @@ fn test_one_file_no_matches_various_flags() {
 
     let flags = vec!["-n", "-l", "-x", "-i"];
 
-    let files = vec!["iliad.txt"];
+    let files = vec!["test_one_file_no_matches_various_flags_iliad.txt"];
 
     let expected = vec![];
 
@@ -310,9 +317,14 @@ fn test_multiple_files_one_match_no_flags() {
 
     let flags = vec![];
 
-    let files = vec!["iliad.txt", "midsummer_night.txt", "paradise_lost.txt"];
+    let files = vec![
+        "test_multiple_files_one_match_no_flags_iliad.txt",
+        "test_multiple_files_one_match_no_flags_midsummer_night.txt",
+        "test_multiple_files_one_match_no_flags_paradise_lost.txt",
+    ];
 
-    let expected = vec!["iliad.txt:Of Atreus, Agamemnon, King of men."];
+    let expected =
+        vec!["test_multiple_files_one_match_no_flags_iliad.txt:Of Atreus, Agamemnon, King of men."];
 
     process_grep_case(&pattern, &flags, &files, &expected);
 }
@@ -325,12 +337,16 @@ fn test_multiple_files_several_matches_no_flags() {
 
     let flags = vec![];
 
-    let files = vec!["iliad.txt", "midsummer_night.txt", "paradise_lost.txt"];
+    let files = vec![
+        "test_multiple_files_several_matches_no_flags_iliad.txt",
+        "test_multiple_files_several_matches_no_flags_midsummer_night.txt",
+        "test_multiple_files_several_matches_no_flags_paradise_lost.txt",
+    ];
 
     let expected = vec![
-        "midsummer_night.txt:Nor how it may concern my modesty,",
-        "midsummer_night.txt:But I beseech your grace that I may know",
-        "midsummer_night.txt:The worst that may befall me in this case,",
+        "test_multiple_files_several_matches_no_flags_midsummer_night.txt:Nor how it may concern my modesty,",
+        "test_multiple_files_several_matches_no_flags_midsummer_night.txt:But I beseech your grace that I may know",
+        "test_multiple_files_several_matches_no_flags_midsummer_night.txt:The worst that may befall me in this case,",
     ];
 
     process_grep_case(&pattern, &flags, &files, &expected);
@@ -344,13 +360,17 @@ fn test_multiple_files_several_matches_print_line_numbers_flag() {
 
     let flags = vec!["-n"];
 
-    let files = vec!["iliad.txt", "midsummer_night.txt", "paradise_lost.txt"];
+    let files = vec![
+        "test_multiple_files_several_matches_print_line_numbers_flag_iliad.txt",
+        "test_multiple_files_several_matches_print_line_numbers_flag_midsummer_night.txt",
+        "test_multiple_files_several_matches_print_line_numbers_flag_paradise_lost.txt",
+    ];
 
     let expected = vec![
-        "midsummer_night.txt:5:But I beseech your grace that I may know",
-        "midsummer_night.txt:6:The worst that may befall me in this case,",
-        "paradise_lost.txt:2:Of that Forbidden Tree, whose mortal tast",
-        "paradise_lost.txt:6:Sing Heav'nly Muse, that on the secret top",
+        "test_multiple_files_several_matches_print_line_numbers_flag_midsummer_night.txt:5:But I beseech your grace that I may know",
+        "test_multiple_files_several_matches_print_line_numbers_flag_midsummer_night.txt:6:The worst that may befall me in this case,",
+        "test_multiple_files_several_matches_print_line_numbers_flag_paradise_lost.txt:2:Of that Forbidden Tree, whose mortal tast",
+        "test_multiple_files_several_matches_print_line_numbers_flag_paradise_lost.txt:6:Sing Heav'nly Muse, that on the secret top",
     ];
 
     process_grep_case(&pattern, &flags, &files, &expected);
@@ -364,9 +384,16 @@ fn test_multiple_files_one_match_print_file_names_flag() {
 
     let flags = vec!["-l"];
 
-    let files = vec!["iliad.txt", "midsummer_night.txt", "paradise_lost.txt"];
+    let files = vec![
+        "test_multiple_files_one_match_print_file_names_flag_iliad.txt",
+        "test_multiple_files_one_match_print_file_names_flag_midsummer_night.txt",
+        "test_multiple_files_one_match_print_file_names_flag_paradise_lost.txt",
+    ];
 
-    let expected = vec!["iliad.txt", "paradise_lost.txt"];
+    let expected = vec![
+        "test_multiple_files_one_match_print_file_names_flag_iliad.txt",
+        "test_multiple_files_one_match_print_file_names_flag_paradise_lost.txt",
+    ];
 
     process_grep_case(&pattern, &flags, &files, &expected);
 }
@@ -379,19 +406,23 @@ fn test_multiple_files_several_matches_caseinsensitive_flag() {
 
     let flags = vec!["-i"];
 
-    let files = vec!["iliad.txt", "midsummer_night.txt", "paradise_lost.txt"];
+    let files = vec![
+        "test_multiple_files_several_matches_caseinsensitive_flag_iliad.txt",
+        "test_multiple_files_several_matches_caseinsensitive_flag_midsummer_night.txt",
+        "test_multiple_files_several_matches_caseinsensitive_flag_paradise_lost.txt",
+    ];
 
     let expected = vec![
-        "iliad.txt:Caused to Achaia's host, sent many a soul",
-        "iliad.txt:Illustrious into Ades premature,",
-        "iliad.txt:And Heroes gave (so stood the will of Jove)",
-        "iliad.txt:To dogs and to all ravening fowls a prey,",
-        "midsummer_night.txt:I do entreat your grace to pardon me.",
-        "midsummer_night.txt:In such a presence here to plead my thoughts;",
-        "midsummer_night.txt:If I refuse to wed Demetrius.",
-        "paradise_lost.txt:Brought Death into the World, and all our woe,",
-        "paradise_lost.txt:Restore us, and regain the blissful Seat,",
-        "paradise_lost.txt:Sing Heav'nly Muse, that on the secret top",
+        "test_multiple_files_several_matches_caseinsensitive_flag_iliad.txt:Caused to Achaia's host, sent many a soul",
+        "test_multiple_files_several_matches_caseinsensitive_flag_iliad.txt:Illustrious into Ades premature,",
+        "test_multiple_files_several_matches_caseinsensitive_flag_iliad.txt:And Heroes gave (so stood the will of Jove)",
+        "test_multiple_files_several_matches_caseinsensitive_flag_iliad.txt:To dogs and to all ravening fowls a prey,",
+        "test_multiple_files_several_matches_caseinsensitive_flag_midsummer_night.txt:I do entreat your grace to pardon me.",
+        "test_multiple_files_several_matches_caseinsensitive_flag_midsummer_night.txt:In such a presence here to plead my thoughts;",
+        "test_multiple_files_several_matches_caseinsensitive_flag_midsummer_night.txt:If I refuse to wed Demetrius.",
+        "test_multiple_files_several_matches_caseinsensitive_flag_paradise_lost.txt:Brought Death into the World, and all our woe,",
+        "test_multiple_files_several_matches_caseinsensitive_flag_paradise_lost.txt:Restore us, and regain the blissful Seat,",
+        "test_multiple_files_several_matches_caseinsensitive_flag_paradise_lost.txt:Sing Heav'nly Muse, that on the secret top",
     ];
 
     process_grep_case(&pattern, &flags, &files, &expected);
@@ -404,13 +435,16 @@ fn test_multiple_files_several_matches_caseinsensitive_flag_utf8() {
 
     let flags = vec!["-i"];
 
-    let files = vec!["in_the_white_night.txt", "paradise_lost.txt"];
+    let files = vec![
+        "test_multiple_files_several_matches_caseinsensitive_flag_utf8_in_the_white_night.txt",
+        "test_multiple_files_several_matches_caseinsensitive_flag_utf8_paradise_lost.txt",
+    ];
 
     let expected = vec![
-        "in_the_white_night.txt:Выплывает в синеве.",
-        "in_the_white_night.txt:Отражается в Неве.",
-        "in_the_white_night.txt:Мне провидится и снится",
-        "in_the_white_night.txt:В вас ли доброе таится,",
+        "test_multiple_files_several_matches_caseinsensitive_flag_utf8_in_the_white_night.txt:Выплывает в синеве.",
+        "test_multiple_files_several_matches_caseinsensitive_flag_utf8_in_the_white_night.txt:Отражается в Неве.",
+        "test_multiple_files_several_matches_caseinsensitive_flag_utf8_in_the_white_night.txt:Мне провидится и снится",
+        "test_multiple_files_several_matches_caseinsensitive_flag_utf8_in_the_white_night.txt:В вас ли доброе таится,",
     ];
 
     process_grep_case(&pattern, &flags, &files, &expected);
@@ -424,12 +458,16 @@ fn test_multiple_files_several_matches_inverted_flag() {
 
     let flags = vec!["-v"];
 
-    let files = vec!["iliad.txt", "midsummer_night.txt", "paradise_lost.txt"];
+    let files = vec![
+        "test_multiple_files_several_matches_inverted_flag_iliad.txt",
+        "test_multiple_files_several_matches_inverted_flag_midsummer_night.txt",
+        "test_multiple_files_several_matches_inverted_flag_paradise_lost.txt",
+    ];
 
     let expected = vec![
-        "iliad.txt:Achilles sing, O Goddess! Peleus' son;",
-        "iliad.txt:The noble Chief Achilles from the son",
-        "midsummer_night.txt:If I refuse to wed Demetrius.",
+        "test_multiple_files_several_matches_inverted_flag_iliad.txt:Achilles sing, O Goddess! Peleus' son;",
+        "test_multiple_files_several_matches_inverted_flag_iliad.txt:The noble Chief Achilles from the son",
+        "test_multiple_files_several_matches_inverted_flag_midsummer_night.txt:If I refuse to wed Demetrius.",
     ];
 
     process_grep_case(&pattern, &flags, &files, &expected);
@@ -443,9 +481,13 @@ fn test_multiple_files_one_match_match_entire_lines_flag() {
 
     let flags = vec!["-x"];
 
-    let files = vec!["iliad.txt", "midsummer_night.txt", "paradise_lost.txt"];
+    let files = vec![
+        "test_multiple_files_one_match_match_entire_lines_flag_iliad.txt",
+        "test_multiple_files_one_match_match_entire_lines_flag_midsummer_night.txt",
+        "test_multiple_files_one_match_match_entire_lines_flag_paradise_lost.txt",
+    ];
 
-    let expected = vec!["midsummer_night.txt:But I beseech your grace that I may know"];
+    let expected = vec!["test_multiple_files_one_match_match_entire_lines_flag_midsummer_night.txt:But I beseech your grace that I may know"];
 
     process_grep_case(&pattern, &flags, &files, &expected);
 }
@@ -458,9 +500,13 @@ fn test_multiple_files_one_match_multiple_flags() {
 
     let flags = vec!["-n", "-i", "-x"];
 
-    let files = vec!["iliad.txt", "midsummer_night.txt", "paradise_lost.txt"];
+    let files = vec![
+        "test_multiple_files_one_match_multiple_flags_iliad.txt",
+        "test_multiple_files_one_match_multiple_flags_midsummer_night.txt",
+        "test_multiple_files_one_match_multiple_flags_paradise_lost.txt",
+    ];
 
-    let expected = vec!["paradise_lost.txt:4:With loss of Eden, till one greater Man"];
+    let expected = vec!["test_multiple_files_one_match_multiple_flags_paradise_lost.txt:4:With loss of Eden, till one greater Man"];
 
     process_grep_case(&pattern, &flags, &files, &expected);
 }
@@ -473,7 +519,11 @@ fn test_multiple_files_no_matches_various_flags() {
 
     let flags = vec!["-n", "-i", "-x", "-l"];
 
-    let files = vec!["iliad.txt", "midsummer_night.txt", "paradise_lost.txt"];
+    let files = vec![
+        "test_multiple_files_no_matches_various_flags_iliad.txt",
+        "test_multiple_files_no_matches_various_flags_midsummer_night.txt",
+        "test_multiple_files_no_matches_various_flags_paradise_lost.txt",
+    ];
 
     let expected = vec![];
 
