20dea118d86159a9c988fed341d6f13a05cb9ae0
ZapAnton <wc3ft2@gmail.com>
2018-07-17 19:42:18 +0300

    roman-numerals: Added template to the stub file

--- exercises/roman-numerals/src/lib.rs
+++ exercises/roman-numerals/src/lib.rs
@@ -1,1 +1,15 @@
+use std::fmt::{Display, Formatter, Result};
 
+pub struct Roman;
+
+impl Display for Roman {
+    fn fmt(&self, _f: &mut Formatter) -> Result {
+        unimplemented!("Return a roman-numeral string representation of the Roman object");
+    }
+}
+
+impl From<i32> for Roman {
+    fn from(num: i32) -> Self {
+        unimplemented!("Construct a Roman object from the '{}' number", num);
+    }
+}
