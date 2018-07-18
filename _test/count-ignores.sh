95692d78dfbdbaeb9740c41c255ac52d03b43f64
Peter Goodspeed-Niklaus <coriolinus@users.noreply.github.com>
2018-07-18 14:31:31 +0200

    Update count-ignores.sh to find attributes which aren't alone on their line

--- _test/count-ignores.sh
+++ _test/count-ignores.sh
@@ -9,8 +9,8 @@ for e in $repo/exercises/*; do
    fi
    if [ -d "$e/tests" ]; then
       for t in $e/tests/*.rs; do
-        tests=$(grep "^\s*\#\[test\]" $t | wc -l | tr -d '[:space:]')
-        ignores=$(grep "^\s*\#\[ignore\]" $t | wc -l | tr -d '[:space:]')
+        tests=$(grep  "\#\[test\]" $t | wc -l | tr -d '[:space:]')
+        ignores=$(grep "\#\[ignore\]" $t | wc -l | tr -d '[:space:]')
         want_ignores=$(expr $tests - 1)
         if [ "$ignores" != "$want_ignores" ]; then
           echo "\033[1;31m$t: Has $tests tests and $ignores ignores (should be $want_ignores)\033[0m"
