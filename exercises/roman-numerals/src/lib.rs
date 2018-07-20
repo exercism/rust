4b58a3d2b73b96e63004364afc7f6107da48c74e
Zapolsky Anton <wc3ft2@gmail.com>
2018-07-20 22:48:17 +0300

    roman-numerals: Added template to the stub file (#579)
    * roman-numerals: Added template to the stub file
    
    * roman-numerals: Replaced i32 type with u32 type in 'From' trait template implementation

--- exercises/roman-numerals/src/lib.rs
+++ exercises/roman-numerals/src/lib.rs
@@ -1,1 +1,15 @@
+use std::fmt::{Display, Formatter, Result};
 
+pub struct Roman;
+
+impl Display for Roman {
+    fn fmt(&self, _f: &mut Formatter) -> Result {
+        unimplemented!("Return a roman-numeral string representation of the Roman object");
+    }
+}
+
+impl From<u32> for Roman {
+    fn from(num: u32) -> Self {
+        unimplemented!("Construct a Roman object from the '{}' number", num);
+    }
+}
