a5fb908f9cddcec940c418d49ee71445b551b57b
ZapAnton <wc3ft2@gmail.com>
2018-07-21 11:35:16 +0300

    binary-search: Fixed typo in the stub template

--- exercises/binary-search/src/lib.rs
+++ exercises/binary-search/src/lib.rs
@@ -1,6 +1,6 @@
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
     unimplemented!(
-        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return it's index.",
+        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
         key,
         array
     );
