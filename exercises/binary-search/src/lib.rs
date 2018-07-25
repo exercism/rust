341e32d705333cecacbada12868c8969c7d3ec91
Zapolsky Anton <wc3ft2@gmail.com>
2018-07-26 00:48:52 +0300

    binary-search: Added template to the stub file (#581)
    * binary-search: Added template to the stub file
    
    * binary-search: Fixed typo in the stub template

--- exercises/binary-search/src/lib.rs
+++ exercises/binary-search/src/lib.rs
@@ -1,1 +1,7 @@
-
+pub fn find(array: &[i32], key: i32) -> Option<usize> {
+    unimplemented!(
+        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
+        key,
+        array
+    );
+}
