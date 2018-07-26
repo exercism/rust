091b5f301b42bdfb8d2d112ee67b0427c603a9c9
ZapAnton <wc3ft2@gmail.com>
2018-07-26 11:50:18 +0300

    queen-attack: Added template to the stub file

--- exercises/queen-attack/src/lib.rs
+++ exercises/queen-attack/src/lib.rs
@@ -1,1 +1,19 @@
+pub struct ChessPosition;
 
+pub struct Queen;
+
+impl ChessPosition {
+    pub fn new(rank: i32, file: i32) -> Result<Self, i32> {
+        unimplemented!("Construct a ChessPosition struct, given the following rank, file: ({}, {}). If the position is invalid return error that contains wrong rank/file.", rank, file);
+    }
+}
+
+impl Queen {
+    pub fn new(_position: ChessPosition) -> Self {
+        unimplemented!("Given the chess position, construct a Queen struct.");
+    }
+
+    pub fn can_attack(&self, _other: &Queen) -> bool {
+        unimplemented!("Determine if this Queen can attack the other Queen");
+    }
+}
