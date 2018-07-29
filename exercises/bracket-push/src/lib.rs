a138defd60b2abd5419d0e025385628018dce3c1
ZapAnton <wc3ft2@gmail.com>
2018-07-29 15:34:55 +0300

    bracket-push: Replaced 'static lifetime with generic lifetime

--- exercises/bracket-push/src/lib.rs
+++ exercises/bracket-push/src/lib.rs
@@ -1,6 +1,6 @@
 pub struct Brackets;
 
-impl From<&'static str> for Brackets {
+impl<'a> From<&'a str> for Brackets {
     fn from(input: &str) -> Self {
         unimplemented!("From the '{}' input construct a new Brackets struct", input);
     }
