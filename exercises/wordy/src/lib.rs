3907ed7309ce221c73bbc2a99ba8cef641098bcf
ZapAnton <wc3ft2@gmail.com>
2018-07-31 18:11:44 +0300

    wordy: Added template to the stub file

--- exercises/wordy/src/lib.rs
+++ exercises/wordy/src/lib.rs
@@ -1,1 +1,16 @@
+pub struct WordProblem;
 
+impl WordProblem {
+    pub fn new(command: &str) -> Self {
+        unimplemented!(
+            "From the '{}' command construct a new WordProblem struct.",
+            command
+        );
+    }
+
+    pub fn answer(&self) -> Result<i32, String> {
+        unimplemented!(
+            "Return the result of the transmitted command, or an error, if the command is invalid."
+        );
+    }
+}
