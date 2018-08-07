f9d0afadbc12cf41432582e011bc9b714e916a9d
ZapAnton <wc3ft2@gmail.com>
2018-08-07 11:53:51 +0300

    parallel-letter-frequency: Added template to the stub file

--- exercises/parallel-letter-frequency/src/lib.rs
+++ exercises/parallel-letter-frequency/src/lib.rs
@@ -1,1 +1,12 @@
+use std::collections::HashMap;
 
+pub fn frequency(input: &[&str], worker_count: i32) -> HashMap<char, u32> {
+    unimplemented!(
+        "Count the frequency of letters in the given input '{:?}'. Ensure that you are using {} to process the input.",
+        input,
+        match worker_count {
+            1 => format!("1 worker"),
+            _ => format!("{} workers", worker_count),
+        }
+    );
+}
