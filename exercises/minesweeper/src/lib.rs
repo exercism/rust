46c7f261b2e2252b550a9432a9291bc44cf3b1b6
ZapAnton <wc3ft2@gmail.com>
2018-08-08 12:09:08 +0300

    minesweeper: Added template to the stub file

--- exercises/minesweeper/src/lib.rs
+++ exercises/minesweeper/src/lib.rs
@@ -1,1 +1,3 @@
-
+pub fn annotate(minefield: &[&str]) -> Vec<String> {
+    unimplemented!("\nAnnotate the given minefield with the number of surrounding mines in each square (blank if there are no surrounding mines):\n{:#?}\n", minefield);
+}
