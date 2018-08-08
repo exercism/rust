5c2d6a568d6542c087e7388d600fd2450951111b
ZapAnton <wc3ft2@gmail.com>
2018-08-08 15:07:35 +0300

    luhn-trait: Added template to the stub file

--- exercises/luhn-trait/src/lib.rs
+++ exercises/luhn-trait/src/lib.rs
@@ -1,1 +1,14 @@
+pub trait Luhn {
+    fn valid_luhn(&self) -> bool;
+}
 
+/// Here is the example of how to implement custom Luhn trait
+/// for the &str type. Naturally, you can implement this trait
+/// by hand for the every other type presented in the test suite,
+/// but you solution will fail if a new type is presented.
+/// Perhaps there exists a better solution for a problem?
+impl Luhn for &'static str {
+    fn valid_luhn(&self) -> bool {
+        unimplemented!("Determine if '{}' is a valid credit card number.", self);
+    }
+}
