6e448cc99cf3dfdf606eb4af9fd14e3e7f1e49f0
ZapAnton <wc3ft2@gmail.com>
2018-08-08 12:21:18 +0300

    sublist: Added template to the stub file

--- exercises/sublist/src/lib.rs
+++ exercises/sublist/src/lib.rs
@@ -1,1 +1,13 @@
+use std::fmt::Debug;
 
+#[derive(Debug, PartialEq)]
+pub enum Comparison {
+    Equal,
+    Sublist,
+    Superlist,
+    Unequal,
+}
+
+pub fn sublist<T: Debug>(first_list: &[T], second_list: &[T]) -> Comparison {
+    unimplemented!("Determine if the first list '{:?}' is equal to, sublist of, superlist of or unequal to the second list '{:?}'", first_list, second_list);
+}
