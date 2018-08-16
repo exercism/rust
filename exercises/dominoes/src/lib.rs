df49ac0908d72fa4e3ecca5c3f8d2d96bd19a06f
Zapolsky Anton <wc3ft2@gmail.com>
2018-08-16 22:45:22 +0300

    dominoes: Add template to the stub file (#611)

--- exercises/dominoes/src/lib.rs
+++ exercises/dominoes/src/lib.rs
@@ -1,1 +1,3 @@
-
+pub fn chain(input: &[(usize, usize)]) -> Option<Vec<(usize, usize)>> {
+    unimplemented!("From the given input '{:?}' construct a proper dominoes chain or return None if it is not possible.", input);
+}
