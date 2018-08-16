8366b543d405b9639b6adefadf74db35e251278e
ZapAnton <wc3ft2@gmail.com>
2018-08-16 12:49:50 +0300

    luhn-from: Fixed typos in the stub template

--- exercises/luhn-from/src/lib.rs
+++ exercises/luhn-from/src/lib.rs
@@ -9,8 +9,8 @@ impl Luhn {
 /// Here is the example of how the From trait could be implemented
 /// for the &str type. Naturally, you can implement this trait
 /// by hand for the every other type presented in the test suite,
-/// but you solution will fail if a new type is presented.
-/// Perhaps there exists a better solution for a problem?
+/// but your solution will fail if a new type is presented.
+/// Perhaps there exists a better solution for this problem?
 impl From<&'static str> for Luhn {
     fn from(input: &'static str) -> Self {
         unimplemented!("From the given input '{}' create a new Luhn struct.", input);
