05cade849fbe443448576e0614e9d14214fa6e74
ZapAnton <wc3ft2@gmail.com>
2018-08-16 23:52:59 +0300

    minesweeper: Rephrased stub template message for clearer wording

--- exercises/minesweeper/src/lib.rs
+++ exercises/minesweeper/src/lib.rs
@@ -1,3 +1,3 @@
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    unimplemented!("\nAnnotate the given minefield with the number of surrounding mines in each square (blank if there are no surrounding mines):\n{:#?}\n", minefield);
+    unimplemented!("\nAnnotate each square of the given minefield with the number of mines that surround said square (blank if there are no surrounding mines):\n{:#?}\n", minefield);
 }
