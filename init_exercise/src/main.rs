14520c35e21d71355f499a92339dfa3d5b781612
ZapAnton <wc3ft2@gmail.com>
2018-08-29 18:35:50 +0300

    init_exercise: Added command-line arguments via clap

--- init_exercise/src/main.rs
+++ init_exercise/src/main.rs
@@ -1,3 +1,25 @@
+extern crate clap;
+
+use clap::{App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let _matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .subcommand(
+            SubCommand::with_name("generate")
+                .about("Generates new exercise")
+                .arg(Arg::with_name("exercise_name").help("The name of the generated exercise"))
+                .arg(Arg::with_name("dont_update_config").long("dont-update-config").short("d").help(
+                    "If set, the command will not update config.json after generating the exercise",
+                ))
+                .arg(Arg::with_name("use_maplit").long("use-maplit").short("m").help("Use the maplit crate to improve the readability of the generated test suite")),
+        )
+        .subcommand(
+            SubCommand::with_name("update")
+                .about("Updates the specified exercise")
+                .arg(Arg::with_name("exercise_name").help("The name of the updated exercise")),
+        )
+        .get_matches();
 }
