311fe16e8c854edccaf744d007e950cf6354a243
ZapAnton <wc3ft2@gmail.com>
2018-08-30 11:09:17 +0300

    init_exercise: Replaced the dont-update-config flag with the no-configure flag

--- init_exercise/src/main.rs
+++ init_exercise/src/main.rs
@@ -11,15 +11,18 @@ fn main() {
             SubCommand::with_name("generate")
                 .about("Generates new exercise")
                 .arg(Arg::with_name("exercise_name").help("The name of the generated exercise"))
-                .arg(Arg::with_name("dont_update_config").long("dont-update-config").short("d").help(
-                    "If set, the command will not update config.json after generating the exercise",
+                .arg(Arg::with_name("no_configure").long("no-configure").short("n").help(
+                    "If set, the command will not edit config.json after generating the exercise",
                 ))
                 .arg(Arg::with_name("use_maplit").long("use-maplit").short("m").help("Use the maplit crate to improve the readability of the generated test suite")),
         )
         .subcommand(
             SubCommand::with_name("update")
                 .about("Updates the specified exercise")
-                .arg(Arg::with_name("exercise_name").help("The name of the updated exercise")),
+                .arg(Arg::with_name("exercise_name").help("The name of the updated exercise"))
+                .arg(Arg::with_name("no_configure").long("no-configure").short("n").help(
+                    "If set, the command will not edit config.json after updating the exercise",
+                ))
         )
         .subcommand(
             SubCommand::with_name("configure")
