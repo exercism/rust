5eeddc560a827b324990cbd2ede3a6998ea894de
Zapolsky Anton <wc3ft2@gmail.com>
2018-08-30 21:59:09 +0300

    sublist: Added template to the stub file (#610)
    * sublist: Added template to the stub file
    
    * sublist: Removed the Debug trait bound from the stub file template
    
    * sublist: Added PartialEq trait bound in the stub file template

--- exercises/sublist/src/lib.rs
+++ exercises/sublist/src/lib.rs
@@ -1,1 +1,11 @@
+#[derive(Debug, PartialEq)]
+pub enum Comparison {
+    Equal,
+    Sublist,
+    Superlist,
+    Unequal,
+}
 
+pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
+    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+}
