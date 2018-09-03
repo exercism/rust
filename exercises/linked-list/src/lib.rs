3f62d63250195c0b8a83d35acd62b9b7296bad01
Emerentius <emerentius@arcor.de>
2018-09-03 07:50:55 +0200

    linked-list: extend apis
    len, iter, clone, seeking forward and backwards,
    insert list before current position in cursor

--- exercises/linked-list/src/lib.rs
+++ exercises/linked-list/src/lib.rs
@@ -153,6 +153,10 @@ impl<T> LinkedList<T> {
         }
     }
 
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
     fn _insert_first(&mut self, element: T) {
         let new_node = Node::new_linkless(element);
         self.head = Some(new_node);
@@ -220,6 +224,18 @@ impl<T> LinkedList<T> {
             list: self,
         }
     }
+
+    unsafe fn immutable_cursor_tail(&self) -> Cursor<T> {
+        let list = &mut *(self as *const LinkedList<T> as *mut LinkedList<T>);
+        list.cursor_tail()
+    }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter {
+            first: true,
+            cursor: unsafe { self.immutable_cursor_tail() },
+        }
+    }
 }
 
 impl<T> Drop for LinkedList<T> {
@@ -237,20 +253,59 @@ impl<T> Drop for LinkedList<T> {
     }
 }
 
+impl<T: Clone> Clone for LinkedList<T> {
+    fn clone(&self) -> Self {
+        let mut new_list = LinkedList::new();
+
+        // TODO: Replace with immutable iter
+        let mut cursor = unsafe { self.immutable_cursor_tail() };
+
+        if let Some(first) = cursor.peek() {
+            new_list.push_back(first.clone());
+        }
+        while let Some(next) = cursor.next() {
+            new_list.push_back(next.clone());
+        }
+        new_list
+    }
+}
+
+pub struct Iter<'a, T: 'a> {
+    first: bool,
+    cursor: Cursor<'a, T>,
+}
+
+impl<'a, T: 'a> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.first {
+            self.first = false;
+            self.cursor.peek()
+        } else {
+            self.cursor.next().map(|r| &*r)
+        }
+    }
+}
+
 impl<'a, T: 'a> Cursor<'a, T> {
-    pub fn peek(&self) -> Option<&T> {
+    // FIXME: I'm pretty certain this lifetime is UNSOUND
+    //        hacked only to get Iter working, temporarily
+    pub fn peek(&self) -> Option<&'a T> {
         unsafe {
-            self.node.as_ref().map(|node| &node.as_ref().element)
+            self.node.map(|node| &(*node.as_ptr()).element)
         }
     }
 
     pub fn peek_mut(&mut self) -> Option<&mut T> {
         unsafe {
-            self.node.as_mut().map(|node| &mut node.as_mut().element)
+            self.node.map(|node| &mut (*node.as_ptr()).element)
         }
     }
 
-    pub fn next(&mut self) -> Option<&mut T> {
+    // FIXME: I'm pretty certain this lifetime is UNSOUND
+    //        hacked only to get Iter working, temporarily
+    pub fn next(&mut self) -> Option<&'a mut T> {
         unsafe {
             if let Some(next) = self.node.as_mut()?.as_mut().next {
                 self.node = Some(next);
@@ -270,6 +325,31 @@ impl<'a, T: 'a> Cursor<'a, T> {
             } else {
                 None
             }
+        }
+    }
+
+    // FIXME: return nothing
+    pub fn seek_forward(&mut self, n: usize) -> Option<()> {
+        unsafe {
+            let mut current_node = &*self.node?.as_ptr();
+
+            for _ in 0..n {
+                let next_node = current_node.next?;
+                current_node = &*next_node.as_ptr();
+                self.node = Some(next_node);
+            }
+            Some(())
+        }
+    }
+
+    pub fn seek_backward(&mut self, n: usize) -> Option<()> {
+        unsafe {
+            let mut current_node = &*self.node?.as_ptr();
+
+            for _ in 0..n {
+                current_node = &*current_node.prev?.as_ptr();
+            }
+            Some(())
         }
     }
 
@@ -326,39 +406,56 @@ impl<'a, T: 'a> Cursor<'a, T> {
         self.list.len += 1;
     }
 
+    fn _insert_list_on_empty_list(&mut self, other: &mut LinkedList<T>) {
+        debug_assert!(self.list.len == 0);
+        std::mem::swap(self.list, other);
+        self.node = self.list.tail;
+    }
+
     pub fn insert_list_after(&mut self, other: &mut LinkedList<T>) {
         unsafe {
             let mut cursor_node = match self.node {
                 Some(node) => node,
-                None => { // own list empty,
-                    debug_assert!(self.list.len == 0);
-                    std::mem::swap(self.list, other);
-                    self.node = self.list.tail;
+                None => {
+                    self._insert_list_on_empty_list(other);
                     return;
                 }
             };
 
-            if let (Some(head), Some(tail)) = (other.head, other.tail) {
-                // first delete from other list, then insert into own
-                // no duplicates if we're interrupted (by panic from another thread)
-                // this list may still be in an inconsistent state (wrong length, leaked data)
-                // but there'll be no double free
-                // TODO: Is the above too paranoid? Do we have more guarantees for panics?
-                other.head = None;
-                other.tail = None;
-                let len = other.len;
-                other.len = 0;
-
+            if let LinkedList { head: Some(head), tail: Some(tail), len, .. } = *other {
+                debug_assert!(head.as_ref().next.is_none());
                 debug_assert!(tail.as_ref().prev.is_none());
-                NodePtr::link(cursor_node, tail);
+                std::ptr::write(other, LinkedList::new()); // empty without dropping
 
-                if let Some(mut next) = cursor_node.as_mut().next {
-                    debug_assert!(head.as_ref().next.is_none());
+                if let Some(next) = cursor_node.as_mut().next {
                     NodePtr::link(head, next);
                 }
+                NodePtr::link(cursor_node, tail);
+                self.list.len += len; // may overflow
+            }
+        }
+    }
 
-                // FIXME: what do on overflow?
-                self.list.len += len;
+    pub fn insert_list_before(&mut self, other: &mut LinkedList<T>) {
+        unsafe {
+            let mut cursor_node = match self.node {
+                Some(node) => node,
+                None => {
+                    self._insert_list_on_empty_list(other);
+                    return;
+                }
+            };
+
+            if let LinkedList { head: Some(head), tail: Some(tail), len, .. } = *other {
+                debug_assert!(head.as_ref().next.is_none());
+                debug_assert!(tail.as_ref().prev.is_none());
+                std::ptr::write(other, LinkedList::new()); // empty without dropping
+
+                if let Some(prev) = cursor_node.as_mut().prev {
+                    NodePtr::link(prev, tail);
+                }
+                NodePtr::link(head, cursor_node);
+                self.list.len += len; // may overflow
             }
         }
     }
