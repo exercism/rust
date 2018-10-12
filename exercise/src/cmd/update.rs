910625fdd26780bd8b8d42a5808b647e4f0e27b7
ZapAnton <wc3ft2@gmail.com>
2018-10-12 22:31:05 +0300

    exercise: Added check if exercise exists for the update subcommand

--- exercise/src/cmd/update.rs
+++ exercise/src/cmd/update.rs
@@ -1,1 +1,30 @@
-pub fn update_exercise(_exercise_name: &str) {}
+use std::path::Path;
+use utils;
+
+fn exercise_exists(exercise_name: &str) -> bool {
+    let track_root = utils::get_track_root();
+
+    let exercises_path = Path::new(&track_root).join("exercises");
+
+    for entry in exercises_path
+        .read_dir()
+        .expect("Failed to read 'exercises' dir")
+    {
+        if let Ok(entry) = entry {
+            if entry.file_type().unwrap().is_dir() && entry.file_name() == exercise_name {
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
+pub fn update_exercise(exercise_name: &str) {
+    if !exercise_exists(exercise_name) {
+        panic!(
+            "Exercise with the name '{}' does not exists. Aborting",
+            exercise_name
+        );
+    }
+}
