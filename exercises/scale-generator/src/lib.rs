091ef0aa822e67ac7f2f62bd78070a919dd9bf53
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-10-12 10:14:51 +0200

    remove stub dependency on failure crate

--- exercises/scale-generator/src/lib.rs
+++ exercises/scale-generator/src/lib.rs
@@ -1,5 +1,15 @@
-extern crate failure;
-use failure::Error;
+// You should change this.
+//
+// Depending on your implementation, there are a variety of potential errors
+// which might occur. They aren't checked by the test suite in order to
+// allow the greatest freedom of implementation, but real libraries should
+// provide useful, descriptive errors so that downstream code can react
+// appropriately.
+//
+// One common idiom is to define an Error enum which wraps all potential
+// errors. Another common idiom is to use a helper type such as failure::Error
+// which does more or less the same thing but automatically.
+pub type Error = ();
 
 pub struct Scale;
 
