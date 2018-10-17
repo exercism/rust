b829ce21350d4eee906e348ed8092a671f00c283
Peter Goodspeed-Niklaus <coriolinus@users.noreply.github.com>
2018-10-17 22:47:27 +0200

    Add scale generator (#667)
    * scale-generator: WIP add first-draft implementation
    
    * scale-generator: WIP extract API from example implementation
    
    * scale-generator: add to config.json
    
    * remove stub dependency on failure crate
    
    * remove unnecessary slice operations
    
    * update property names in handler doccomments

---
+++ exercises/scale-generator/src/lib.rs
@@ -0,0 +1,32 @@
+// You should change this.
+//
+// Depending on your implementation, there are a variety of potential errors
+// which might occur. They aren't checked by the test suite in order to
+// allow the greatest freedom of implementation, but real libraries should
+// provide useful, descriptive errors so that downstream code can react
+// appropriately.
+//
+// One common idiom is to define an Error enum which wraps all potential
+// errors. Another common idiom is to use a helper type such as failure::Error
+// which does more or less the same thing but automatically.
+pub type Error = ();
+
+pub struct Scale;
+
+impl Scale {
+    pub fn new(tonic: &str, intervals: &str) -> Result<Scale, Error> {
+        unimplemented!(
+            "Construct a new scale with tonic {} and intervals {}",
+            tonic,
+            intervals
+        )
+    }
+
+    pub fn chromatic(tonic: &str) -> Result<Scale, Error> {
+        unimplemented!("Construct a new chromatic scale with tonic {}", tonic)
+    }
+
+    pub fn enumerate(&self) -> Vec<String> {
+        unimplemented!()
+    }
+}
