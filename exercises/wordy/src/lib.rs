638262d89db9aebf147a780428e7b49d90982d87
Peter Tseng <petertseng@users.noreply.github.com>
2018-11-04 18:27:00 +0000

    wordy: Move `answer` to top level (#703)
    The previous API of `WordProblem::new(c).answer()` didn't lend itself
    well to failing fast, because we should be able to know at the time of
    `new` whether the command is erroneous, instead of having to wait for
    `answer`.
    
    Having the function that parses the command immediately return the
    `Option` solves this problem.
    
    Closes https://github.com/exercism/rust/issues/644

--- exercises/wordy/src/lib.rs
+++ exercises/wordy/src/lib.rs
@@ -1,16 +1,8 @@
 pub struct WordProblem;
 
-impl WordProblem {
-    pub fn new(command: &str) -> Self {
-        unimplemented!(
-            "From the '{}' command construct a new WordProblem struct.",
-            command
-        );
-    }
-
-    pub fn answer(&self) -> Option<i32> {
-        unimplemented!(
-            "Return the result of the transmitted command or None, if the command is invalid."
-        );
-    }
+pub fn answer(command: &str) -> Option<i32> {
+    unimplemented!(
+        "Return the result of the command '{}' or None, if the command is invalid.",
+        command
+    );
 }
