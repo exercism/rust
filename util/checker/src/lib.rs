72f5bcc999276a22a28573c2e78685909d716d6b
ZapAnton <wc3ft2@gmail.com>
2018-11-04 17:42:02 +0300

    checker: Added get_all_exercises function

--- util/checker/src/lib.rs
+++ util/checker/src/lib.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::{path::Path, process::Command};
 
 fn get_track_root() -> String {
     let rev_parse_output = Command::new("git")
@@ -10,6 +10,17 @@ fn get_track_root() -> String {
         .expect("Failed to convert 'git rev-parse' output into UTF-8 String")
         .trim()
         .to_string()
+}
+
+pub fn get_all_exercises() -> Vec<String> {
+    let exercises_path = Path::new(&get_track_root()).join("exercises");
+
+    exercises_path
+        .read_dir()
+        .unwrap_or_else(|_| panic!("Failed to read {:?} directory", &exercises_path))
+        .filter(|entry| entry.is_ok() && entry.as_ref().unwrap().path().is_dir())
+        .map(|entry| entry.unwrap().path().to_str().unwrap().to_string())
+        .collect()
 }
 
 pub fn get_current_branch_name() -> String {
--- util/checker/src/main.rs
+++ util/checker/src/main.rs
@@ -2,9 +2,7 @@ fn main() {
     let branch_name = checker::get_current_branch_name();
 
     let _modified_exercises: Vec<String> = match branch_name.as_ref() {
-        "master" => vec![],
+        "master" => checker::get_all_exercises(),
         _ => checker::get_modified_exercises(),
     };
-
-
 }
