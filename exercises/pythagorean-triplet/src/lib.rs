62a3e45178b2c2110badafcc41f6a5d82291d5ea
ZapAnton <wc3ft2@gmail.com>
2018-11-12 23:59:48 +0300

    pythagorean-triplet: Added the unimplemented! message in the stub file

--- exercises/pythagorean-triplet/src/lib.rs
+++ exercises/pythagorean-triplet/src/lib.rs
@@ -1,3 +1,3 @@
 pub fn find(sum: u32) -> Option<Vec<[u32; 3]>> {
-    unimplemented!();
+    unimplemented!("Given the sum {}, return all possible Pythagorean triplets, which produce the said sum, or None if there are no such triplets. Note that you are expected to return triplets in [a, b, c] order, where a < b < c", sum);
 }
