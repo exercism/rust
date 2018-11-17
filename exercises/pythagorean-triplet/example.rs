e66cfcf8c3a9d12386ed1084055a8753abe0e371
Peter Tseng <pht24@cornell.edu>
2018-11-17 17:35:39 +0000

    pythagorean-triplet: Iterate only to limits of `a < b < c`
    The tests run in debug mode.
    (We could make them run in `--release` mode, but I've arbitrarily
    decided it's not worth it yet)
    
    That take ~16 seconds on my machine (this includes the time taken to
    compile).
    
    We can get some quick wins by only iterating to the limits made possible
    by `a < b < c`.
    For example, if `a >= (a + b + c) / 3`, then `b` and `c` cannot both be
    larger than `a`.
    Similarly for `b` and `b < c`, given a fixed `a`.
    
    Cuts down to ~6 seconds on my machine.

--- exercises/pythagorean-triplet/example.rs
+++ exercises/pythagorean-triplet/example.rs
@@ -3,8 +3,8 @@ use std::collections::HashSet;
 pub fn find(sum: u32) -> HashSet<[u32; 3]> {
     let mut triplets = HashSet::new();
 
-    for a in 1..sum {
-        for b in (a + 1)..(sum - a) {
+    for a in 1..(sum / 3) {
+        for b in (a + 1)..=((sum - a) / 2) {
             let c = sum - (a + b);
 
             if a * a + b * b == c * c {
