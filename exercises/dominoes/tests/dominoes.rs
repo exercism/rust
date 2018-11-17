45e59ddea91b5f1bc00717814bafc8f79b79ddd3
Peter Tseng <pht24@cornell.edu>
2018-11-17 02:20:57 +0000

    dominoes: Pass value, not ref, to `normalize`
    Domino is `copy` so apparently this is better.
    
    Clippy suggested:
    warning: this argument is passed by reference, but would be more efficient if passed by value
    https://travis-ci.org/exercism/rust/builds/456215053
    
    Also as a direct consequence removes the reason for a different warning:
    warning: you don't need to add `&` to all patterns

--- exercises/dominoes/tests/dominoes.rs
+++ exercises/dominoes/tests/dominoes.rs
@@ -14,10 +14,10 @@ enum CheckResult {
     DominoMismatch(Vec<Domino>), // different dominoes are used in input and output
 }
 
-fn normalize(d: &Domino) -> Domino {
+fn normalize(d: Domino) -> Domino {
     match d {
-        &(m, n) if m > n => (n, m),
-        &(m, n) => (m, n),
+        (m, n) if m > n => (n, m),
+        (m, n) => (m, n),
     }
 }
 
@@ -33,9 +33,9 @@ fn check(input: &[Domino]) -> CheckResult {
         return Correct;
     }
 
-    let mut output_sorted = output.iter().map(|d| normalize(d)).collect::<Vec<Domino>>();
+    let mut output_sorted = output.iter().map(|&d| normalize(d)).collect::<Vec<Domino>>();
     output_sorted.sort();
-    let mut input_sorted = input.iter().map(|d| normalize(d)).collect::<Vec<Domino>>();
+    let mut input_sorted = input.iter().map(|&d| normalize(d)).collect::<Vec<Domino>>();
     input_sorted.sort();
     if input_sorted != output_sorted {
         return DominoMismatch(output);
