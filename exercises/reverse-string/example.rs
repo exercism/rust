4920ba553ebabcd6a3a11e14306fb6328f8143c9
Meade Kincke <thedarkula2049@gmail.com>
2019-03-23 16:21:11 +0000

    reverse-string: Use a shorter example
    For better clarity and signal-to-noise ratio.

--- exercises/reverse-string/example.rs
+++ exercises/reverse-string/example.rs
@@ -1,5 +1,3 @@
 pub fn reverse(input: &str) -> String {
-    let mut output = String::with_capacity(input.len());
-    output.extend(input.chars().rev());
-    output
+    input.chars().rev().collect()
 }
