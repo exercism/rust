2428a7aed780bcd45c278166218033ca469c8c8f
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-05-06 09:13:08 +0200

    Revert "Wrong tests"
    This reverts commit b39d0309aa7358f052b82ce5df592c1570cee26b.
    
    The tests were not wrong; the documentation was confusing. See
    discussion in https://github.com/exercism/rust/pull/823 for details.

--- exercises/nth-prime/tests/nth-prime.rs
+++ exercises/nth-prime/tests/nth-prime.rs
@@ -2,23 +2,23 @@ use nth_prime as np;
 
 #[test]
 fn test_first_prime() {
-    assert_eq!(np::nth(1), 2);
+    assert_eq!(np::nth(0), 2);
 }
 
 #[test]
 #[ignore]
 fn test_second_prime() {
-    assert_eq!(np::nth(2), 3);
+    assert_eq!(np::nth(1), 3);
 }
 
 #[test]
 #[ignore]
 fn test_sixth_prime() {
-    assert_eq!(np::nth(6), 13);
+    assert_eq!(np::nth(5), 13);
 }
 
 #[test]
 #[ignore]
 fn test_big_prime() {
-    assert_eq!(np::nth(10001), 104743);
+    assert_eq!(np::nth(10000), 104743);
 }
