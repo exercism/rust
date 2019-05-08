e40e5316891780e402a70fa64d9b5859ec379d37
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-05-08 08:00:53 +0200

    respond to reviewer feedback

--- exercises/fizzy/example.rs
+++ exercises/fizzy/example.rs
@@ -159,7 +159,7 @@ mod test {
             "1", "2", "fizz", "4", "buzz", "fizz", "7", "8", "fizz", "buzz", "11", "fizz", "13",
             "14", "fizzbuzz", "16",
         ];
-        // a tiny bit more complicated becuase range isn't natively implemented on floats
+        // a tiny bit more complicated because range isn't natively implemented on floats
         let got = fizz_buzz()
             .apply(std::iter::successors(Some(1.0), |prev| Some(prev + 1.0)))
             .take(16)
