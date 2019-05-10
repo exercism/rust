6796cccb9787d8a9dd4dacfe9f4a9922c64a478b
ZapAnton <wc3ft2@gmail.com>
2019-05-10 12:17:34 +0300

    exercise crate: Refactored the fetch_configlet command to use the Github API

--- util/exercise/src/fetch_configlet.rs
+++ util/exercise/src/fetch_configlet.rs
@@ -1,76 +1,49 @@
 use failure::{format_err, Error};
 use flate2::read::GzDecoder;
+use serde_json::Value;
+use std::{
+    path::{Path, PathBuf},
+    string::ToString,
+};
 use tar::Archive;
 
-use std::path::{Path, PathBuf};
-
-pub const LATEST_URL: &str = "https://github.com/exercism/configlet/releases/latest";
-
-#[cfg(target_os = "macos")]
-pub fn target_os() -> &'static str {
-    "mac"
+// Returns the "os-arch" string, according to the host machine parameters
+fn get_os_arch() -> String {
+    let os = if cfg!(target_os = "windows") {
+        "windows"
+    } else if cfg!(target_os = "macos") {
+        "mac"
+    } else {
+        "linux"
+    };
+    let arch = if cfg!(target_pointer_width = "32") {
+        "32bit"
+    } else {
+        "64bit"
+    };
+    format!("{}-{}", os, arch)
 }
 
-#[cfg(target_os = "windows")]
-pub fn target_os() -> &'static str {
-    "windows"
-}
-
-#[cfg(not(any(target_os = "macos", target_os = "windows")))]
-pub fn target_os() -> &'static str {
-    "linux"
-}
-
-#[cfg(target_pointer_width = "32")]
-pub fn target_arch() -> &'static str {
-    "32bit"
-}
-
-#[cfg(not(target_pointer_width = "32"))]
-pub fn target_arch() -> &'static str {
-    "64bit"
-}
-
-pub fn get_latest() -> Result<String, Error> {
-    use reqwest::header;
-
-    let response = reqwest::ClientBuilder::new()
-        .redirect(reqwest::RedirectPolicy::none())
-        .build()?
-        .head(LATEST_URL)
-        .send()?;
-
-    let location = response
-        .headers()
-        .get(header::LOCATION)
-        .ok_or_else(|| format_err!("location not found in headers\n{:#?}", response.headers()))?;
-
-    Ok(location
-        .to_str()?
-        .trim()
-        .rsplit('/')
-        .nth(0)
-        .ok_or_else(|| format_err!("path separator not in location"))?
-        .to_string())
-}
-
-/// get the current URL for the appropriate configlet tarball
-pub fn pkg_url() -> Result<String, Error> {
-    Ok(format!(
-        "https://github.com/exercism/configlet/releases/download/{version}/configlet-{os}-{arch}.tgz",
-        version=get_latest()?,
-        os=target_os(),
-        arch=target_arch(),
-    ))
+// Makes a request to the Github API to get and return the download url for the latest configlet release
+fn get_download_url() -> Result<String, Error> {
+    reqwest::get("https://api.github.com/repos/exercism/configlet/releases/latest")?
+        .json::<Value>()?
+        .get("assets")
+        .and_then(Value::as_array)
+        .ok_or_else(|| format_err!("failed to get the 'assets' field from the Github response"))?
+        .iter()
+        .filter_map(|asset| asset.get("browser_download_url").and_then(Value::as_str))
+        .find(|url| url.contains(&get_os_arch()))
+        .map(ToString::to_string)
+        .ok_or_else(|| format_err!("failed to get the configlet release url"))
 }
 
 // download and extract configlet into the repo's /bin folder
 //
 // returns the path into which the bin was extracted on success
 pub fn download() -> Result<PathBuf, Error> {
-    let response = reqwest::get(&pkg_url()?)?;
+    let response = reqwest::get(&get_download_url()?)?;
     let mut archive = Archive::new(GzDecoder::new(response));
-
     let download_path = Path::new(&*crate::TRACK_ROOT).join("bin");
     archive
         .unpack(&download_path)
