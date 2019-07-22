897294f2623f5d934a6098a976a46d0e505a2f57
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-07-22 21:33:50 +0200

    update tests

--- exercises/palindrome-products/src/lib.rs
+++ exercises/palindrome-products/src/lib.rs
@@ -1,22 +1,9 @@
 pub type Palindrome = u64;
-pub fn get_palindrome_products(min: u64, max: u64) -> Vec<Palindrome> {
+
+pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
     unimplemented!(
-        "Find all palindromic numbers which are products of numbers in the inclusive range ({}..{})",
+        "Find the min and max palindromic numbers which are products of numbers in the inclusive range ({}..{})",
         min,
         max
-    )
-}
-
-pub fn min(palindromes: &[Palindrome]) -> Option<Palindrome> {
-    unimplemented!(
-        "Return the palindrome of minimal value from the supplied list: {:?}",
-        palindromes
-    )
-}
-
-pub fn max(palindromes: &[Palindrome]) -> Option<Palindrome> {
-    unimplemented!(
-        "Return the palindrome of maximal value from the supplied list: {:?}",
-        palindromes
     )
 }
--- exercises/palindrome-products/tests/palindrome-products.rs
+++ exercises/palindrome-products/tests/palindrome-products.rs
@@ -1,56 +1,56 @@
-use palindrome_products::*;
+use palindrome_products::palindrome_products;
 
 #[test]
 fn single_digits() {
-    let palindromes = get_palindrome_products(1, 9);
-    assert_eq!(min(&palindromes), Some(1));
-    assert_eq!(max(&palindromes), Some(9));
+    let (min, max) = palindrome_products(1, 9).unwrap();
+    assert_eq!(min, 1);
+    assert_eq!(max, 9);
 }
 
 #[test]
 #[ignore]
 fn double_digits() {
-    let palindromes = get_palindrome_products(10, 99);
-    assert_eq!(min(&palindromes), Some(121));
-    assert_eq!(max(&palindromes), Some(9009));
+    let (min, max) = palindrome_products(10, 99).unwrap();
+    assert_eq!(min, 121);
+    assert_eq!(max, 9009);
 }
 
 #[test]
 #[ignore]
 fn triple_digits() {
-    let palindromes = get_palindrome_products(100, 999);
-    assert_eq!(min(&palindromes), Some(10201));
-    assert_eq!(max(&palindromes), Some(906609));
+    let (min, max) = palindrome_products(100, 999).unwrap();
+    assert_eq!(min, 10201);
+    assert_eq!(max, 906609);
 }
 
 #[test]
 #[ignore]
 fn four_digits() {
-    let palindromes = get_palindrome_products(1000, 9999);
-    assert_eq!(min(&palindromes), Some(1002001));
-    assert_eq!(max(&palindromes), Some(99000099));
+    let (min, max) = palindrome_products(1000, 9999).unwrap();
+    assert_eq!(min, 1002001);
+    assert_eq!(max, 99000099);
 }
 
 #[test]
 #[ignore]
 fn empty_result_for_smallest_palindrome() {
-    assert_eq!(min(&get_palindrome_products(1002, 1003)), None);
+    assert_eq!(palindrome_products(1002, 1003), None);
 }
 
 #[test]
 #[ignore]
 fn empty_result_for_largest_palindrome() {
-    assert_eq!(max(&get_palindrome_products(15, 15)), None);
+    assert_eq!(palindrome_products(15, 15), None);
 }
 
 #[test]
 #[ignore]
 fn error_smallest_palindrome_when_min_gt_max() {
-    assert_eq!(min(&get_palindrome_products(1000, 1)), None);
+    assert_eq!(palindrome_products(1000, 1), None);
 }
 
 #[test]
 #[ignore]
 fn error_largest_palindrome_when_min_st_max() {
-    assert_eq!(max(&get_palindrome_products(2, 1)), None);
+    assert_eq!(palindrome_products(2, 1), None);
 }
