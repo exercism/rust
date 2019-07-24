1634057511f1065e8aa7dd7f941ed7f72952147f
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-07-24 23:32:04 +0200

    update required function to conform to test description
    simultaneously update test suite to v1.2.0

--- exercises/palindrome-products/src/lib.rs
+++ exercises/palindrome-products/src/lib.rs
@@ -1,4 +1,21 @@
-pub type Palindrome = u64;
+#[derive(Debug, PartialEq, Eq)]
+pub struct Palindrome {
+    // implement your palindrome type here
+}
+
+impl Palindrome {
+    pub fn new(a: u64, b: u64) -> Palindrome {
+        unimplemented!("create a palindrome with factors ({}, {})", a, b)
+    }
+
+    pub fn value(&self) -> u64 {
+        unimplemented!("return the value of this palindrome")
+    }
+
+    pub fn insert(&mut self, a: u64, b: u64) {
+        unimplemented!("insert new factors ({}, {}) into this palindrome", a, b)
+    }
+}
 
 pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
     unimplemented!(
