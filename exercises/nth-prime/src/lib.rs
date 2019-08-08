34b1d3cae216172b59159afce0cf3c285929af0b
韦宏毅 <weihongyi@syswin.com>
2019-08-08 19:09:16 +0800

    素数

--- exercises/nth-prime/src/lib.rs
+++ exercises/nth-prime/src/lib.rs
@@ -1,3 +1,33 @@
+
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    let mut index = 2;
+    let mut counter  = 0;
+    let result = loop {
+        if is_prime(index) {
+            if counter == n {
+                break index;
+            }
+            counter += 1;
+        }
+        index += 1;
+    };
+    return result;
+}
+fn is_prime(n: u32) -> bool {
+    if n <= 3 {
+        return n > 1;
+    }
+    if n % 2 ==0 || n % 3 == 0 {
+        return false;
+    }
+
+    let mut i = 5;
+    while (i * i ) <= n {
+        if n % i == 0 || n % (i+2) == 0 {
+            return false;
+        }
+        i += 6
+    }
+
+    return true;
 }
--- exercises/raindrops/src/lib.rs
+++ exercises/raindrops/src/lib.rs
@@ -1,4 +1,19 @@
 pub fn raindrops(n: u32) -> String {
-    let s = n.toString();
+    let mut drops = String::new();
+    if n % 3 == 0 {
+        drops.push_str("Pling")
+    }
 
+    if n % 5 == 0 {
+        drops.push_str("Plang")
+    }
+    if n % 7 == 0 {
+        drops.push_str("Plong")
+    }
+
+    if drops.is_empty() {
+        let s = format!("{}", n);
+        drops.push_str(&s);
+    }
+    drops
 }
