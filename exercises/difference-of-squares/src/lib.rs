42572e7345f93e75b7004c475ba390fb603cd26b
韦宏毅 <weihongyi@syswin.com>
2019-08-09 18:18:10 +0800

    做两题

--- exercises/difference-of-squares/src/lib.rs
+++ exercises/difference-of-squares/src/lib.rs
@@ -1,14 +1,12 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{}", n)
+    let sum: u32 = (1..=n).map(|x| x).sum();
+    sum * sum
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{}", n)
+    (1..=n).map(|x| x * x).sum()
 }
 
 pub fn difference(n: u32) -> u32 {
-    unimplemented!(
-        "difference between square of sum of 1...{n} and sum of squares of 1...{n}",
-        n = n,
-    )
+    square_of_sum(n) - sum_of_squares(n)
 }
--- exercises/proverb/src/lib.rs
+++ exercises/proverb/src/lib.rs
@@ -1,3 +1,11 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    let mut v = Vec::new();
+    for index in 0..list.len() {
+        if index == list.len() - 1 {
+            v.push(format!("And all for the want of a {}.", list[0]));
+        } else {
+            v.push(format!("For want of a {} the {} was lost.", list[index], list[index+1]));
+        }
+    }
+    v.join("\n")
 }
