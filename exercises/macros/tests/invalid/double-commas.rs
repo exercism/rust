0dd82131fcca01364733d616e89a73a1c7ead118
pedantic79 <pedantic@gmail.com>
2020-05-19 07:58:41 -0500

    macros: Assign invalid tests to variable (#947)
    - Try to assign to variable, in most cases this is `<_, _>`
    - only-arrow and only-comma use `<(), ()>`
    - Also enables leading-comma which was accidentally not enabled

--- exercises/macros/tests/invalid/double-commas.rs
+++ exercises/macros/tests/invalid/double-commas.rs
@@ -2,5 +2,5 @@ use macros::hashmap;
 
 fn main() {
     // a single trailing comma is okay, but two is not
-    hashmap!('a' => 2, ,);
+    let _hm: ::std::collections::HashMap<_, _> = hashmap!('a' => 2, ,);
 }
