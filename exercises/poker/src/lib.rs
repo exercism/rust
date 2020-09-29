b3fceede28a719e085bccfeb358b97647dfeec52
Hendrik Bilges <elektronaut0815@users.noreply.github.com>
2020-09-29 18:02:28 +0200

    correcting function signature in lib.rs

--- exercises/poker/src/lib.rs
+++ exercises/poker/src/lib.rs
@@ -2,6 +2,6 @@
 ///
 /// Note the type signature: this function should return _the same_ reference to
 /// the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
-pub fn winning_hands<'a>(hands: &[&'a str]) -> Option<Vec<&'a str>> {
+pub fn winning_hands<'a>(hands: &[&'a str]) -> Result<Vec<&'a str>, &'static str> {
     unimplemented!("Out of {:?}, which hand wins?", hands)
 }
