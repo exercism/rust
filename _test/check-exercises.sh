02101b294ff70a0f1c97f722666a0a830b06d0e6
Devin Gunay <devingunay@gmail.com>
2020-10-07 16:36:08 -0700

    add set -e -o pipefail to script

--- _test/check-exercises.sh
+++ _test/check-exercises.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Improve error propagation during CI
+set -e -o pipefail
+
 # test for existence and executability of the test-exercise script
 # this depends on that
 if [ ! -f "./bin/test-exercise" ]; then
