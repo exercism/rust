b21175c978f336b22896ed69ef1889ad32ae2346
Mitchell <thebeatbutton@gmail.com>
2020-10-28 14:07:54 -0600

    Correct lifetimes in anagrams_for signature
    A completed solution will otherwise fail to compile

--- exercises/anagram/src/lib.rs
+++ exercises/anagram/src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
     unimplemented!(
         "For the '{}' word find anagrams among the following words: {:?}",
         word,
