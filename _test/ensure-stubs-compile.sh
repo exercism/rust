d81e2b2b00259de695bc7302f3187531d07a99e9
Peter Tseng <pht24@cornell.edu>
2020-11-18 09:05:30 +0000

    GitHub Actions: Run Clippy on stubs
    Fails the build if any stubs have Clippy errors.
    Does not fail the build if stubs have Clippy warnings, but does display
    them (and there are a number of them).
    
    The author of this PR is not sure we will ever get around to fixing all
    the Clippy warnings, but supposes that it is at least beneficial to make
    sure we add no new Clippy errors.
    
    1/3 of https://github.com/exercism/rust/issues/659
    
    (By the way, ensure-stubs-compile already had a bad mix of spaces and
    tabs and this commit did not help any in making it better. The author's
    text editor cannot easily put tabs in shell scripts)

--- _test/ensure-stubs-compile.sh
+++ _test/ensure-stubs-compile.sh
@@ -30,21 +30,40 @@ for dir in $changed_exercises; do
 	cp -r $dir/tests $dir/tests.orig
 	cp $dir/src/lib.rs $dir/lib.rs.orig
 
-	# This sed serves two purposes:
-	# First, in Travis CI, we may have already compiled using the example solution.
-	# Edit the src/lib.rs file so that we surely recompile using the stub.
-	# Second, ensures that the stub compiles without warnings.
-	sed -i -e '1i #![deny(warnings)]' "$dir/src/lib.rs"
+  # In CI, we may have already compiled using the example solution.
+  # So we want to touch the src/lib.rs in some way,
+  # so that we surely recompile using the stub.
+  if [ -n "$CLIPPY" ]; then
+    # We don't deny warnings in clippy mode,
+    # just because there are many we don't want to deal with right now.
+    # So in clippy mode it's just a touch.
+    touch "$dir/src/lib.rs"
+  else
+    # In non-clippy mode, we do want to compile without warnings.
+    sed -i -e '1i #![deny(warnings)]' "$dir/src/lib.rs"
+  fi
 
 	# Deny warnings in the tests that may result from compiling the stubs.
 	# This helps avoid, for example, an overflowing literal warning
 	# that could be caused by a stub with a type that is too small.
 	sed -i -e '1i #![deny(warnings)]' $dir/tests/*.rs
 
-	if ! (cd $dir && cargo test --quiet --no-run); then
-	  echo "$exercise's stub does not compile; please make it compile"
-	  broken="$broken\n$exercise"
-	fi
+  if [ -n "$CLIPPY" ]; then
+    if ! (cd $dir && cargo clippy --lib --tests --color always 2>clippy.log); then
+      cat $dir/clippy.log
+      broken="$broken\n$exercise"
+    elif grep -q warning $dir/clippy.log; then
+      # Warnings will be outputted, but do not fail the build.
+      echo "clippy $exercise WARN"
+      cat $dir/clippy.log
+    else
+      # Just to show progress
+      echo "clippy $exercise OK"
+    fi
+  elif ! (cd $dir && cargo test --quiet --no-run); then
+    echo "$exercise's stub does not compile; please make it compile"
+    broken="$broken\n$exercise"
+  fi
 
 	# Restore tests and stub.
 	mv $dir/lib.rs.orig $dir/src/lib.rs
