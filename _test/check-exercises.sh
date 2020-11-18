bfc554ad471a4dcfb628f3dd1cca7eabbb89a28a
Peter Tseng <pht24@cornell.edu>
2020-11-18 09:42:58 +0000

    DO NOT MERGE: run on all exercises

--- _test/check-exercises.sh
+++ _test/check-exercises.sh
@@ -28,7 +28,7 @@ fi
 
 repo=$(cd "$(dirname "$0")/.." && pwd)
 
-if [ "$GITHUB_EVENT_NAME" = "pull_request" ]; then
+if false; then
    files="$(git diff --diff-filter=d --name-only remotes/origin/master | grep "exercises/" | cut -d '/' -f -2 | sort -u | awk -v repo=$repo '{print repo"/"$1}')"
 else
 	files=$repo/exercises/*
