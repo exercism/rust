411b94dd72778659d9028308a53bf213abf7b8be
Eli Flanagan <efx@users.noreply.github.com>
2021-01-26 14:49:41 -0500

    start vec-macro concept exercise
    * start vec-macro concept exercise
    
    Per our [discussion](https://github.com/exercism/v3/pull/2918#issuecomment-756743573) it makes sense to decompose the `vec` concept exercise into smaller, focused chunks.
    I attempt to do so here for the first concept `vec-macro`.
    
    * add fibonacci and improve naming
    
    * gloss Fibonacci sequence
    
    Stole definition from https://mathmusic.pages.roanoke.edu/fibonaccis-sequence/
    
    * Update languages/exercises/concept/vec-macro/.meta/example.rs
    
    Co-authored-by: Peter Goodspeed-Niklaus <coriolinus@users.noreply.github.com>
    
    * better rustdoc formatting
    
    * start populating exercism metadata
    
    * break out concept documentation, format
    
    We'll need a distinct subdirectory in `concepts` for each concept
    we use. I'm on the fence about this for Vectors as a whole.
    
    * Update languages/concepts/vec-macro/about.md
    
    Co-authored-by: Peter Goodspeed-Niklaus <coriolinus@users.noreply.github.com>
    
    * Update languages/concepts/vec-macro/about.md
    
    Co-authored-by: Peter Goodspeed-Niklaus <coriolinus@users.noreply.github.com>
    
    * Update languages/concepts/vec-macro/introduction.md
    
    Co-authored-by: Peter Goodspeed-Niklaus <coriolinus@users.noreply.github.com>
    
    * Update languages/exercises/concept/vec-macro/.meta/design.md
    
    Co-authored-by: Peter Goodspeed-Niklaus <coriolinus@users.noreply.github.com>
    
    * Update languages/exercises/concept/vec-macro/.meta/example.rs
    
    Co-authored-by: Peter Goodspeed-Niklaus <coriolinus@users.noreply.github.com>
    
    * Update languages/exercises/concept/vec-macro/.meta/example.rs
    
    Co-authored-by: Peter Goodspeed-Niklaus <coriolinus@users.noreply.github.com>
    
    * add unimplemented
    
    * use coriolinus' improvement!
    
    More precise way to assert understanding:
    https://github.com/exercism/v3/pull/3061#discussion_r564630987
    
    * add other suggestion, attribution!
    
    * remove needless TODO string
    
    * Update languages/exercises/concept/vec-macro/src/lib.rs
    
    Co-authored-by: Peter Goodspeed-Niklaus <coriolinus@users.noreply.github.com>
    
    * mirror lib.rs changes
    
    Co-authored-by: Peter Goodspeed-Niklaus <coriolinus@users.noreply.github.com>

---
+++ concepts/vec-macro/about.md
@@ -0,0 +1,9 @@
+# `vec![]`
+
+You can use Rust macros to generate vectors.
+
+## Syntax
+
+- `vec![]` uses a std Rust macro to initialize an empty `Vec<T>`
+- `vec![1; 3]` initializes a vector with three ones
+- `vec![1, 2, 3]` initializes a vector with the literal values 1, 2, 3
---
+++ exercises/concept/vec-macro/src/lib.rs
@@ -0,0 +1,21 @@
+//! vec-macro for creating empty vectors, and vectors of particular size, with vec!
+
+/// Create an empty vector
+fn create_empty() -> Vec<u8> {
+    unimplemented!()
+}
+
+/// Create a buffer of `count` zeroes.
+///
+/// Applications often use buffers when serializing data to send over the network.
+fn create_buffer(count: usize) -> Vec<u8> {
+    unimplemented!()
+}
+
+/// Create a vector containing the first five digits of the Fibonacci sequence.
+///
+/// Fibonacci's sequence is the list of numbers where the next number is a sum of the previous two.
+/// Its first five elements are `1, 1, 2, 3, 5`.
+fn fibonacci() -> Vec<u8> {
+    unimplemented!()
+}
