03bf868c8e250c606831705977fe994bef5bf54f
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-01-29 16:54:55 +0100

    fix changed-file detection where GITHUB_EVENT_NAME=pull_request

--- _test/check-exercises.sh
+++ _test/check-exercises.sh
@@ -29,7 +29,13 @@ fi
 repo=$(cd "$(dirname "$0")/.." && pwd)
 
 if [ "$GITHUB_EVENT_NAME" = "pull_request" ]; then
-   files="$(git diff --diff-filter=d --name-only remotes/origin/main | grep "exercises/" | cut -d '/' -f -2 | sort -u | awk -v repo=$repo '{print repo"/"$1}')"
+   files="$(
+      git diff --diff-filter=d --name-only remotes/origin/main |
+      grep "exercises/" |
+      cut -d '/' -f -3 |
+      sort -u |
+      awk -v repo=$repo '{print repo"/"$1}'
+   )"
 else
 	files=$repo/exercises/*/*
 fi
