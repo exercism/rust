2a66abbefe959dd9fc223ec730cd261d86b0a7f9
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-01-29 16:45:17 +0100

    fix vec-macro bugs

--- exercises/concept/vec-macro/src/lib.rs
+++ exercises/concept/vec-macro/src/lib.rs
@@ -1,14 +1,14 @@
 //! vec-macro for creating empty vectors, and vectors of particular size, with vec!
 
 /// Create an empty vector
-fn create_empty() -> Vec<u8> {
+pub fn create_empty() -> Vec<u8> {
     unimplemented!()
 }
 
 /// Create a buffer of `count` zeroes.
 ///
 /// Applications often use buffers when serializing data to send over the network.
-fn create_buffer(count: usize) -> Vec<u8> {
+pub fn create_buffer(count: usize) -> Vec<u8> {
     unimplemented!()
 }
 
@@ -16,6 +16,6 @@ fn create_buffer(count: usize) -> Vec<u8> {
 ///
 /// Fibonacci's sequence is the list of numbers where the next number is a sum of the previous two.
 /// Its first five elements are `1, 1, 2, 3, 5`.
-fn fibonacci() -> Vec<u8> {
+pub fn fibonacci() -> Vec<u8> {
     unimplemented!()
 }
