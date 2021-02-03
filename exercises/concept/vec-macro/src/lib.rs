480a2caa8c9c022bc678ef631a8323d023baaf1c
Eli Flanagan <efx@users.noreply.github.com>
2021-02-03 10:14:17 -0500

    fix stub correctly

--- exercises/concept/vec-macro/src/lib.rs
+++ exercises/concept/vec-macro/src/lib.rs
@@ -7,7 +7,7 @@ pub fn create_empty() -> Vec<u8> {
 ///
 /// Applications often use buffers when serializing data to send over the network.
 pub fn create_buffer(count: usize) -> Vec<u8> {
-    unimplemented!()
+    unimplemented!("create a zeroized buffer of {} bytes", count)
 }
 
 /// Create a vector containing the first five elements of the Fibonacci sequence.
