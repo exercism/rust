da56c42c38b23fe4507c16105e0f63abcdfca251
Eli Flanagan <efx@users.noreply.github.com>
2021-02-03 10:24:52 -0500

    finish vec-macro (#1099)
    * remove generated comment and intro comment
    
    * remove other comment
    
    * add beta flag
    
    * add barebones hints
    
    * fill out vec-macro intro docs
    
    * use consistent term
    
    * restore instructional comment
    
    * Update exercises/concept/vec-macro/.docs/instructions.md
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>
    
    * fix a typo, gloss Fibonacci sequence, add examples
    
    * add definition for completeness
    
    * fix definition
    
    * yolo active and remove unused in stub
    
    * fix stub correctly
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>

---
+++ exercises/concept/vec-macro/.docs/instructions.md
@@ -0,0 +1,20 @@
+You are going to initialize empty buffers and list the first five numbers, or elements, of the Fibonacci sequence.
+
+The Fibonacci sequence is a set of numbers where the next element is the sum of the prior two. We start the sequence at one. So the first two elements are 1 and 1.
+
+## 1. Create a buffer of `count` zeroes.
+
+Create a function that creates a buffer of `count` zeroes.
+```rust
+let my_buffer = create_buffer(5);
+// [0, 0, 0, 0, 0]
+```
+
+## 2. List the first five elements of the Fibonacci sequence
+
+Create a function that returns the first five numbers of the Fibonacci sequence. 
+Its first five elements are `1, 1, 2, 3, 5`
+```rust
+let first_five = fibonacci();
+// [1, 1, 2, 3, 5]
+```
--- exercises/concept/vec-macro/src/lib.rs
+++ exercises/concept/vec-macro/src/lib.rs
@@ -1,9 +1,3 @@
-// This stub file contains items which aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
-
-//! vec-macro for creating empty vectors, and vectors of particular size, with vec!
-
 /// Create an empty vector
 pub fn create_empty() -> Vec<u8> {
     unimplemented!()
@@ -13,10 +7,10 @@ pub fn create_empty() -> Vec<u8> {
 ///
 /// Applications often use buffers when serializing data to send over the network.
 pub fn create_buffer(count: usize) -> Vec<u8> {
-    unimplemented!()
+    unimplemented!("create a zeroized buffer of {} bytes", count)
 }
 
-/// Create a vector containing the first five digits of the Fibonacci sequence.
+/// Create a vector containing the first five elements of the Fibonacci sequence.
 ///
 /// Fibonacci's sequence is the list of numbers where the next number is a sum of the previous two.
 /// Its first five elements are `1, 1, 2, 3, 5`.
