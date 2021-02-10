45e6e2ce8f33b771a486c894a29ff1b9e546a288
Eli Flanagan <efx@users.noreply.github.com>
2021-02-10 10:49:05 -0500

    use a valid function signature

--- exercises/practice/accumulate/src/lib.rs
+++ exercises/practice/accumulate/src/lib.rs
@@ -1,4 +1,7 @@
 /// What should the type of _function be?
-pub fn map(input: Vec<i32>, _function: ???) -> Vec<i32> {
+pub fn map<F, T, U>(input: Vec<i32>, _function: F) -> Vec<i32>
+where
+    F: FnMut(T) -> U,
+{
     unimplemented!("Transform input vector {:?} using passed function", input);
 }
