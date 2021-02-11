3f0bdc21264cbd318f6ff5a3729abb4914a59947
Eli Flanagan <efx@users.noreply.github.com>
2021-02-11 17:29:19 -0500

    add error handling to CI formatting check (#1130)
    * add error handling to CI formatting check
    
    Start addressing #1129.
    I added error handling flag `-e` and shellcheck lints.
    I'll push up fixes for the associated failures.
    
    * use a valid function signature
    
    * Apply suggestions from code review
    
    Co-authored-by: Peter Goodspeed-Niklaus <coriolinus@users.noreply.github.com>
    
    * generalize rustfmt runner
    
    * run ./bin/format_exercises
    
    * restore accumlate's source, fix format_exercises
    
    I copied the behavior from 'ensure-stubs-compile.sh' to assert the stub
    is allowed to not compile and print an informative message.
    
    * remove my pesky tabs!
    
    Co-authored-by: Peter Goodspeed-Niklaus <coriolinus@users.noreply.github.com>

--- exercises/concept/numbers/.meta/exemplar.rs
+++ exercises/concept/numbers/.meta/exemplar.rs
@@ -6,7 +6,7 @@ pub fn production_rate_per_hour(speed: u8) -> f64 {
         s @ 1..=4 => (PRODUCTION_RATE_DEFAULT * s) as f64,
         s @ 5..=8 => (PRODUCTION_RATE_DEFAULT * s) as f64 * 0.9,
         s @ 9..=10 => (PRODUCTION_RATE_DEFAULT * s) as f64 * 0.77,
-        _ => panic!("Speed should be a value from 0 to 10")
+        _ => panic!("Speed should be a value from 0 to 10"),
     }
 }
 
