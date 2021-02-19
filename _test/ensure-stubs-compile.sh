b4611bde0a6c23eba84fa71794b21f29a41093cd
Eli Flanagan <efx@users.noreply.github.com>
2021-02-19 14:13:59 -0500

    more lints

--- _test/ensure-stubs-compile.sh
+++ _test/ensure-stubs-compile.sh
@@ -8,10 +8,10 @@ if [ "$GITHUB_EVENT_NAME" = "pull_request" ]; then
     grep "exercises/" |
     cut -d '/' -f -3 |
     sort -u |
-    awk -v repo=$repo '{print repo"/"$1}'
+    awk -v repo="$repo" '{print repo"/"$1}'
   )"
 else
-	changed_exercises=$repo/exercises/*/*
+	changed_exercises="$repo/exercises/*/*"
 fi
 
 if [ -z "$changed_exercises" ]; then
@@ -27,14 +27,14 @@ for dir in $changed_exercises; do
 
 	allowed_file=$dir/.meta/ALLOWED_TO_NOT_COMPILE
 
-	if [ -f $allowed_file ]; then
+	if [ -f "$allowed_file" ]; then
 	  echo "$exercise's stub is allowed to not compile"
 	  continue
 	fi
 
 	# Backup tests and stub; this script will modify them.
-	cp -r $dir/tests $dir/tests.orig
-	cp $dir/src/lib.rs $dir/lib.rs.orig
+	cp -r "$dir/tests" "$dir/tests.orig"
+	cp "$dir/src/lib.rs" "$dir/lib.rs.orig"
 
   # In CI, we may have already compiled using the example solution.
   # So we want to touch the src/lib.rs in some way,
@@ -45,31 +45,31 @@ for dir in $changed_exercises; do
 	# Deny warnings in the tests that may result from compiling the stubs.
 	# This helps avoid, for example, an overflowing literal warning
 	# that could be caused by a stub with a type that is too small.
-	sed -i -e '1i #![deny(warnings)]' $dir/tests/*.rs
+	sed -i -e '1i #![deny(warnings)]' "$dir"/tests/*.rs
 
   if [ -n "$CLIPPY" ]; then
     echo "clippy $exercise..."
     # We don't find it useful in general to have students implement Default,
     # since we're generally not going to test it.
     if ! (
-      cd $dir &&
+      cd "$dir" &&
       cargo clippy --lib --tests --color always -- --allow clippy::new_without_default 2>clippy.log
     ); then
-      cat $dir/clippy.log
+      cat "$dir/clippy.log"
       broken="$broken\n$exercise"
     else
       # Just to show progress
       echo "... OK"
     fi
-  elif ! (cd $dir && cargo test --quiet --no-run); then
+  elif ! (cd "$dir" && cargo test --quiet --no-run); then
     echo "$exercise's stub does not compile; please make it compile"
     broken="$broken\n$exercise"
   fi
 
 	# Restore tests and stub.
-	mv $dir/lib.rs.orig $dir/src/lib.rs
-	rm -r $dir/tests
-	mv $dir/tests.orig $dir/tests
+	mv "$dir/lib.rs.orig" "$dir/src/lib.rs"
+	rm -r "$dir/tests"
+	mv "$dir/tests.orig" "$dir/tests"
 done
 
 if [ -n "$broken" ]; then
