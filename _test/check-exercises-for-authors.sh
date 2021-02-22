4464c6da375256cd9c97351ebd31b09b6d84a657
Eli Flanagan <efx@users.noreply.github.com>
2021-02-22 14:52:45 -0500

    use cleaner idiom

--- _test/check-exercises-for-authors.sh
+++ _test/check-exercises-for-authors.sh
@@ -2,9 +2,7 @@
 
 repo=$(cd "$(dirname "$0")/.." && pwd)
 
-grep -rnw "$repo/exercises/" --include="*.toml" -e "authors"
-# shellcheck disable=SC2181
-if [ $? -eq 0 ]; then
+if grep -rnw "$repo/exercises/" --include="*.toml" -e "authors"; then
 	echo "Found 'authors' field in exercises";
 	exit 1;
 fi
