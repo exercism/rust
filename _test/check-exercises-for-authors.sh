ce074ab3fca8f143ae198d37a289831cbb32be54
Eli Flanagan <efx@users.noreply.github.com>
2021-02-22 14:24:18 -0500

    disable one shellcheck

--- _test/check-exercises-for-authors.sh
+++ _test/check-exercises-for-authors.sh
@@ -3,7 +3,7 @@
 repo=$(cd "$(dirname "$0")/.." && pwd)
 
 grep -rnw "$repo/exercises/" --include="*.toml" -e "authors"
-
+# shellcheck disable=SC2181
 if [ $? -eq 0 ]; then
 	echo "Found 'authors' field in exercises";
 	exit 1;
