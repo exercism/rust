c154b08272d553b9d1198513e394fc22295d5809
Eli Flanagan <efx@users.noreply.github.com>
2021-02-26 16:56:59 -0500

    Apply suggestions from code review
    Co-authored-by: Peter Goodspeed-Niklaus <coriolinus@users.noreply.github.com>

--- bin/lint_tool_file_names.sh
+++ bin/lint_tool_file_names.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-# Prohibit internal shell script file names snake_case
+# Require that internal shell script file names use snake_case
 set -eo pipefail
 
 # find a list of files whose names do not match our convention
-errant_files=$(find bin/ _test/ -iname '*\.sh' -exec basename {} \; | grep '[^a-z_.].*')
+errant_files=$(find bin/ _test/ -iname '*\.sh' -exec basename {} \; | grep '[^a-z_.]')
 
 if [ -n "$errant_files" ]; then
     echo "These file names do not follow our snake case convention:"
     # find them again to print the whole relative path
     while IFS= read -r file_name; do
-        find . -iname "$file_name"
+        find . -name "$file_name"
     done <<< "$errant_files"
     echo "Please correct them!"
     exit 1
