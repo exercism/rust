e15a1912176e82c3707036ddf14be6c119924124
Will Hakes <info@cwilliamhakes.com>
2021-04-21 11:19:48 -0600

    Run cargo fmt

--- exercises/concept/final-electric-bill/src/lib.rs
+++ exercises/concept/final-electric-bill/src/lib.rs
@@ -1,3 +1,6 @@
 pub fn fix_billing_summary(summary: Vec<i32>) -> Option<Vec<i32>> {
-    unimplemented!("Combine the final two elements of {:?}, or `None` if fewer than two elements present.", summary)
+    unimplemented!(
+        "Combine the final two elements of {:?}, or `None` if fewer than two elements present.",
+        summary
+    )
 }
