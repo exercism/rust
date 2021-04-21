7318f31358a84af2096f5793a667a44b374f2194
C William Hakes <info@cwilliamhakes.com>
2021-04-21 10:26:55 -0600

    Update exercises/concept/final-electric-bill/src/lib.rs
    Co-authored-by: Peter Goodspeed-Niklaus <coriolinus@users.noreply.github.com>

--- exercises/concept/final-electric-bill/src/lib.rs
+++ exercises/concept/final-electric-bill/src/lib.rs
@@ -1,3 +1,3 @@
-pub fn fix_billing_summary(_summary: Vec<i32>) -> Vec<i32> {
-    unimplemented!()
+pub fn fix_billing_summary(summary: Vec<i32>) -> Vec<i32> {
+    unimplemented!("Combine the final two elements of {}, or `None` if fewer than two elements present.", summary)
 }
