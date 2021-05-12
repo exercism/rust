f85e109618a4bb30faab655242247c967651886a
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-05-12 09:14:07 +0200

    fix clippy beta lints

--- exercises/practice/isogram/tests/isogram.rs
+++ exercises/practice/isogram/tests/isogram.rs
@@ -2,21 +2,20 @@ use isogram::check;
 
 #[test]
 fn empty_string() {
-    assert_eq!(check(""), true, "An empty string should be an isogram.")
+    assert!(check(""), "An empty string should be an isogram.")
 }
 
 #[test]
 #[ignore]
 fn only_lower_case_characters() {
-    assert_eq!(check("isogram"), true, "\"isogram\" should be an isogram.")
+    assert!(check("isogram"), "\"isogram\" should be an isogram.")
 }
 
 #[test]
 #[ignore]
 fn one_duplicated_character() {
-    assert_eq!(
+    assert!(
         check("eleven"),
-        false,
         "\"eleven\" has more than one \'e\', therefore it is no isogram."
     )
 }
@@ -24,9 +23,8 @@ fn one_duplicated_character() {
 #[test]
 #[ignore]
 fn longest_reported_english_isogram() {
-    assert_eq!(
+    assert!(
         check("subdermatoglyphic"),
-        true,
         "\"subdermatoglyphic\" should be an isogram."
     )
 }
@@ -34,9 +32,8 @@ fn longest_reported_english_isogram() {
 #[test]
 #[ignore]
 fn one_duplicated_character_mixed_case() {
-    assert_eq!(
+    assert!(
         check("Alphabet"),
-        false,
         "\"Alphabet\" has more than one \'a\', therefore it is no isogram."
     )
 }
@@ -44,9 +41,8 @@ fn one_duplicated_character_mixed_case() {
 #[test]
 #[ignore]
 fn hypothetical_isogramic_word_with_hyphen() {
-    assert_eq!(
+    assert!(
         check("thumbscrew-japingly"),
-        true,
         "\"thumbscrew-japingly\" should be an isogram."
     )
 }
@@ -54,9 +50,8 @@ fn hypothetical_isogramic_word_with_hyphen() {
 #[test]
 #[ignore]
 fn isogram_with_duplicated_hyphen() {
-    assert_eq!(
+    assert!(
         check("six-year-old"),
-        true,
         "\"six-year-old\" should be an isogram."
     )
 }
@@ -64,9 +59,8 @@ fn isogram_with_duplicated_hyphen() {
 #[test]
 #[ignore]
 fn made_up_name_that_is_an_isogram() {
-    assert_eq!(
+    assert!(
         check("Emily Jung Schwartzkopf"),
-        true,
         "\"Emily Jung Schwartzkopf\" should be an isogram."
     )
 }
@@ -74,9 +68,8 @@ fn made_up_name_that_is_an_isogram() {
 #[test]
 #[ignore]
 fn duplicated_character_in_the_middle() {
-    assert_eq!(
+    assert!(
         check("accentor"),
-        false,
         "\"accentor\" has more than one \'c\', therefore it is no isogram."
     )
 }
