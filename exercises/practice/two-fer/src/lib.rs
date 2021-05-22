20dfb8577fa65bff0ebeff40d2507d1d52bec07a
Jeremy Walker <jez.walker@gmail.com>
2021-05-22 15:32:32 +0100

    Blank out the stub file of deprecated two-fer

--- exercises/practice/two-fer/src/lib.rs
+++ exercises/practice/two-fer/src/lib.rs
@@ -1,6 +1,3 @@
 pub fn twofer(name: &str) -> String {
-    match name {
-        "" => "One for you, one for me.".to_string(),
-        _ => format!("One for {}, one for me.", name),
-    }
+    "".to_string()
 }
