83cc5102dcd230c0e55451edbec87af548422889
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-06 15:59:14 +0200

    Fix rust formatting

--- exercises/concept/lucians-luscious-lasagna/src/lib.rs
+++ exercises/concept/lucians-luscious-lasagna/src/lib.rs
@@ -7,13 +7,23 @@ pub fn expected_minutes_in_oven() -> u32 {
 }
 
 pub fn remaining_minutes_in_oven(actual_minutes_in_oven: u32) -> u32 {
-    unimplemented!("calculate remaining minutes in oven given actual minutes in oven: {}", actual_minutes_in_oven)
+    unimplemented!(
+        "calculate remaining minutes in oven given actual minutes in oven: {}",
+        actual_minutes_in_oven
+    )
 }
 
 pub fn preparation_time_in_minutes(number_of_layers: u32) -> u32 {
-    unimplemented!("calculate preparation time in minutes for number of layers: {}", number_of_layers)
+    unimplemented!(
+        "calculate preparation time in minutes for number of layers: {}",
+        number_of_layers
+    )
 }
 
 pub fn elapsed_time_in_minutes(number_of_layers: u32, actual_minutes_in_oven: u32) -> u32 {
-    unimplemented!("calculate elapsed time in minutes for number of layers {} and actual minutes in oven {}", number_of_layers, actual_minutes_in_oven)
+    unimplemented!(
+        "calculate elapsed time in minutes for number of layers {} and actual minutes in oven {}",
+        number_of_layers,
+        actual_minutes_in_oven
+    )
 }
