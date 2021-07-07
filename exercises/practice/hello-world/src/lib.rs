91cd812c3e137f2874aad0a8d0df20f6d89a225c
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-07 11:44:01 +0200

    Simplify the `hello-world` exercise's stub. The goal of this is to make things easier for students to get started and to introduce as little syntax as possible.

--- exercises/practice/hello-world/src/lib.rs
+++ exercises/practice/hello-world/src/lib.rs
@@ -1,5 +1,3 @@
-// The &'static here means the return type has a static lifetime.
-// This is a Rust feature that you don't need to worry about now.
 pub fn hello() -> &'static str {
-    "Goodbye, World!"
+    "Goodbye, Mars!"
 }
