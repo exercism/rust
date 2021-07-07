b149d7742e82fc127d8e993c4b045c93194be66f
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-07 09:06:29 +0200

    Simplify module method calls in tests file

--- exercises/concept/lucians-luscious-lasagna/tests/lucians-luscious-lasagna.rs
+++ exercises/concept/lucians-luscious-lasagna/tests/lucians-luscious-lasagna.rs
@@ -1,34 +1,41 @@
+use lucians_luscious_lasagna::{
+    expected_minutes_in_oven,
+    remaining_minutes_in_oven,
+    preparation_time_in_minutes,
+    elapsed_time_in_minutes,
+};
+
 #[test]
-fn expected_minutes_in_oven() {
-    assert_eq!(40, lucians_luscious_lasagna::expected_minutes_in_oven());
+fn expected_minutes_in_oven_is_correct() {
+    assert_eq!(40, expected_minutes_in_oven());
 }
 
 #[ignore]
 #[test]
-fn remaining_minutes_in_oven() {
-    assert_eq!(15, lucians_luscious_lasagna::remaining_minutes_in_oven(25));
+fn remaining_minutes_in_oven_after_fifteen_minutes() {
+    assert_eq!(15, remaining_minutes_in_oven(25));
 }
 
 #[ignore]
 #[test]
 fn preparation_time_in_minutes_for_one_layer() {
-    assert_eq!(2, lucians_luscious_lasagna::preparation_time_in_minutes(1));
+    assert_eq!(2, preparation_time_in_minutes(1));
 }
 
 #[ignore]
 #[test]
 fn preparation_time_in_minutes_for_multiple_layers() {
-    assert_eq!(8, lucians_luscious_lasagna::preparation_time_in_minutes(4));
+    assert_eq!(8, preparation_time_in_minutes(4));
 }
 
 #[ignore]
 #[test]
 fn elapsed_time_in_minutes_for_one_layer() {
-    assert_eq!(32, lucians_luscious_lasagna::elapsed_time_in_minutes(1, 30));
+    assert_eq!(32, elapsed_time_in_minutes(1, 30));
 }
 
 #[ignore]
 #[test]
 fn elapsed_time_in_minutes_for_multiple_layers() {
-    assert_eq!(16, lucians_luscious_lasagna::elapsed_time_in_minutes(4, 8));
+    assert_eq!(16, elapsed_time_in_minutes(4, 8));
 }
