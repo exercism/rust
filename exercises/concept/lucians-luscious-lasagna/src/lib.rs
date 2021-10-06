20962f0b90fdb273be9544d8f0eccf900db3f859
myucesan <my96@live.nl>
2021-10-06 12:55:42 +0200

    Updated lucians-luscious-lasagna
    Added semicolons to unimplemented!(). If you try to return a value after the editor shows an error because lack of semicolon.

--- exercises/concept/lucians-luscious-lasagna/src/lib.rs
+++ exercises/concept/lucians-luscious-lasagna/src/lib.rs
@@ -3,21 +3,21 @@
 #![allow(unused)]
 
 pub fn expected_minutes_in_oven() -> i32 {
-    unimplemented!("return expected minutes in the oven")
+    unimplemented!("return expected minutes in the oven");
 }
 
 pub fn remaining_minutes_in_oven(actual_minutes_in_oven: i32) -> i32 {
     unimplemented!(
         "calculate remaining minutes in oven given actual minutes in oven: {}",
         actual_minutes_in_oven
-    )
+    );
 }
 
 pub fn preparation_time_in_minutes(number_of_layers: i32) -> i32 {
     unimplemented!(
         "calculate preparation time in minutes for number of layers: {}",
         number_of_layers
-    )
+    );
 }
 
 pub fn elapsed_time_in_minutes(number_of_layers: i32, actual_minutes_in_oven: i32) -> i32 {
@@ -25,5 +25,5 @@ pub fn elapsed_time_in_minutes(number_of_layers: i32, actual_minutes_in_oven: i3
         "calculate elapsed time in minutes for number of layers {} and actual minutes in oven {}",
         number_of_layers,
         actual_minutes_in_oven
-    )
+    );
 }
