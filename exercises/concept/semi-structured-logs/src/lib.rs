fd0a5f311d38d22b262bb2c8b99b23ee915df76d
myucesan <my96@live.nl>
2021-10-06 13:35:51 +0200

    Update rust/exercises/concept/semi-structured-logs
    Added semicolons

--- exercises/concept/semi-structured-logs/src/lib.rs
+++ exercises/concept/semi-structured-logs/src/lib.rs
@@ -11,14 +11,14 @@ pub enum LogLevel {
 }
 /// primary function for emitting logs
 pub fn log(level: LogLevel, message: &str) -> String {
-    unimplemented!()
+    unimplemented!();
 }
 pub fn info(message: &str) -> String {
-    unimplemented!()
+    unimplemented!();
 }
 pub fn warn(message: &str) -> String {
-    unimplemented!()
+    unimplemented!();
 }
 pub fn error(message: &str) -> String {
-    unimplemented!()
+    unimplemented!();
 }
