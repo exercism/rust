3a2aa18985add45c2a35be4d0933871f3ab34f18
lonox <olaf@klingt.org>
2021-10-20 02:28:19 +0800

    Update example.rs

--- exercises/practice/collatz-conjecture/.meta/example.rs
+++ exercises/practice/collatz-conjecture/.meta/example.rs
@@ -4,6 +4,6 @@ pub fn collatz(n: u64) -> Option<u64> {
         0 => None,
         1 => Some(0),
         n if n % 2 == 0 => collatz(n / 2).map(|c| c + 1),
-        n => collatz(n.checked_mul(3)?.checked_add(1)?).map(|c| c + 1)
+        n => collatz(n.checked_mul(3)?.checked_add(1)?).map(|c| c + 1),
     }
 }
