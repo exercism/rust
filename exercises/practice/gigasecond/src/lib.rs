26de5d10f12540f46d796f04e10da938001dcd1f
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-10-28 09:41:39 +0200

    gigasecond: Replace chrono dependency with time
    Rationale: see https://passcod.name/technical/no-time-for-chrono.html
    
    It is good for exercism to suggest crates which are reasonably current.

--- exercises/practice/gigasecond/src/lib.rs
+++ exercises/practice/gigasecond/src/lib.rs
@@ -1,6 +1,6 @@
-use chrono::{DateTime, Utc};
+use time::PrimitiveDateTime as DateTime;
 
-// Returns a Utc DateTime one billion seconds after start.
-pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
+// Returns a DateTime one billion seconds after start.
+pub fn after(start: DateTime) -> DateTime {
     unimplemented!("What time is a gigasecond later than {}", start);
 }
