81e95a5404d6a4a6048b7baef934a624c9ab2016
Rauno Viskus <rauno56@gmail.com>
2021-11-06 18:04:48 +0200

    Relax the precision for assembly-line tests further

--- exercises/concept/assembly-line/tests/assembly-line.rs
+++ exercises/concept/assembly-line/tests/assembly-line.rs
@@ -1,7 +1,7 @@
 fn process_rate_per_hour(speed: u8, expected_rate: f64) {
     let actual_rate = assembly_line::production_rate_per_hour(speed);
     let actual_rate = (actual_rate * 100.0).round() / 100.0;
-    assert!((actual_rate - expected_rate).abs() < f64::EPSILON);
+    assert!((actual_rate - expected_rate).abs() < 0.00001);
 }
 
 fn process_rate_per_minute(speed: u8, expected_rate: u32) {
