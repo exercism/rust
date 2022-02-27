8dd993e78be0132fcb36d69ebf11b5742f3c2404
Paweł Kwiek <49445551+pkwiek@users.noreply.github.com>
2022-02-27 21:56:44 +0100

    alphametics: add test case for too short sum (#1454)
    It was possible to pass tests without minding width of sum word. A test
    with too short sum word exists, however the equation itself is invalid
    due to leading zero, causing a lucky coincidence for some solutions.
    This patch adds a test which is invalid only due to sum word length.

--- exercises/practice/alphametics/tests/alphametics.rs
+++ exercises/practice/alphametics/tests/alphametics.rs
@@ -27,6 +27,13 @@ fn test_leading_zero_solution_is_invalid() {
 
 #[test]
 #[ignore]
+fn test_sum_must_be_wide_enough() {
+    let answer = alphametics::solve("ABC + DEF == GH");
+    assert_eq!(answer, None);
+}
+
+#[test]
+#[ignore]
 fn puzzle_with_two_digits_final_carry() {
     assert_alphametic_solution_eq(
         "A + A + A + A + A + A + A + A + A + A + A + B == BCC",
