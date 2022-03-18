fea16063d1e7a08bf9a96cbcbe6f9018a813559e
FelixGibson <99159795+FelixGibson@users.noreply.github.com>
2022-03-18 23:45:05 +0800

    Add more strict unit tests (#1468) (#1469)
    Signed-off-by: felix <jjtom5f@gmail.com>

--- exercises/practice/palindrome-products/src/lib.rs
+++ exercises/practice/palindrome-products/src/lib.rs
@@ -1,26 +1,28 @@
-#[derive(Debug, PartialEq, Eq)]
-pub struct Palindrome {
-    // implement your palindrome type here
-}
+/// `Palindrome` is a newtype which only exists when the contained value is a palindrome number in base ten.
+///
+/// A struct with a single field which is used to constrain behavior like this is called a "newtype", and its use is
+/// often referred to as the "newtype pattern". This is a fairly common pattern in Rust.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Palindrome(u64);
 
 impl Palindrome {
-    pub fn new(a: u64, b: u64) -> Palindrome {
-        unimplemented!("create a palindrome with factors ({}, {})", a, b)
+    /// Create a `Palindrome` only if `value` is in fact a palindrome when represented in base ten. Otherwise, `None`.
+    pub fn new(value: u64) -> Option<Palindrome> {
+        unimplemented!(
+            "if the value {} is a palindrome return Some, otherwise return None",
+            value
+        );
     }
 
-    pub fn value(&self) -> u64 {
-        unimplemented!("return the value of this palindrome")
-    }
-
-    pub fn insert(&mut self, a: u64, b: u64) {
-        unimplemented!("insert new factors ({}, {}) into this palindrome", a, b)
+    /// Get the value of this palindrome.
+    pub fn into_inner(self) -> u64 {
+        unimplemented!("return inner value of a Palindrome");
     }
 }
 
 pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
     unimplemented!(
-        "Find the min and max palindromic numbers which are products of numbers in the inclusive range ({}..{})",
-        min,
-        max
-    )
+        "returns the minimum and maximum number of palindromes of the products of two factors in the range {} to {}",
+        min, max
+    );
 }
