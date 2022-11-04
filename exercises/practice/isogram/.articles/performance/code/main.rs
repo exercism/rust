b49d4f9d58bead5534ef9233da41360121ba751b
Bob Hoeppner <32035397+bobahop@users.noreply.github.com>
2022-11-04 13:47:04 -0500

    Update main.rs

--- exercises/practice/isogram/.articles/performance/code/main.rs
+++ exercises/practice/isogram/.articles/performance/code/main.rs
@@ -12,8 +12,8 @@ pub fn check_hash(candidate: &str) -> bool {
     let mut hs = HashSet::new();
     candidate
         .to_lowercase()
-        .chars()
-        .filter(|&c| c.is_alphabetic())
+        .bytes()
+        .filter(|&c| c.is_ascii_alphabetic())
         .all(|c| hs.insert(c))
 }
 
