79ac6380989b5c2a8d18d351592588a733d5f815
Saurabh Mishra <saurabhm@proton.me>
2022-11-05 11:31:23 -0700

    updated method names and added missing ignore to test_name

--- exercises/concept/health-statistics/tests/health-statistics.rs
+++ exercises/concept/health-statistics/tests/health-statistics.rs
@@ -5,6 +5,7 @@ const AGE: u32 = 89;
 const WEIGHT: f32 = 131.6;
 
 #[test]
+#[ignore]
 fn test_name() {
     let user = User::new(NAME.into(), AGE, WEIGHT);
     assert_eq!(user.name(), NAME);
@@ -29,7 +30,7 @@ fn test_weight() {
 fn test_set_age() {
     let new_age: u32 = 90;
     let mut user = User::new(NAME.into(), AGE, WEIGHT);
-    user.set_age(new_age);
+    user.age_mut(new_age);
     assert_eq!(user.age(), new_age);
 }
 
@@ -38,6 +39,6 @@ fn test_set_age() {
 fn test_set_weight() {
     let new_weight: f32 = 129.4;
     let mut user = User::new(NAME.into(), AGE, WEIGHT);
-    user.set_weight(new_weight);
+    user.weight_mut(new_weight);
     assert!((user.weight() - new_weight).abs() < f32::EPSILON);
 }
