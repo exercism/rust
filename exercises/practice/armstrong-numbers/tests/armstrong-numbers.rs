98ed5670d632d385272d934998e92f40d6bd54f5
Robert Xiao <nneonneo@gmail.com>
2022-12-09 19:30:22 -0800

    Update comment and test case name
    Adding a comment and updating the test case's name to better reflect what the "fake" case is testing.

--- exercises/practice/armstrong-numbers/tests/armstrong-numbers.rs
+++ exercises/practice/armstrong-numbers/tests/armstrong-numbers.rs
@@ -71,8 +71,11 @@ fn test_ten_digit_non_armstrong_number() {
     assert!(!is_armstrong_number(3_999_999_999));
 }
 
+// The following number has an Armstrong sum equal to 2^32 plus itself,
+// and therefore will be detected as an Armstrong number if you are
+// incorrectly using wrapping arithmetic.
 #[test]
 #[ignore]
-fn test_ten_digit_fake_armstrong_number() {
+fn test_properly_handles_overflow() {
     assert!(!is_armstrong_number(4_106_098_957));
 }
