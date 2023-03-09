4bf3cd80b7496473c0778d62a47004c2b9228205
dem4ron <demaaron88@gmail.com>
2023-03-09 21:21:18 +0100

    Change casing, fix shellcheck disables, add .shekkcheckrc

--- bin/generator-utils/templates.sh
+++ bin/generator-utils/templates.sh
@@ -42,6 +42,7 @@ EOT
     else
         local canonical_json
         canonical_json=$(cat canonical_data.json)
+        local canonical_json
 
         # sometimes canonical data has multiple levels with multiple `cases` arrays.
         #(see kindergarten-garden https://github.com/exercism/problem-specifications/blob/main/exercises/kindergarten-garden/canonical-data.json)
@@ -114,6 +115,7 @@ create_example_rs_template() {
 
     local fn_name
     fn_name=$(create_fn_name "$slug" "$has_canonical_data")
+    local fn_name
 
     mkdir "${exercise_dir}/.meta"
     cat <<EOT >"${exercise_dir}/.meta/example.rs"
