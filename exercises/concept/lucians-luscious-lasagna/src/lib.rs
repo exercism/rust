f18e09768a56ccfc80c095b853de8550ac11649a
Ahmed <111569638+0Ahmed-0@users.noreply.github.com>
2023-03-27 16:26:56 +0200

    Time can not be negative, changed i32 to u32.
    It's not just we don't need negative values, but in this case it should not.

--- exercises/concept/lucians-luscious-lasagna/src/lib.rs
+++ exercises/concept/lucians-luscious-lasagna/src/lib.rs
@@ -1,18 +1,18 @@
-pub fn expected_minutes_in_oven() -> i32 {
+pub fn expected_minutes_in_oven() -> u32 {
     unimplemented!("return expected minutes in the oven")
 }
 
-pub fn remaining_minutes_in_oven(actual_minutes_in_oven: i32) -> i32 {
+pub fn remaining_minutes_in_oven(actual_minutes_in_oven: u32) -> u32 {
     unimplemented!(
         "calculate remaining minutes in oven given actual minutes in oven: {actual_minutes_in_oven}"
     )
 }
 
-pub fn preparation_time_in_minutes(number_of_layers: i32) -> i32 {
+pub fn preparation_time_in_minutes(number_of_layers: u32) -> u32 {
     unimplemented!("calculate preparation time in minutes for number of layers: {number_of_layers}")
 }
 
-pub fn elapsed_time_in_minutes(number_of_layers: i32, actual_minutes_in_oven: i32) -> i32 {
+pub fn elapsed_time_in_minutes(number_of_layers: u32, actual_minutes_in_oven: u32) -> u32 {
     unimplemented!(
         "calculate elapsed time in minutes for number of layers {number_of_layers} and actual minutes in oven {actual_minutes_in_oven}"
     )
