d14a92585050f98f3f96f4c0ec4abfe5d5ed97d6
dem4ron <demaaron88@gmail.com>
2023-04-01 23:06:11 +0200

    Add knapsack exercise

---
+++ exercises/practice/knapsack/src/lib.rs
@@ -0,0 +1,8 @@
+pub struct Item {
+    pub weight: u32,
+    pub value: u32,
+}
+
+pub fn maximum_value(max_weight: u32, items: Vec<Item>) -> u32 {
+    unimplemented!("Solve the knapsack exercise");
+}
