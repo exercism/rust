c2b4c75ff794dd9dae992a542b6cf8ae34680291
dem4ron <demaaron88@gmail.com>
2023-04-01 23:14:55 +0200

    Underscore unused params, turn string diff to int diff

--- exercises/practice/knapsack/src/lib.rs
+++ exercises/practice/knapsack/src/lib.rs
@@ -3,6 +3,6 @@ pub struct Item {
     pub value: u32,
 }
 
-pub fn maximum_value(max_weight: u32, items: Vec<Item>) -> u32 {
+pub fn maximum_value(_max_weight: u32, _items: Vec<Item>) -> u32 {
     unimplemented!("Solve the knapsack exercise");
 }
