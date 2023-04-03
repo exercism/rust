88fb1c44c2c83b0cd601ef5a42f943be44e3785e
dem4ron <demaaron88@gmail.com>
2023-04-03 17:40:21 +0200

    Simplify escape double quotes algo

--- util/escape_double_quotes/src/utils/escape_double_quotes.rs
+++ util/escape_double_quotes/src/utils/escape_double_quotes.rs
@@ -1,19 +1,19 @@
 pub fn escape_double_quotes(input: &str) -> String {
     let mut output = String::new();
-    let mut brace_type = None;
+    let mut escape = true;
 
     let mut chars = input.chars().peekable();
     while let Some(char) = chars.next() {
         match char {
             '$' if chars.peek() == Some(&'{') => {
-                brace_type = Some("${");
+                escape = false;
                 output.push(char)
             }
             '}' if chars.peek() == Some(&'$') => {
-                brace_type = Some("}$");
+                escape = true;
                 output.push(char)
             }
-            '"' if brace_type == Some("}$") || brace_type == None => output.push_str("\\\""),
+            '"' if escape => output.push_str("\\\""),
             _ => output.push(char),
         }
     }
