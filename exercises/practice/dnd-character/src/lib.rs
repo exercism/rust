82e2081eb2ae187d733d854f70b58255a49a0a5f
dem4ron <demaaron88@gmail.com>
2023-04-07 23:03:39 +0200

    Fill in struct details in stub file

--- exercises/practice/dnd-character/src/lib.rs
+++ exercises/practice/dnd-character/src/lib.rs
@@ -2,7 +2,15 @@ pub fn modifier(_ability_score: u8) -> f32 {
     unimplemented!("Implement a utility function that calculates the ability modifier for a given ability score")
 }
 
-pub struct Character {}
+pub struct Character {
+    pub strength: u8,
+    pub dexterity: u8,
+    pub constitution: u8,
+    pub intelligence: u8,
+    pub wisdom: u8,
+    pub charisma: u8,
+    pub hitpoints: u8,
+}
 
 impl Character {
     pub fn new() -> Self {
