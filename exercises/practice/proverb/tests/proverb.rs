66f5fd5a1782830a6deeae97ba845b05fe5bba71
Remo Senekowitsch <remo@buenzli.dev>
2023-07-20 12:04:05 +0200

    Fix new clippy warnings

--- exercises/practice/proverb/tests/proverb.rs
+++ exercises/practice/proverb/tests/proverb.rs
@@ -3,7 +3,7 @@ use proverb::build_proverb;
 #[test]
 fn test_two_pieces() {
     let input = vec!["nail", "shoe"];
-    let expected = vec![
+    let expected = [
         "For want of a nail the shoe was lost.",
         "And all for the want of a nail.",
     ]
@@ -16,7 +16,7 @@ fn test_two_pieces() {
 #[ignore]
 fn test_three_pieces() {
     let input = vec!["nail", "shoe", "horse"];
-    let expected = vec![
+    let expected = [
         "For want of a nail the shoe was lost.",
         "For want of a shoe the horse was lost.",
         "And all for the want of a nail.",
@@ -47,7 +47,7 @@ fn test_full() {
     let input = vec![
         "nail", "shoe", "horse", "rider", "message", "battle", "kingdom",
     ];
-    let expected = vec![
+    let expected = [
         "For want of a nail the shoe was lost.",
         "For want of a shoe the horse was lost.",
         "For want of a horse the rider was lost.",
@@ -64,7 +64,7 @@ fn test_full() {
 #[ignore]
 fn test_three_pieces_modernized() {
     let input = vec!["pin", "gun", "soldier", "battle"];
-    let expected = vec![
+    let expected = [
         "For want of a pin the gun was lost.",
         "For want of a gun the soldier was lost.",
         "For want of a soldier the battle was lost.",
