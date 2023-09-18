d9406c4e456cfb1d2758f27fe4f01e94b0ecedb4
Remo Senekowitsch <remo@buenzli.dev>
2023-09-18 22:14:51 +0200

    Sync pascals-triangle with problem-specifications (#1743)

--- exercises/practice/pascals-triangle/tests/pascals-triangle.rs
+++ exercises/practice/pascals-triangle/tests/pascals-triangle.rs
@@ -1,18 +1,18 @@
-use pascals_triangle::*;
+use pascals_triangle::PascalsTriangle;
 
 #[test]
-fn no_rows() {
+fn zero_rows() {
     let pt = PascalsTriangle::new(0);
-    let expected: Vec<Vec<u32>> = Vec::new();
-    assert_eq!(expected, pt.rows());
+    let expected: Vec<Vec<u32>> = vec![];
+    assert_eq!(pt.rows(), expected);
 }
 
 #[test]
 #[ignore]
-fn one_row() {
+fn single_row() {
     let pt = PascalsTriangle::new(1);
     let expected: Vec<Vec<u32>> = vec![vec![1]];
-    assert_eq!(expected, pt.rows());
+    assert_eq!(pt.rows(), expected);
 }
 
 #[test]
@@ -20,7 +20,7 @@ fn one_row() {
 fn two_rows() {
     let pt = PascalsTriangle::new(2);
     let expected: Vec<Vec<u32>> = vec![vec![1], vec![1, 1]];
-    assert_eq!(expected, pt.rows());
+    assert_eq!(pt.rows(), expected);
 }
 
 #[test]
@@ -28,15 +28,15 @@ fn two_rows() {
 fn three_rows() {
     let pt = PascalsTriangle::new(3);
     let expected: Vec<Vec<u32>> = vec![vec![1], vec![1, 1], vec![1, 2, 1]];
-    assert_eq!(expected, pt.rows());
+    assert_eq!(pt.rows(), expected);
 }
 
 #[test]
 #[ignore]
-fn last_of_four_rows() {
+fn four_rows() {
     let pt = PascalsTriangle::new(4);
-    let expected: Vec<u32> = vec![1, 3, 3, 1];
-    assert_eq!(Some(expected), pt.rows().pop());
+    let expected: Vec<Vec<u32>> = vec![vec![1], vec![1, 1], vec![1, 2, 1], vec![1, 3, 3, 1]];
+    assert_eq!(pt.rows(), expected);
 }
 
 #[test]
@@ -50,7 +50,7 @@ fn five_rows() {
         vec![1, 3, 3, 1],
         vec![1, 4, 6, 4, 1],
     ];
-    assert_eq!(expected, pt.rows());
+    assert_eq!(pt.rows(), expected);
 }
 
 #[test]
@@ -65,23 +65,7 @@ fn six_rows() {
         vec![1, 4, 6, 4, 1],
         vec![1, 5, 10, 10, 5, 1],
     ];
-    assert_eq!(expected, pt.rows());
-}
-
-#[test]
-#[ignore]
-fn seven_rows() {
-    let pt = PascalsTriangle::new(7);
-    let expected: Vec<Vec<u32>> = vec![
-        vec![1],
-        vec![1, 1],
-        vec![1, 2, 1],
-        vec![1, 3, 3, 1],
-        vec![1, 4, 6, 4, 1],
-        vec![1, 5, 10, 10, 5, 1],
-        vec![1, 6, 15, 20, 15, 6, 1],
-    ];
-    assert_eq!(expected, pt.rows());
+    assert_eq!(pt.rows(), expected);
 }
 
 #[test]
@@ -100,5 +84,5 @@ fn ten_rows() {
         vec![1, 8, 28, 56, 70, 56, 28, 8, 1],
         vec![1, 9, 36, 84, 126, 126, 84, 36, 9, 1],
     ];
-    assert_eq!(expected, pt.rows());
+    assert_eq!(pt.rows(), expected);
 }
