8ce6ec089236178a03791d880a072d9e2f7c6e83
Remo Senekowitsch <remo@buenzli.dev>
2023-09-18 19:40:36 +0200

    cleanup test script
    Most importantly, move all required files for testing to a tmp dir.
    This means the actual files in the repository are not modified anymore.
    Running these scripts locally does not interrupt the git workflow now.
    
    The output is also cleaned up in various situations.

--- bin/check_exercises.sh
+++ bin/check_exercises.sh
@@ -1,66 +1,45 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-# can't benchmark with a stable compiler; to bench, use
-# $ BENCHMARK=1 rustup run nightly bin/check_exercises.sh
-if [ -n "$BENCHMARK" ]; then
-    target_dir=benches
-else
-    target_dir=tests
-fi
-
 repo=$(git rev-parse --show-toplevel)
 
 if [ "$GITHUB_EVENT_NAME" = "pull_request" ]; then
     git fetch --depth=1 origin main
     files="$(
         git diff --diff-filter=d --name-only remotes/origin/main |
-        grep "exercises/" || true |
-        cut -d '/' -f -3 |
-        sort -u |
-        awk -v repo="$repo" '{print repo"/"$1}'
+        grep "exercises/" |
+        cut --delimiter '/' --fields -3 |
+        sort --unique || true
     )"
 else
     files="$repo/exercises/*/*"
 fi
 
-return_code=0
 # An exercise worth testing is defined here as any top level directory with
 # a 'tests' directory and a .meta/config.json.
 for exercise in $files; do
-    exercise="$exercise/$target_dir"
-
-    # This assumes that exercises are only one directory deep
-    # and that the primary module is named the same as the directory
-    directory=$(dirname "${exercise}")
+    slug=$(basename "$exercise")
 
     # An exercise must have a .meta/config.json
-    metaconf="$directory/.meta/config.json"
+    metaconf="$exercise/.meta/config.json"
     if [ ! -f "$metaconf" ]; then
         continue
     fi
 
-    release=""
-    if [ -z "$BENCHMARK" ] && jq --exit-status '.custom?."test-in-release-mode"?' "$metaconf"; then
+    # suppress compilation output
+    cargo_args="--quiet"
+
+    if [ -z "$BENCHMARK" ] && jq --exit-status '.custom?."test-in-release-mode"?' "$metaconf" > /dev/null; then
         # release mode is enabled if not benchmarking and the appropriate key is neither `false` nor `null`.
-        release="--release"
+        cargo_args="$cargo_args --release"
     fi
 
-    if [ -n "$DENYWARNINGS" ]; then
-        # Output a progress dot, because we may not otherwise produce output in > 10 mins.
-        echo -n '.'
+    if [ -n "$DENYWARNINGS" ] && [ -z "$CLIPPY" ]; then
         # No-run mode so we see no test output.
-        # Quiet mode so we see no compile output
-        # (such as "Compiling"/"Downloading").
-        # Compiler errors will still be shown though.
-        # Both flags are necessary to keep things quiet.
-        ./bin/test_exercise.sh "$directory" --quiet --no-run
-        return_code=$((return_code | $?))
-    else
-        # Run the test and get the status
-        ./bin/test_exercise.sh "$directory" $release
-        return_code=$((return_code | $?))
+        # clippy does not understand this flag.
+        cargo_args="$cargo_args --no-run"
     fi
-done
 
-exit $return_code
+    echo "Checking $slug..."
+    ./bin/test_exercise.sh "$slug" "$cargo_args"
+done
