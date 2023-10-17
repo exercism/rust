6e9e9cc57d8f861262ae0c848c89ffe916264aa3
Remo Senekowitsch <remo@buenzli.dev>
2023-10-17 17:26:15 +0200

    Fix lifetime annotation in anagram stub (#1755)
    [no important files changed]

--- exercises/practice/anagram/src/lib.rs
+++ exercises/practice/anagram/src/lib.rs
@@ -1,5 +1,5 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
     todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
 }
