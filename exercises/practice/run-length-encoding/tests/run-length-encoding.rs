53c2e832691d78a6199644e15df22edd056e2651
Remo Senekowitsch <remo@buenzli.dev>
2023-11-18 00:31:06 +0100

    Sync run-length-encoding with problem-specifications

--- exercises/practice/run-length-encoding/tests/run-length-encoding.rs
+++ exercises/practice/run-length-encoding/tests/run-length-encoding.rs
@@ -1,93 +1,117 @@
 use run_length_encoding as rle;
 
-// encoding tests
-
 #[test]
 fn encode_empty_string() {
-    assert_eq!("", rle::encode(""));
+    let input = "";
+    let output = rle::encode(input);
+    let expected = "";
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn encode_single_characters() {
-    assert_eq!("XYZ", rle::encode("XYZ"));
+fn encode_single_characters_only_are_encoded_without_count() {
+    let input = "XYZ";
+    let output = rle::encode(input);
+    let expected = "XYZ";
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn encode_string_with_no_single_characters() {
-    assert_eq!("2A3B4C", rle::encode("AABBBCCCC"));
+    let input = "AABBBCCCC";
+    let output = rle::encode(input);
+    let expected = "2A3B4C";
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn encode_single_characters_mixed_with_repeated_characters() {
-    assert_eq!(
-        "12WB12W3B24WB",
-        rle::encode("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB")
-    );
+    let input = "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB";
+    let output = rle::encode(input);
+    let expected = "12WB12W3B24WB";
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn encode_multiple_whitespace_mixed_in_string() {
-    assert_eq!("2 hs2q q2w2 ", rle::encode("  hsqq qww  "));
+    let input = "  hsqq qww  ";
+    let output = rle::encode(input);
+    let expected = "2 hs2q q2w2 ";
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn encode_lowercase_characters() {
-    assert_eq!("2a3b4c", rle::encode("aabbbcccc"));
+    let input = "aabbbcccc";
+    let output = rle::encode(input);
+    let expected = "2a3b4c";
+    assert_eq!(output, expected);
 }
-
-// decoding tests
 
 #[test]
 #[ignore]
 fn decode_empty_string() {
-    assert_eq!("", rle::decode(""));
+    let input = "";
+    let output = rle::decode(input);
+    let expected = "";
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn decode_single_characters_only() {
-    assert_eq!("XYZ", rle::decode("XYZ"));
+    let input = "XYZ";
+    let output = rle::decode(input);
+    let expected = "XYZ";
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn decode_string_with_no_single_characters() {
-    assert_eq!("AABBBCCCC", rle::decode("2A3B4C"));
+    let input = "2A3B4C";
+    let output = rle::decode(input);
+    let expected = "AABBBCCCC";
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn decode_single_characters_with_repeated_characters() {
-    assert_eq!(
-        "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB",
-        rle::decode("12WB12W3B24WB")
-    );
+    let input = "12WB12W3B24WB";
+    let output = rle::decode(input);
+    let expected = "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB";
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn decode_multiple_whitespace_mixed_in_string() {
-    assert_eq!("  hsqq qww  ", rle::decode("2 hs2q q2w2 "));
+    let input = "2 hs2q q2w2 ";
+    let output = rle::decode(input);
+    let expected = "  hsqq qww  ";
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn decode_lower_case_string() {
-    assert_eq!("aabbbcccc", rle::decode("2a3b4c"));
+fn decode_lowercase_string() {
+    let input = "2a3b4c";
+    let output = rle::decode(input);
+    let expected = "aabbbcccc";
+    assert_eq!(output, expected);
 }
-
-// consistency test
 
 #[test]
 #[ignore]
-fn consistency() {
-    assert_eq!(
-        "zzz ZZ  zZ",
-        rle::decode(rle::encode("zzz ZZ  zZ").as_str())
-    );
+fn consistency_encode_followed_by_decode_gives_original_string() {
+    let input = "zzz ZZ  zZ";
+    let output = rle::decode(&rle::encode(input));
+    let expected = "zzz ZZ  zZ";
+    assert_eq!(output, expected);
 }
