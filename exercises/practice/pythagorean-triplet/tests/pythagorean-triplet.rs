3e3603c9bcfa55ff3bd3100a4d69aa4144a467aa
Remo Senekowitsch <remo@buenzli.dev>
2023-11-19 22:04:30 +0100

    Sync pythagorean-triplet with problem-specifications

--- exercises/practice/pythagorean-triplet/tests/pythagorean-triplet.rs
+++ exercises/practice/pythagorean-triplet/tests/pythagorean-triplet.rs
@@ -1,76 +1,85 @@
-use pythagorean_triplet::find;
 use std::collections::HashSet;
-
-fn process_tripletswithsum_case(sum: u32, expected: &[[u32; 3]]) {
-    let triplets = find(sum);
-
-    if !expected.is_empty() {
-        let expected: HashSet<_> = expected.iter().cloned().collect();
-
-        assert_eq!(expected, triplets);
-    } else {
-        assert!(triplets.is_empty());
-    }
-}
 
 #[test]
 fn triplets_whose_sum_is_12() {
-    process_tripletswithsum_case(12, &[[3, 4, 5]]);
+    let input = 12;
+    let output = pythagorean_triplet::find(input);
+    let expected = [[3, 4, 5]];
+    let expected: HashSet<_> = expected.iter().cloned().collect();
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn triplets_whose_sum_is_108() {
-    process_tripletswithsum_case(108, &[[27, 36, 45]]);
+    let input = 108;
+    let output = pythagorean_triplet::find(input);
+    let expected = [[27, 36, 45]];
+    let expected: HashSet<_> = expected.iter().cloned().collect();
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn triplets_whose_sum_is_1000() {
-    process_tripletswithsum_case(1000, &[[200, 375, 425]]);
+    let input = 1000;
+    let output = pythagorean_triplet::find(input);
+    let expected = [[200, 375, 425]];
+    let expected: HashSet<_> = expected.iter().cloned().collect();
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn no_matching_triplets_for_1001() {
-    process_tripletswithsum_case(1001, &[]);
+    let input = 1001;
+    let output = pythagorean_triplet::find(input);
+    let expected = [];
+    let expected: HashSet<_> = expected.iter().cloned().collect();
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn returns_all_matching_triplets() {
-    process_tripletswithsum_case(90, &[[9, 40, 41], [15, 36, 39]]);
+    let input = 90;
+    let output = pythagorean_triplet::find(input);
+    let expected = [[9, 40, 41], [15, 36, 39]];
+    let expected: HashSet<_> = expected.iter().cloned().collect();
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn several_matching_triplets() {
-    process_tripletswithsum_case(
-        840,
-        &[
-            [40, 399, 401],
-            [56, 390, 394],
-            [105, 360, 375],
-            [120, 350, 370],
-            [140, 336, 364],
-            [168, 315, 357],
-            [210, 280, 350],
-            [240, 252, 348],
-        ],
-    );
+    let input = 840;
+    let output = pythagorean_triplet::find(input);
+    let expected = [
+        [40, 399, 401],
+        [56, 390, 394],
+        [105, 360, 375],
+        [120, 350, 370],
+        [140, 336, 364],
+        [168, 315, 357],
+        [210, 280, 350],
+        [240, 252, 348],
+    ];
+    let expected: HashSet<_> = expected.iter().cloned().collect();
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn triplets_for_large_number() {
-    process_tripletswithsum_case(
-        30_000,
-        &[
-            [1200, 14_375, 14_425],
-            [1875, 14_000, 14_125],
-            [5000, 12_000, 13_000],
-            [6000, 11_250, 12_750],
-            [7500, 10_000, 12_500],
-        ],
-    );
+    let input = 30000;
+    let output = pythagorean_triplet::find(input);
+    let expected = [
+        [1200, 14375, 14425],
+        [1875, 14000, 14125],
+        [5000, 12000, 13000],
+        [6000, 11250, 12750],
+        [7500, 10000, 12500],
+    ];
+    let expected: HashSet<_> = expected.iter().cloned().collect();
+    assert_eq!(output, expected);
 }
