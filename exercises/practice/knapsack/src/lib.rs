25cd3322b2f4a9c541abec6785a3c7e1725b061b
Remo Senekowitsch <remo@buenzli.dev>
2023-12-17 22:34:04 +0100

    Fix compilation failure in stub

--- exercises/practice/knapsack/src/lib.rs
+++ exercises/practice/knapsack/src/lib.rs
@@ -4,5 +4,5 @@ pub struct Item {
 }
 
 pub fn maximum_value(max_weight: u32, items: &[Item]) -> u32 {
-    todo!("calculate the maximum value achievable with the given {items} and {max_weight}");
+    todo!("calculate the maximum value achievable with the given {items:?} and {max_weight}");
 }
