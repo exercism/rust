a5c96c48cf2a0abcec14f8f87ad8e3b6e3f3eb78
Remo Senekowitsch <remo@buenzli.dev>
2023-12-19 13:12:03 +0100

    Sync prime-factors with problem-specifications (#1832)

--- exercises/practice/prime-factors/tests/prime-factors.rs
+++ exercises/practice/prime-factors/tests/prime-factors.rs
@@ -1,42 +1,96 @@
-use prime_factors::factors;
+use prime_factors::*;
 
 #[test]
 fn no_factors() {
-    assert_eq!(factors(1), vec![]);
+    let factors = factors(1);
+    let expected = [];
+    assert_eq!(factors, expected);
 }
 
 #[test]
 #[ignore]
 fn prime_number() {
-    assert_eq!(factors(2), vec![2]);
+    let factors = factors(2);
+    let expected = [2];
+    assert_eq!(factors, expected);
+}
+
+#[test]
+#[ignore]
+fn another_prime_number() {
+    let factors = factors(3);
+    let expected = [3];
+    assert_eq!(factors, expected);
 }
 
 #[test]
 #[ignore]
 fn square_of_a_prime() {
-    assert_eq!(factors(9), vec![3, 3]);
+    let factors = factors(9);
+    let expected = [3, 3];
+    assert_eq!(factors, expected);
+}
+
+#[test]
+#[ignore]
+fn product_of_first_prime() {
+    let factors = factors(4);
+    let expected = [2, 2];
+    assert_eq!(factors, expected);
 }
 
 #[test]
 #[ignore]
 fn cube_of_a_prime() {
-    assert_eq!(factors(8), vec![2, 2, 2]);
+    let factors = factors(8);
+    let expected = [2, 2, 2];
+    assert_eq!(factors, expected);
+}
+
+#[test]
+#[ignore]
+fn product_of_second_prime() {
+    let factors = factors(27);
+    let expected = [3, 3, 3];
+    assert_eq!(factors, expected);
+}
+
+#[test]
+#[ignore]
+fn product_of_third_prime() {
+    let factors = factors(625);
+    let expected = [5, 5, 5, 5];
+    assert_eq!(factors, expected);
+}
+
+#[test]
+#[ignore]
+fn product_of_first_and_second_prime() {
+    let factors = factors(6);
+    let expected = [2, 3];
+    assert_eq!(factors, expected);
 }
 
 #[test]
 #[ignore]
 fn product_of_primes_and_non_primes() {
-    assert_eq!(factors(12), vec![2, 2, 3]);
+    let factors = factors(12);
+    let expected = [2, 2, 3];
+    assert_eq!(factors, expected);
 }
 
 #[test]
 #[ignore]
 fn product_of_primes() {
-    assert_eq!(factors(901_255), vec![5, 17, 23, 461]);
+    let factors = factors(901255);
+    let expected = [5, 17, 23, 461];
+    assert_eq!(factors, expected);
 }
 
 #[test]
 #[ignore]
-fn factors_include_large_prime() {
-    assert_eq!(factors(93_819_012_551), vec![11, 9539, 894_119]);
+fn factors_include_a_large_prime() {
+    let factors = factors(93819012551);
+    let expected = [11, 9539, 894119];
+    assert_eq!(factors, expected);
 }
