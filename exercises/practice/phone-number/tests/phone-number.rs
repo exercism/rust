0cece7a95131d2541dc59bfb14ec9fec423fdb7b
Remo Senekowitsch <remo@buenzli.dev>
2023-12-19 13:43:41 +0100

    Sync phone-number with problem-specifications (#1835)
    [no important files changed]

--- exercises/practice/phone-number/tests/phone-number.rs
+++ exercises/practice/phone-number/tests/phone-number.rs
@@ -1,112 +1,149 @@
-use phone_number as phone;
-
-fn process_clean_case(number: &str, expected: Option<&str>) {
-    assert_eq!(phone::number(number), expected.map(|x| x.to_string()));
-}
+use phone_number::*;
 
 #[test]
 fn cleans_the_number() {
-    process_clean_case("(223) 456-7890", Some("2234567890"));
+    let input = "(223) 456-7890";
+    let output = number(input);
+    let expected = Some("2234567890".into());
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn cleans_numbers_with_dots() {
-    process_clean_case("223.456.7890", Some("2234567890"));
+    let input = "223.456.7890";
+    let output = number(input);
+    let expected = Some("2234567890".into());
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn cleans_numbers_with_multiple_spaces() {
-    process_clean_case("223 456   7890   ", Some("2234567890"));
+    let input = "223 456   7890   ";
+    let output = number(input);
+    let expected = Some("2234567890".into());
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn invalid_when_9_digits() {
-    process_clean_case("123456789", None);
+    let input = "123456789";
+    let output = number(input);
+    assert!(output.is_none());
 }
 
 #[test]
 #[ignore]
 fn invalid_when_11_digits_does_not_start_with_a_1() {
-    process_clean_case("22234567890", None);
+    let input = "22234567890";
+    let output = number(input);
+    assert!(output.is_none());
 }
 
 #[test]
 #[ignore]
 fn valid_when_11_digits_and_starting_with_1() {
-    process_clean_case("12234567890", Some("2234567890"));
+    let input = "12234567890";
+    let output = number(input);
+    let expected = Some("2234567890".into());
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn valid_when_11_digits_and_starting_with_1_even_with_punctuation() {
-    process_clean_case("+1 (223) 456-7890", Some("2234567890"));
+    let input = "+1 (223) 456-7890";
+    let output = number(input);
+    let expected = Some("2234567890".into());
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn invalid_when_more_than_11_digits() {
-    process_clean_case("321234567890", None);
+    let input = "321234567890";
+    let output = number(input);
+    assert!(output.is_none());
 }
 
 #[test]
 #[ignore]
 fn invalid_with_letters() {
-    process_clean_case("123-abc-7890", None);
+    let input = "523-abc-7890";
+    let output = number(input);
+    assert!(output.is_none());
 }
 
 #[test]
 #[ignore]
 fn invalid_with_punctuations() {
-    process_clean_case("123-@:!-7890", None);
-}
-
-#[test]
-#[ignore]
-fn invalid_if_area_code_starts_with_1_on_valid_11digit_number() {
-    process_clean_case("1 (123) 456-7890", None);
-}
-
-#[test]
-#[ignore]
-fn invalid_if_area_code_starts_with_0_on_valid_11digit_number() {
-    process_clean_case("1 (023) 456-7890", None);
-}
-
-#[test]
-#[ignore]
-fn invalid_if_area_code_starts_with_1() {
-    process_clean_case("(123) 456-7890", None);
-}
-
-#[test]
-#[ignore]
-fn invalid_if_exchange_code_starts_with_1() {
-    process_clean_case("(223) 156-7890", None);
-}
-
-#[test]
-#[ignore]
-fn invalid_if_exchange_code_starts_with_0() {
-    process_clean_case("(223) 056-7890", None);
-}
-
-#[test]
-#[ignore]
-fn invalid_if_exchange_code_starts_with_1_on_valid_11digit_number() {
-    process_clean_case("1 (223) 156-7890", None);
-}
-
-#[test]
-#[ignore]
-fn invalid_if_exchange_code_starts_with_0_on_valid_11digit_number() {
-    process_clean_case("1 (223) 056-7890", None);
+    let input = "523-@:!-7890";
+    let output = number(input);
+    assert!(output.is_none());
 }
 
 #[test]
 #[ignore]
 fn invalid_if_area_code_starts_with_0() {
-    process_clean_case("(023) 456-7890", None);
+    let input = "(023) 456-7890";
+    let output = number(input);
+    assert!(output.is_none());
+}
+
+#[test]
+#[ignore]
+fn invalid_if_area_code_starts_with_1() {
+    let input = "(123) 456-7890";
+    let output = number(input);
+    assert!(output.is_none());
+}
+
+#[test]
+#[ignore]
+fn invalid_if_exchange_code_starts_with_0() {
+    let input = "(223) 056-7890";
+    let output = number(input);
+    assert!(output.is_none());
+}
+
+#[test]
+#[ignore]
+fn invalid_if_exchange_code_starts_with_1() {
+    let input = "(223) 156-7890";
+    let output = number(input);
+    assert!(output.is_none());
+}
+
+#[test]
+#[ignore]
+fn invalid_if_area_code_starts_with_0_on_valid_11_digit_number() {
+    let input = "1 (023) 456-7890";
+    let output = number(input);
+    assert!(output.is_none());
+}
+
+#[test]
+#[ignore]
+fn invalid_if_area_code_starts_with_1_on_valid_11_digit_number() {
+    let input = "1 (123) 456-7890";
+    let output = number(input);
+    assert!(output.is_none());
+}
+
+#[test]
+#[ignore]
+fn invalid_if_exchange_code_starts_with_0_on_valid_11_digit_number() {
+    let input = "1 (223) 056-7890";
+    let output = number(input);
+    assert!(output.is_none());
+}
+
+#[test]
+#[ignore]
+fn invalid_if_exchange_code_starts_with_1_on_valid_11_digit_number() {
+    let input = "1 (223) 156-7890";
+    let output = number(input);
+    assert!(output.is_none());
 }
