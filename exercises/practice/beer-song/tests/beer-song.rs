7f808cdbb1c8419cb00e01e792a87a82ee7c3c87
Remo Senekowitsch <remo@buenzli.dev>
2024-01-23 17:46:27 +0100

    wip

--- exercises/practice/beer-song/tests/beer-song.rs
+++ exercises/practice/beer-song/tests/beer-song.rs
@@ -34,3 +34,6 @@ fn song_8_6() {
 fn song_3_0() {
     assert_eq!(beer::sing(3, 0), "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n\n2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n\n1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n");
 }
+
+// This is a change that should trigger the comment.
+// The commit was pushed after the PR was already created.
