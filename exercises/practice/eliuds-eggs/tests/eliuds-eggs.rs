d04c778daf0ecafa36234217815861cbe358d88b
Remo Senekowitsch <remo@buenzli.dev>
2024-03-24 20:03:48 +0100

    Add exercise eliuds-eggs

---
+++ exercises/practice/eliuds-eggs/tests/eliuds-eggs.rs
@@ -0,0 +1,36 @@
+use eliuds_eggs::*;
+
+#[test]
+fn test_0_eggs() {
+    let input = 0;
+    let output = egg_count(input);
+    let expected = 0;
+    assert_eq!(output, expected);
+}
+
+#[test]
+#[ignore]
+fn test_1_egg() {
+    let input = 16;
+    let output = egg_count(input);
+    let expected = 1;
+    assert_eq!(output, expected);
+}
+
+#[test]
+#[ignore]
+fn test_4_eggs() {
+    let input = 89;
+    let output = egg_count(input);
+    let expected = 4;
+    assert_eq!(output, expected);
+}
+
+#[test]
+#[ignore]
+fn test_13_eggs() {
+    let input = 2000000000;
+    let output = egg_count(input);
+    let expected = 13;
+    assert_eq!(output, expected);
+}
