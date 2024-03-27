7192672cf32f25eb0c50b509c74a27339fa62e20
jpal91 <pallanschja@gmail.com>
2024-03-27 09:17:49 -0400

    updated example.rs

--- exercises/practice/matrix/.meta/example.rs
+++ exercises/practice/matrix/.meta/example.rs
@@ -6,22 +6,14 @@ impl Matrix {
     pub fn new(input: &str) -> Self {
         let grid = input
             .lines()
-            .map(|l| {
-                l.split(" ")
-                    .map(|n| n.parse::<u32>().unwrap())
-                    .collect()
-            })
+            .map(|l| l.split(' ').map(|n| n.parse::<u32>().unwrap()).collect())
             .collect();
 
         Self { grid }
     }
 
     pub fn row(&self, row_no: usize) -> Option<Vec<u32>> {
-        if row_no > self.grid.len() {
-            return None;
-        };
-
-        Some(self.grid[row_no - 1].to_owned())
+        self.grid.get(row_no - 1).map(|row| row.to_owned())
     }
 
     pub fn column(&self, col_no: usize) -> Option<Vec<u32>> {
