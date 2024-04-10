c1c8c4d2b8c80a6e00b6c00da2e614461917be37
Remo Senekowitsch <remo@buenzli.dev>
2024-04-10 19:18:27 +0200

    pangram: sync

--- exercises/practice/pangram/tests/pangram.rs
+++ exercises/practice/pangram/tests/pangram.rs
@@ -1,70 +1,70 @@
 use pangram::*;
 
 #[test]
-fn empty_strings_are_not_pangrams() {
+fn empty_sentence() {
     let sentence = "";
     assert!(!is_pangram(sentence));
 }
 
 #[test]
 #[ignore]
-fn classic_pangram_is_a_pangram() {
+fn perfect_lower_case() {
+    let sentence = "abcdefghijklmnopqrstuvwxyz";
+    assert!(is_pangram(sentence));
+}
+
+#[test]
+#[ignore]
+fn only_lower_case() {
     let sentence = "the quick brown fox jumps over the lazy dog";
     assert!(is_pangram(sentence));
 }
 
 #[test]
 #[ignore]
-fn pangrams_must_have_all_letters() {
+fn missing_the_letter_x() {
     let sentence = "a quick movement of the enemy will jeopardize five gunboats";
     assert!(!is_pangram(sentence));
 }
 
 #[test]
 #[ignore]
-fn pangrams_must_have_all_letters_two() {
-    let sentence = "the quick brown fish jumps over the lazy dog";
+fn missing_the_letter_h() {
+    let sentence = "five boxing wizards jump quickly at it";
     assert!(!is_pangram(sentence));
 }
 
 #[test]
 #[ignore]
-fn pangrams_must_include_z() {
-    let sentence = "the quick brown fox jumps over the lay dog";
-    assert!(!is_pangram(sentence));
-}
-
-#[test]
-#[ignore]
-fn underscores_do_not_affect_pangrams() {
+fn with_underscores() {
     let sentence = "the_quick_brown_fox_jumps_over_the_lazy_dog";
     assert!(is_pangram(sentence));
 }
 
 #[test]
 #[ignore]
-fn numbers_do_not_affect_pangrams() {
+fn with_numbers() {
     let sentence = "the 1 quick brown fox jumps over the 2 lazy dogs";
     assert!(is_pangram(sentence));
 }
 
 #[test]
 #[ignore]
-fn numbers_can_not_replace_letters() {
+fn missing_letters_replaced_by_numbers() {
     let sentence = "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog";
     assert!(!is_pangram(sentence));
 }
 
 #[test]
 #[ignore]
-fn capitals_and_punctuation_can_be_in_pangrams() {
+fn mixed_case_and_punctuation() {
     let sentence = "\"Five quacking Zephyrs jolt my wax bed.\"";
     assert!(is_pangram(sentence));
 }
 
 #[test]
 #[ignore]
-fn non_ascii_characters_can_be_in_pangrams() {
-    let sentence = "Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich.";
-    assert!(is_pangram(sentence));
+fn a_m_and_a_m_are_26_different_characters_but_not_a_pangram() {
+    let sentence = "abcdefghijklm ABCDEFGHIJKLM";
+    assert!(!is_pangram(sentence));
 }
