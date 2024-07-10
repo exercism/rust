f501025bfba9f4df28643e9d4a782d352e3fffbd
Remo Senekowitsch <remo@buenzli.dev>
2024-07-10 06:46:43 +0200

    isogram: sync

--- exercises/practice/isogram/tests/isogram.rs
+++ exercises/practice/isogram/tests/isogram.rs
@@ -1,75 +1,84 @@
-use isogram::check;
+use isogram::*;
 
 #[test]
 fn empty_string() {
-    assert!(check(""), "An empty string should be an isogram.")
+    assert!(check(""));
 }
 
 #[test]
 #[ignore]
-fn only_lower_case_characters() {
-    assert!(check("isogram"), "\"isogram\" should be an isogram.")
+fn isogram_with_only_lower_case_characters() {
+    assert!(check("isogram"));
 }
 
 #[test]
 #[ignore]
-fn one_duplicated_character() {
-    assert!(
-        !check("eleven"),
-        "\"eleven\" has more than one \'e\', therefore it is no isogram."
-    )
+fn word_with_one_duplicated_character() {
+    assert!(!check("eleven"));
+}
+
+#[test]
+#[ignore]
+fn word_with_one_duplicated_character_from_the_end_of_the_alphabet() {
+    assert!(!check("zzyzx"));
 }
 
 #[test]
 #[ignore]
 fn longest_reported_english_isogram() {
-    assert!(
-        check("subdermatoglyphic"),
-        "\"subdermatoglyphic\" should be an isogram."
-    )
+    assert!(check("subdermatoglyphic"));
 }
 
 #[test]
 #[ignore]
-fn one_duplicated_character_mixed_case() {
-    assert!(
-        !check("Alphabet"),
-        "\"Alphabet\" has more than one \'a\', therefore it is no isogram."
-    )
+fn word_with_duplicated_character_in_mixed_case() {
+    assert!(!check("Alphabet"));
 }
 
 #[test]
 #[ignore]
-fn hypothetical_isogramic_word_with_hyphen() {
-    assert!(
-        check("thumbscrew-japingly"),
-        "\"thumbscrew-japingly\" should be an isogram."
-    )
+fn word_with_duplicated_character_in_mixed_case_lowercase_first() {
+    assert!(!check("alphAbet"));
+}
+
+#[test]
+#[ignore]
+fn hypothetical_isogrammic_word_with_hyphen() {
+    assert!(check("thumbscrew-japingly"));
+}
+
+#[test]
+#[ignore]
+fn hypothetical_word_with_duplicated_character_following_hyphen() {
+    assert!(!check("thumbscrew-jappingly"));
 }
 
 #[test]
 #[ignore]
 fn isogram_with_duplicated_hyphen() {
-    assert!(
-        check("six-year-old"),
-        "\"six-year-old\" should be an isogram."
-    )
+    assert!(check("six-year-old"));
 }
 
 #[test]
 #[ignore]
 fn made_up_name_that_is_an_isogram() {
-    assert!(
-        check("Emily Jung Schwartzkopf"),
-        "\"Emily Jung Schwartzkopf\" should be an isogram."
-    )
+    assert!(check("Emily Jung Schwartzkopf"));
 }
 
 #[test]
 #[ignore]
 fn duplicated_character_in_the_middle() {
-    assert!(
-        !check("accentor"),
-        "\"accentor\" has more than one \'c\', therefore it is no isogram."
-    )
+    assert!(!check("accentor"));
+}
+
+#[test]
+#[ignore]
+fn same_first_and_last_characters() {
+    assert!(!check("angola"));
+}
+
+#[test]
+#[ignore]
+fn word_with_duplicated_character_and_with_two_hyphens() {
+    assert!(!check("up-to-date"));
 }
