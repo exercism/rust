9534dc53a0ec3a2066b30599fc2f465994fa9f1e
Remo Senekowitsch <remo@buenzli.dev>
2024-08-12 17:43:26 +0200

    armstrong-numbers: sync (#1959)
    This also adds a new custom tera filter to format number literals in a
    more human-readable way (by inserting `_` every third digit).
    
    Most of the additional tests don't seem to add any particular value
    except for the last one, which is [under consideration for
    upstreaming](https://forum.exercism.org/t/armstrong-numbers-test-case-against-overflow/12457).
    
    [no important files changed]

--- rust-tooling/generate/src/custom_filters.rs
+++ rust-tooling/generate/src/custom_filters.rs
@@ -4,7 +4,11 @@ use tera::{Result, Value};
 
 type Filter = fn(&Value, &HashMap<String, Value>) -> Result<Value>;
 
-pub static CUSTOM_FILTERS: &[(&str, Filter)] = &[("to_hex", to_hex), ("snake_case", snake_case)];
+pub static CUSTOM_FILTERS: &[(&str, Filter)] = &[
+    ("to_hex", to_hex),
+    ("snake_case", snake_case),
+    ("fmt_num", fmt_num),
+];
 
 pub fn to_hex(value: &Value, _args: &HashMap<String, Value>) -> Result<Value> {
     let Some(value) = value.as_u64() else {
@@ -27,4 +31,26 @@ pub fn snake_case(value: &Value, _args: &HashMap<String, Value>) -> Result<Value
         // slug is the same dependency tera uses for its builtin 'slugify'
         slug::slugify(value).replace('-', "_"),
     ))
+}
+
+pub fn fmt_num(value: &Value, _args: &HashMap<String, Value>) -> Result<Value> {
+    let Some(value) = value.as_number() else {
+        return Err(tera::Error::call_filter(
+            "fmt_num filter expects a number",
+            "serde_json::value::Value::as_number",
+        ));
+    };
+    let mut num: Vec<_> = value.to_string().into();
+    num.reverse();
+
+    let mut pretty_digits = num
+        .chunks(3)
+        .flat_map(|digits| digits.iter().copied().chain([b'_']))
+        .collect::<Vec<_>>();
+    if pretty_digits.last() == Some(&b'_') {
+        pretty_digits.pop();
+    }
+    pretty_digits.reverse();
+    let pretty_num = String::from_utf8(pretty_digits).unwrap_or_default();
+    Ok(Value::String(pretty_num))
 }
