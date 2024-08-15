a8125d7de62fe15a0116799751b09fe4e47f9e9e
Remo Senekowitsch <remo@buenzli.dev>
2024-08-15 18:46:50 +0200

    dominoes: sync (#1974)
    [no important files changed]
    
    part of https://github.com/exercism/rust/issues/1824

--- exercises/practice/dominoes/tests/dominoes.rs
+++ exercises/practice/dominoes/tests/dominoes.rs
@@ -1,166 +1,89 @@
-use crate::CheckResult::*;
-
-type Domino = (u8, u8);
-
-#[derive(Debug)]
-enum CheckResult {
-    GotInvalid, // chain returned None
-    Correct,
-    ChainingFailure(Vec<Domino>), // failure to match the dots at the right side of one domino with
-    // the one on the left side of the next
-    LengthMismatch(Vec<Domino>),
-    DominoMismatch(Vec<Domino>), // different dominoes are used in input and output
-}
-
-fn normalize(d: Domino) -> Domino {
-    match d {
-        (m, n) if m > n => (n, m),
-        (m, n) => (m, n),
-    }
-}
-
-fn check(input: &[Domino]) -> CheckResult {
-    let output = match dominoes::chain(input) {
-        None => return GotInvalid,
-        Some(o) => o,
-    };
-    if input.len() != output.len() {
-        return LengthMismatch(output);
-    } else if input.is_empty() {
-        // and thus output.is_empty()
-        return Correct;
-    }
-
-    let mut output_sorted = output
-        .iter()
-        .map(|&d| normalize(d))
-        .collect::<Vec<Domino>>();
-    output_sorted.sort_unstable();
-    let mut input_sorted = input.iter().map(|&d| normalize(d)).collect::<Vec<Domino>>();
-    input_sorted.sort_unstable();
-    if input_sorted != output_sorted {
-        return DominoMismatch(output);
-    }
-
-    // both input and output have at least 1 element
-    // This essentially puts the first element after the last one, thereby making it
-    // easy to check whether the domino chains "wraps around".
-    let mut fail = false;
-    {
-        let mut n = output[0].1;
-        let iter = output.iter().skip(1).chain(output.iter().take(1));
-        for &(first, second) in iter {
-            if n != first {
-                fail = true;
-                break;
-            }
-            n = second
-        }
-    }
-    if fail {
-        ChainingFailure(output)
-    } else {
-        Correct
-    }
-}
-
-fn assert_correct(input: &[Domino]) {
-    match check(input) {
-        Correct => (),
-        GotInvalid => panic!("Unexpectedly got invalid on input {input:?}"),
-        ChainingFailure(output) => {
-            panic!("Chaining failure for input {input:?}, output {output:?}")
-        }
-        LengthMismatch(output) => {
-            panic!("Length mismatch for input {input:?}, output {output:?}")
-        }
-        DominoMismatch(output) => {
-            panic!("Domino mismatch for input {input:?}, output {output:?}")
-        }
-    }
-}
-
 #[test]
 fn empty_input_empty_output() {
     let input = &[];
-    assert_eq!(dominoes::chain(input), Some(vec![]));
+    let output = dominoes::chain(input);
+    assert!(output.is_some());
+    assert_correct(input, output.unwrap());
 }
 
 #[test]
 #[ignore]
 fn singleton_input_singleton_output() {
     let input = &[(1, 1)];
-    assert_correct(input);
+    let output = dominoes::chain(input);
+    assert!(output.is_some());
+    assert_correct(input, output.unwrap());
 }
 
 #[test]
 #[ignore]
-fn singleton_that_cant_be_chained() {
+fn singleton_that_can_t_be_chained() {
     let input = &[(1, 2)];
-    assert_eq!(dominoes::chain(input), None);
+    assert!(dominoes::chain(input).is_none());
 }
 
 #[test]
 #[ignore]
-fn no_repeat_numbers() {
+fn three_elements() {
     let input = &[(1, 2), (3, 1), (2, 3)];
-    assert_correct(input);
+    let output = dominoes::chain(input);
+    assert!(output.is_some());
+    assert_correct(input, output.unwrap());
 }
 
 #[test]
 #[ignore]
 fn can_reverse_dominoes() {
     let input = &[(1, 2), (1, 3), (2, 3)];
-    assert_correct(input);
+    let output = dominoes::chain(input);
+    assert!(output.is_some());
+    assert_correct(input, output.unwrap());
 }
 
 #[test]
 #[ignore]
-fn no_chains() {
+fn can_t_be_chained() {
     let input = &[(1, 2), (4, 1), (2, 3)];
-    assert_eq!(dominoes::chain(input), None);
+    assert!(dominoes::chain(input).is_none());
 }
 
 #[test]
 #[ignore]
 fn disconnected_simple() {
     let input = &[(1, 1), (2, 2)];
-    assert_eq!(dominoes::chain(input), None);
+    assert!(dominoes::chain(input).is_none());
 }
 
 #[test]
 #[ignore]
 fn disconnected_double_loop() {
     let input = &[(1, 2), (2, 1), (3, 4), (4, 3)];
-    assert_eq!(dominoes::chain(input), None);
+    assert!(dominoes::chain(input).is_none());
 }
 
 #[test]
 #[ignore]
 fn disconnected_single_isolated() {
     let input = &[(1, 2), (2, 3), (3, 1), (4, 4)];
-    assert_eq!(dominoes::chain(input), None);
+    assert!(dominoes::chain(input).is_none());
 }
 
 #[test]
 #[ignore]
 fn need_backtrack() {
     let input = &[(1, 2), (2, 3), (3, 1), (2, 4), (2, 4)];
-    assert_correct(input);
+    let output = dominoes::chain(input);
+    assert!(output.is_some());
+    assert_correct(input, output.unwrap());
 }
 
 #[test]
 #[ignore]
 fn separate_loops() {
     let input = &[(1, 2), (2, 3), (3, 1), (1, 1), (2, 2), (3, 3)];
-    assert_correct(input);
-}
-
-#[test]
-#[ignore]
-fn pop_same_value_first() {
-    let input = &[(2, 3), (3, 1), (1, 1), (2, 2), (3, 3), (2, 1)];
-    assert_correct(input);
+    let output = dominoes::chain(input);
+    assert!(output.is_some());
+    assert_correct(input, output.unwrap());
 }
 
 #[test]
@@ -177,5 +100,56 @@ fn nine_elements() {
         (3, 4),
         (5, 6),
     ];
-    assert_correct(input);
+    let output = dominoes::chain(input);
+    assert!(output.is_some());
+    assert_correct(input, output.unwrap());
+}
+
+#[test]
+#[ignore]
+fn separate_three_domino_loops() {
+    let input = &[(1, 2), (2, 3), (3, 1), (4, 5), (5, 6), (6, 4)];
+    assert!(dominoes::chain(input).is_none());
+}
+type Domino = (u8, u8);
+
+fn assert_correct(input: &[Domino], output: Vec<Domino>) {
+    if input.len() != output.len() {
+        panic!("Length mismatch for input {input:?}, output {output:?}");
+    } else if input.is_empty() {
+        // and thus output.is_empty()
+        return;
+    }
+
+    let mut output_sorted = output
+        .iter()
+        .map(|&d| normalize(d))
+        .collect::<Vec<Domino>>();
+    output_sorted.sort_unstable();
+    let mut input_sorted = input.iter().map(|&d| normalize(d)).collect::<Vec<Domino>>();
+    input_sorted.sort_unstable();
+    if input_sorted != output_sorted {
+        panic!("Domino mismatch for input {input:?}, output {output:?}");
+    }
+
+    // both input and output have at least 1 element
+    // This essentially puts the first element after the last one, thereby making it
+    // easy to check whether the domino chains "wraps around".
+    {
+        let mut n = output[0].1;
+        let iter = output.iter().skip(1).chain(output.iter().take(1));
+        for &(first, second) in iter {
+            if n != first {
+                panic!("Chaining failure for input {input:?}, output {output:?}")
+            }
+            n = second
+        }
+    }
+}
+
+fn normalize(d: Domino) -> Domino {
+    match d {
+        (m, n) if m > n => (n, m),
+        (m, n) => (m, n),
+    }
 }
