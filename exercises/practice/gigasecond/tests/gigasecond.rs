e5968ccc4917cb2eeac0cd86f561286344b0525b
Remo Senekowitsch <remo@buenzli.dev>
2024-08-16 06:54:40 +0200

    gigasecond: sync (#1975)
    [no important files changed]
    
    part of https://github.com/exercism/rust/issues/1824
    
    The small util function for constructing a `DateTime` value was left as
    is because it's only noise and not necessary to understand what's going
    on in the test case.

--- exercises/practice/gigasecond/tests/gigasecond.rs
+++ exercises/practice/gigasecond/tests/gigasecond.rs
@@ -1,52 +1,59 @@
-use time::PrimitiveDateTime as DateTime;
+#[test]
+fn date_only_specification_of_time() {
+    let start = datetime(2011, 4, 25, 0, 0, 0);
+    let actual = gigasecond::after(start);
+    let expected = datetime(2043, 1, 1, 1, 46, 40);
+    assert_eq!(actual, expected);
+}
 
-/// Create a datetime from the given numeric point in time.
-///
-/// Panics if any field is invalid.
-fn dt(year: i32, month: u8, day: u8, hour: u8, minute: u8, second: u8) -> DateTime {
-    use time::{Date, Time};
+#[test]
+#[ignore]
+fn second_test_for_date_only_specification_of_time() {
+    let start = datetime(1977, 6, 13, 0, 0, 0);
+    let actual = gigasecond::after(start);
+    let expected = datetime(2009, 2, 19, 1, 46, 40);
+    assert_eq!(actual, expected);
+}
 
-    DateTime::new(
+#[test]
+#[ignore]
+fn third_test_for_date_only_specification_of_time() {
+    let start = datetime(1959, 7, 19, 0, 0, 0);
+    let actual = gigasecond::after(start);
+    let expected = datetime(1991, 3, 27, 1, 46, 40);
+    assert_eq!(actual, expected);
+}
+
+#[test]
+#[ignore]
+fn full_time_specified() {
+    let start = datetime(2015, 1, 24, 22, 0, 0);
+    let actual = gigasecond::after(start);
+    let expected = datetime(2046, 10, 2, 23, 46, 40);
+    assert_eq!(actual, expected);
+}
+
+#[test]
+#[ignore]
+fn full_time_with_day_roll_over() {
+    let start = datetime(2015, 1, 24, 23, 59, 59);
+    let actual = gigasecond::after(start);
+    let expected = datetime(2046, 10, 3, 1, 46, 39);
+    assert_eq!(actual, expected);
+}
+
+fn datetime(
+    year: i32,
+    month: u8,
+    day: u8,
+    hour: u8,
+    minute: u8,
+    second: u8,
+) -> time::PrimitiveDateTime {
+    use time::{Date, PrimitiveDateTime, Time};
+
+    PrimitiveDateTime::new(
         Date::from_calendar_date(year, month.try_into().unwrap(), day).unwrap(),
         Time::from_hms(hour, minute, second).unwrap(),
     )
-}
-
-#[test]
-fn date() {
-    let start_date = dt(2011, 4, 25, 0, 0, 0);
-
-    assert_eq!(gigasecond::after(start_date), dt(2043, 1, 1, 1, 46, 40));
-}
-
-#[test]
-#[ignore]
-fn another_date() {
-    let start_date = dt(1977, 6, 13, 0, 0, 0);
-
-    assert_eq!(gigasecond::after(start_date), dt(2009, 2, 19, 1, 46, 40));
-}
-
-#[test]
-#[ignore]
-fn third_date() {
-    let start_date = dt(1959, 7, 19, 0, 0, 0);
-
-    assert_eq!(gigasecond::after(start_date), dt(1991, 3, 27, 1, 46, 40));
-}
-
-#[test]
-#[ignore]
-fn datetime() {
-    let start_date = dt(2015, 1, 24, 22, 0, 0);
-
-    assert_eq!(gigasecond::after(start_date), dt(2046, 10, 2, 23, 46, 40));
-}
-
-#[test]
-#[ignore]
-fn another_datetime() {
-    let start_date = dt(2015, 1, 24, 23, 59, 59);
-
-    assert_eq!(gigasecond::after(start_date), dt(2046, 10, 3, 1, 46, 39));
 }
