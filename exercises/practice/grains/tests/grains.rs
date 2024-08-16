6ea361030c64c0c5f587cc01368060380622c574
Remo Senekowitsch <remo@buenzli.dev>
2024-08-16 07:34:09 +0200

    grains: sync
    part of https://github.com/exercism/rust/issues/1824

--- exercises/practice/grains/tests/grains.rs
+++ exercises/practice/grains/tests/grains.rs
@@ -1,62 +1,59 @@
-fn process_square_case(input: u32, expected: u64) {
-    assert_eq!(grains::square(input), expected);
-}
+use grains::*;
 
 #[test]
-fn one() {
-    process_square_case(1, 1);
+fn grains_on_square_1() {
+    assert_eq!(square(1), 1);
 }
 
 #[test]
 #[ignore]
-fn two() {
-    process_square_case(2, 2);
+fn grains_on_square_2() {
+    assert_eq!(square(2), 2);
 }
 
 #[test]
 #[ignore]
-fn three() {
-    process_square_case(3, 4);
+fn grains_on_square_3() {
+    assert_eq!(square(3), 4);
 }
 
 #[test]
 #[ignore]
-fn four() {
-    process_square_case(4, 8);
+fn grains_on_square_4() {
+    assert_eq!(square(4), 8);
 }
 
 #[test]
 #[ignore]
-fn sixteen() {
-    process_square_case(16, 32_768);
+fn grains_on_square_16() {
+    assert_eq!(square(16), 32_768);
 }
 
 #[test]
 #[ignore]
-fn thirty_two() {
-    process_square_case(32, 2_147_483_648);
+fn grains_on_square_32() {
+    assert_eq!(square(32), 2_147_483_648);
 }
 
 #[test]
 #[ignore]
-fn sixty_four() {
-    process_square_case(64, 9_223_372_036_854_775_808);
+fn grains_on_square_64() {
+    assert_eq!(square(64), 9_223_372_036_854_775_808);
 }
 
 #[test]
 #[ignore]
-#[should_panic(expected = "Square must be between 1 and 64")]
-fn square_0_raises_an_exception() {
-    grains::square(0);
+#[should_panic]
+fn square_0_is_invalid() {
+    square(0);
 }
 
 #[test]
 #[ignore]
-#[should_panic(expected = "Square must be between 1 and 64")]
-fn square_greater_than_64_raises_an_exception() {
-    grains::square(65);
+#[should_panic]
+fn square_greater_than_64_is_invalid() {
+    square(65);
 }
-
 #[test]
 #[ignore]
 fn returns_the_total_number_of_grains_on_the_board() {
