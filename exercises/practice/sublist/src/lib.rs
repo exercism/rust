0f32defef7b6b3ca415f796ef015337dbe2a5394
ellnix <ellnix@disroot.org>
2025-01-29 15:16:50 +0100

    Sublist: use args in todo! instead of naming with _ (#2004)
    This necessitated adding `std::fmt::Debug` as a constraint to `T`. All
    of the types in the tests are `i32` so this does not cause the tests to
    break.
    
    Corresponding forum post:
    https://forum.exercism.org/t/warn-explain-or-remove-variables/15285

--- exercises/practice/sublist/src/lib.rs
+++ exercises/practice/sublist/src/lib.rs
@@ -6,6 +6,6 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    todo!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+pub fn sublist(first_list: &[i32], second_list: &[i32]) -> Comparison {
+    todo!("Determine if the {first_list:?} is equal to, sublist of, superlist of or unequal to {second_list:?}.");
 }
