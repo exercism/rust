85b0464b4b275d4399c0f764e5f7f48fd6a93658
ellnix <ellnix@disroot.org>
2025-02-08 22:37:33 +0100

    atbash-cipher: Use Tera template for tests
    This should stop configlet's warnings about missing tests.
    
    [no important files changed]

--- exercises/practice/atbash-cipher/tests/atbash-cipher.rs
+++ exercises/practice/atbash-cipher/tests/atbash-cipher.rs
@@ -1,99 +1,126 @@
-use atbash_cipher as cipher;
+use atbash_cipher::*;
 
 #[test]
 fn encode_yes() {
-    assert_eq!(cipher::encode("yes"), "bvh");
+    let output = encode("yes");
+    let expected = "bvh";
+
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn encode_no() {
-    assert_eq!(cipher::encode("no"), "ml");
+    let output = encode("no");
+    let expected = "ml";
+
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn encode_omg() {
-    assert_eq!(cipher::encode("OMG"), "lnt");
+    let output = encode("OMG");
+    let expected = "lnt";
+
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn encode_spaces() {
-    assert_eq!(cipher::encode("O M G"), "lnt");
+    let output = encode("O M G");
+    let expected = "lnt";
+
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn encode_mindblowingly() {
-    assert_eq!(cipher::encode("mindblowingly"), "nrmwy oldrm tob");
+    let output = encode("mindblowingly");
+    let expected = "nrmwy oldrm tob";
+
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn encode_numbers() {
-    assert_eq!(
-        cipher::encode("Testing,1 2 3, testing."),
-        "gvhgr mt123 gvhgr mt"
-    );
+    let output = encode("Testing,1 2 3, testing.");
+    let expected = "gvhgr mt123 gvhgr mt";
+
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn encode_deep_thought() {
-    assert_eq!(cipher::encode("Truth is fiction."), "gifgs rhurx grlm");
+    let output = encode("Truth is fiction.");
+    let expected = "gifgs rhurx grlm";
+
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn encode_all_the_letters() {
-    assert_eq!(
-        cipher::encode("The quick brown fox jumps over the lazy dog."),
-        "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"
-    );
+    let output = encode("The quick brown fox jumps over the lazy dog.");
+    let expected = "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt";
+
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn decode_exercism() {
-    assert_eq!(cipher::decode("vcvix rhn"), "exercism");
+    let output = decode("vcvix rhn");
+    let expected = "exercism";
+
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn decode_a_sentence() {
-    assert_eq!(
-        cipher::decode("zmlyh gzxov rhlug vmzhg vkkrm thglm v"),
-        "anobstacleisoftenasteppingstone"
-    );
+    let output = decode("zmlyh gzxov rhlug vmzhg vkkrm thglm v");
+    let expected = "anobstacleisoftenasteppingstone";
+
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn decode_numbers() {
-    assert_eq!(cipher::decode("gvhgr mt123 gvhgr mt"), "testing123testing");
+    let output = decode("gvhgr mt123 gvhgr mt");
+    let expected = "testing123testing";
+
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn decode_all_the_letters() {
-    assert_eq!(
-        cipher::decode("gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"),
-        "thequickbrownfoxjumpsoverthelazydog"
-    );
+    let output = decode("gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt");
+    let expected = "thequickbrownfoxjumpsoverthelazydog";
+
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn decode_with_too_many_spaces() {
-    assert_eq!(cipher::decode("vc vix    r hn"), "exercism");
+    let output = decode("vc vix    r hn");
+    let expected = "exercism";
+
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
 fn decode_with_no_spaces() {
-    assert_eq!(
-        cipher::decode("zmlyhgzxovrhlugvmzhgvkkrmthglmv"),
-        "anobstacleisoftenasteppingstone",
-    );
+    let output = decode("zmlyhgzxovrhlugvmzhgvkkrmthglmv");
+    let expected = "anobstacleisoftenasteppingstone";
+
+    assert_eq!(output, expected);
 }
