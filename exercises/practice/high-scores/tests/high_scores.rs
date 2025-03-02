74ce2e36987917959ef10194dfc20ff595262967
ellnix <ellnix@disroot.org>
2025-03-02 23:43:10 +0100

    high-scores: Sync tests & generate with template (#2048)

--- exercises/practice/high-scores/tests/high_scores.rs
+++ exercises/practice/high-scores/tests/high_scores.rs
@@ -4,6 +4,7 @@ use high_scores::HighScores;
 fn list_of_scores() {
     let expected = [30, 50, 20, 70];
     let high_scores = HighScores::new(&expected);
+
     assert_eq!(high_scores.scores(), &expected);
 }
 
@@ -11,7 +12,98 @@ fn list_of_scores() {
 #[ignore]
 fn latest_score() {
     let high_scores = HighScores::new(&[100, 0, 90, 30]);
+
     assert_eq!(high_scores.latest(), Some(30));
+}
+
+#[test]
+#[ignore]
+fn personal_best() {
+    let high_scores = HighScores::new(&[40, 100, 70]);
+
+    assert_eq!(high_scores.personal_best(), Some(100));
+}
+
+#[test]
+#[ignore]
+fn personal_top_three_from_a_list_of_scores() {
+    let high_scores = HighScores::new(&[10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70]);
+
+    assert_eq!(high_scores.personal_top_three(), vec![100, 90, 70]);
+}
+
+#[test]
+#[ignore]
+fn personal_top_highest_to_lowest() {
+    let high_scores = HighScores::new(&[20, 10, 30]);
+
+    assert_eq!(high_scores.personal_top_three(), vec![30, 20, 10]);
+}
+
+#[test]
+#[ignore]
+fn personal_top_when_there_is_a_tie() {
+    let high_scores = HighScores::new(&[40, 20, 40, 30]);
+
+    assert_eq!(high_scores.personal_top_three(), vec![40, 40, 30]);
+}
+
+#[test]
+#[ignore]
+fn personal_top_when_there_are_less_than_3() {
+    let high_scores = HighScores::new(&[30, 70]);
+
+    assert_eq!(high_scores.personal_top_three(), vec![70, 30]);
+}
+
+#[test]
+#[ignore]
+fn personal_top_when_there_is_only_one() {
+    let high_scores = HighScores::new(&[40]);
+
+    assert_eq!(high_scores.personal_top_three(), vec![40]);
+}
+
+#[test]
+#[ignore]
+fn latest_score_unchanged_after_personal_top_scores() {
+    let high_scores = HighScores::new(&[70, 50, 20, 30]);
+
+    let _ = high_scores.personal_top_three();
+
+    assert_eq!(high_scores.latest(), Some(30));
+}
+
+#[test]
+#[ignore]
+fn scores_unchanged_after_personal_top_scores() {
+    let expected = [30, 50, 20, 70];
+    let high_scores = HighScores::new(&expected);
+
+    let _ = high_scores.personal_top_three();
+
+    assert_eq!(high_scores.scores(), &expected);
+}
+
+#[test]
+#[ignore]
+fn latest_score_unchanged_after_personal_best() {
+    let high_scores = HighScores::new(&[20, 70, 15, 25, 30]);
+
+    let _ = high_scores.personal_best();
+
+    assert_eq!(high_scores.latest(), Some(30));
+}
+
+#[test]
+#[ignore]
+fn scores_unchanged_after_personal_best() {
+    let expected = [20, 70, 15, 25, 30];
+    let high_scores = HighScores::new(&expected);
+
+    let _ = high_scores.personal_best();
+
+    assert_eq!(high_scores.scores(), &expected);
 }
 
 #[test]
@@ -23,51 +115,9 @@ fn latest_score_empty() {
 
 #[test]
 #[ignore]
-fn personal_best() {
-    let high_scores = HighScores::new(&[40, 100, 70]);
-    assert_eq!(high_scores.personal_best(), Some(100));
-}
-
-#[test]
-#[ignore]
 fn personal_best_empty() {
     let high_scores = HighScores::new(&[]);
     assert_eq!(high_scores.personal_best(), None);
-}
-
-#[test]
-#[ignore]
-fn personal_top_three() {
-    let high_scores = HighScores::new(&[10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70]);
-    assert_eq!(high_scores.personal_top_three(), vec![100, 90, 70]);
-}
-
-#[test]
-#[ignore]
-fn personal_top_three_highest_to_lowest() {
-    let high_scores = HighScores::new(&[20, 10, 30]);
-    assert_eq!(high_scores.personal_top_three(), vec![30, 20, 10]);
-}
-
-#[test]
-#[ignore]
-fn personal_top_three_with_tie() {
-    let high_scores = HighScores::new(&[40, 20, 40, 30]);
-    assert_eq!(high_scores.personal_top_three(), vec![40, 40, 30]);
-}
-
-#[test]
-#[ignore]
-fn personal_top_three_with_less_than_three_scores() {
-    let high_scores = HighScores::new(&[30, 70]);
-    assert_eq!(high_scores.personal_top_three(), vec![70, 30]);
-}
-
-#[test]
-#[ignore]
-fn personal_top_three_only_one_score() {
-    let high_scores = HighScores::new(&[40]);
-    assert_eq!(high_scores.personal_top_three(), vec![40]);
 }
 
 #[test]
