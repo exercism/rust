517c0e704ef3383146a2d36aaa580d43d65a8b60
Olivier Boulant <olivier.boulant@proton.me>
2025-05-12 21:06:23 +0200

    sieve

--- exercises/practice/sieve/src/lib.rs
+++ exercises/practice/sieve/src/lib.rs
@@ -1,3 +1,22 @@
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    todo!("Construct a vector of all primes up to {upper_bound}");
+    let candidates: Vec<u64> = (2..=upper_bound).collect();
+    let nb_el: usize = (upper_bound - 1) as usize;
+    let mut mask: Vec<bool> = vec![true; nb_el ]; // Same size as `candidates`. 1:1 matching. 
+
+    // Iterate over `candidates`
+    for (i, &val) in candidates.iter().enumerate() {
+        if mask[i] {
+            // If prime, mark as not prime all multiples of `val`
+            for j in ((i+val as usize)..nb_el).step_by(val as usize) {
+                mask[j] = false;
+            }
+        }
+    }
+
+    // Filter `candidates` based on `mask` 
+    candidates
+    .into_iter()
+    .zip(mask)
+    .filter_map(|(val, is_prime)| if is_prime { Some(val) } else { None })
+    .collect()    
 }
