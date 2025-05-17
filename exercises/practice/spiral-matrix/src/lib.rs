d89b0950ce024abd766eb5d4ea309b0d59feb383
Olivier Boulant <olivier.boulant@proton.me>
2025-05-17 17:48:55 +0200

    spiral_matrix

--- exercises/practice/spiral-matrix/src/lib.rs
+++ exercises/practice/spiral-matrix/src/lib.rs
@@ -1,3 +1,54 @@
 pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
-    todo!("Function that returns the spiral matrix of square size {size}");
+    let mut size: usize = size as usize;
+    let mut start_count = 1;
+    let mut start_pos = (0, 0);
+
+    let mut res: Vec<Vec<u32>> = vec![vec![0; size]; size];
+
+    loop {
+
+        // Empty square, return
+        if size == 0 {
+            return res;
+        }
+
+        // Upper segment
+        for k in 0..size {
+            res[start_pos.0][start_pos.1 + k] = start_count;
+            start_count+=1;
+        }
+
+        // Single cell square. 
+        if size == 1 {
+            break;
+        }
+
+        // Right segment
+        for k in 1..size {
+            res[start_pos.0 + k][start_pos.1 + size-1] = start_count;
+            start_count+=1;
+        }
+
+        // Lower segment
+        for k in (0..=(size-2)).rev() {
+            res[start_pos.0 + size-1][start_pos.1 + k] = start_count;
+            start_count+=1;
+        }
+
+        // Left segment
+        for k in (1..=(size-2)).rev() {
+            res[start_pos.0 + k][start_pos.1] = start_count;
+            start_count+=1;
+        }
+
+        if size <= 2 {
+            break;
+        }
+        size -= 2;
+        start_pos = (start_pos.0+1, start_pos.1 + 1);
+    }
+
+
+    res
+
 }
