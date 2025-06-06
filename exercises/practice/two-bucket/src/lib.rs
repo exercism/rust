c1621d825410c9f1e66ec9e7b46e7fe0d3ec9205
Olivier Boulant <olivier.boulant@proton.me>
2025-06-06 13:43:39 +0200

    two-bucket

--- exercises/practice/two-bucket/src/lib.rs
+++ exercises/practice/two-bucket/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp::max;
+
 #[derive(PartialEq, Eq, Debug)]
 pub enum Bucket {
     One,
@@ -16,6 +18,17 @@ pub struct BucketStats {
     pub other_bucket: u8,
 }
 
+fn get_gcd(a: &u8, b: &u8) -> u8 {
+    let mut a = *a;
+    let mut b = *b;
+    while b != 0 {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    a
+}
+
 /// Solve the bucket problem
 pub fn solve(
     capacity_1: u8,
@@ -23,7 +36,106 @@ pub fn solve(
     goal: u8,
     start_bucket: &Bucket,
 ) -> Option<BucketStats> {
-    todo!(
-        "Given one bucket of capacity {capacity_1}, another of capacity {capacity_2}, starting with {start_bucket:?}, find pours to reach {goal}, or None if impossible"
-    );
+
+    let mut res = BucketStats{
+        moves:0,
+        goal_bucket: Bucket::One,
+        other_bucket: 2
+    };
+
+    // Goal larger than both buckets: not possible
+    if goal > max(capacity_1, capacity_2) {
+        return None;
+    }
+
+    // It is only possible to get multiples of the greatest common factor of both numbers.
+    let gcd = get_gcd(&capacity_1, &capacity_2); 
+    if goal % gcd != 0 {
+        return None;
+    }
+
+    // To be agnostic of bucket one or two.
+    let (size_b1, size_b2) = match *start_bucket {
+        Bucket::One => (capacity_1, capacity_2),
+        Bucket::Two => (capacity_2, capacity_1)
+    };
+
+    // Initial step, first half
+    let mut in_b1: u8 = match *start_bucket {
+        Bucket::One => capacity_1,
+        Bucket::Two => capacity_2
+    };
+    let mut in_b2: u8 = 0;
+    res.moves += 1;
+
+    // Complete second half of first step
+    if in_b1 != goal {
+        if size_b2 == goal {
+            in_b2 = goal;
+            res.moves += 1; 
+        }
+        else {
+            let to_xfer = in_b1.min(size_b2 - in_b2);
+            in_b2 += to_xfer;
+            in_b1 -= to_xfer;
+            res.moves += 1; 
+        }
+    }
+
+    // main loop
+    while in_b1 != goal && in_b2 != goal {
+
+        // first half of a step
+        if in_b2 == size_b2 {
+            in_b2 = 0;
+        }
+        else {
+            in_b1 = size_b1;
+        }
+        res.moves += 1;
+        if in_b1 == goal || in_b2 == goal {
+            break;
+        }
+        
+        // second half of a step
+        let to_xfer = in_b1.min(size_b2 - in_b2);
+        in_b2 += to_xfer;
+        in_b1 -= to_xfer;
+        res.moves += 1;
+        if in_b1 == goal || in_b2 == goal {
+            break;
+        }
+
+    }
+
+    // Format output
+    // Handle goal_bucket
+    match *start_bucket {
+        Bucket::One => {
+            if in_b1 == goal {
+                res.goal_bucket = Bucket::One;
+            }
+            else {
+                res.goal_bucket = Bucket::Two;
+            }
+        },
+        Bucket::Two => {
+            if in_b1 == goal {
+                res.goal_bucket = Bucket::Two;
+            }
+            else {
+                res.goal_bucket = Bucket::One;
+            }
+        }
+    }
+
+    // Handle other_bucket
+    if in_b1 == goal {
+        res.other_bucket = in_b2;
+    }
+    else {
+        res.other_bucket = in_b1;
+    }
+
+    Some(res)
 }
