5d7c10d5691fa7bd06af0dcab2ce76a59ac8c2f6
Olivier Boulant <olivier.boulant@proton.me>
2025-06-12 09:43:55 +0200

    robot name

--- exercises/practice/robot-name/src/lib.rs
+++ exercises/practice/robot-name/src/lib.rs
@@ -1,15 +1,60 @@
-pub struct Robot;
+use once_cell::sync::Lazy;
+use rand::{thread_rng, Rng};
+use std::collections::HashSet;
+use std::sync::Mutex;
+
+pub struct Robot {
+    name: String
+}
+
+static ROBOT_NAMES: Lazy<Mutex<HashSet<String>>> = Lazy::new(|| {
+    Mutex::new(HashSet::new())
+});
+
+fn generate_robot_name() -> String {
+    let mut rng = thread_rng();
+
+    let letters: String = (0..2)
+    .map(|_| rng.gen_range(b'A'..=b'Z') as char)
+    .collect();
+
+    let digits: String = (0..3)
+    .map(|_| rng.gen_range(0..=9).to_string())
+    .collect();
+
+    format!("{letters}{digits}")
+}
 
 impl Robot {
     pub fn new() -> Self {
-        todo!("Construct a new Robot struct.");
+        let mut names = ROBOT_NAMES.lock().unwrap();
+        let mut name = generate_robot_name();
+        
+        while names.contains(&name)  {
+            name = generate_robot_name();
+        }
+
+        names.insert(name.clone());
+
+        Self { 
+            name 
+        }
     }
 
     pub fn name(&self) -> &str {
-        todo!("Return the reference to the robot's name.");
+        &self.name
     }
 
     pub fn reset_name(&mut self) {
-        todo!("Assign a new unique name to the robot.");
+        let mut names = ROBOT_NAMES.lock().unwrap();
+        let mut name = generate_robot_name();
+
+        while names.contains(&name) {
+            name = generate_robot_name();
+        }
+
+        names.remove(&name);
+
+        self.name = name;
     }
 }
