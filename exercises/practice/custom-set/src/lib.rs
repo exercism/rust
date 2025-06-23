402f31277f1d7bcfbf8109deb0a66877ac52583e
Olivier Boulant <olivier.boulant@proton.me>
2025-06-23 09:38:15 +0200

    custom-set

--- exercises/practice/custom-set/src/lib.rs
+++ exercises/practice/custom-set/src/lib.rs
@@ -1,47 +1,56 @@
+use std::collections::HashSet;
+use std::hash::Hash;
+
 #[derive(Debug, PartialEq, Eq)]
-pub struct CustomSet<T> {
+pub struct CustomSet<T: Eq + Hash> {
     // We fake using T here, so the compiler does not complain that
     // "parameter `T` is never used". Delete when no longer needed.
-    phantom: std::marker::PhantomData<T>,
+    elements: HashSet<T>
 }
 
-impl<T> CustomSet<T> {
+impl<T: Eq + Hash + Clone> CustomSet<T> {
     pub fn new(_input: &[T]) -> Self {
-        todo!();
+        Self {
+            elements: _input.iter().cloned().collect()
+        }
     }
 
     pub fn contains(&self, _element: &T) -> bool {
-        todo!();
+        self.elements.contains(_element)
     }
 
     pub fn add(&mut self, _element: T) {
-        todo!();
+        self.elements.insert(_element);
     }
 
     pub fn is_subset(&self, _other: &Self) -> bool {
-        todo!();
+        self.elements.is_subset(&_other.elements)
     }
 
     pub fn is_empty(&self) -> bool {
-        todo!();
+        self.elements.is_empty()
     }
 
     pub fn is_disjoint(&self, _other: &Self) -> bool {
-        todo!();
+        self.elements.is_disjoint(&_other.elements)
     }
 
     #[must_use]
     pub fn intersection(&self, _other: &Self) -> Self {
-        todo!();
+        Self {
+            elements: self.elements.intersection(&_other.elements).cloned().collect()
+        }
     }
 
     #[must_use]
     pub fn difference(&self, _other: &Self) -> Self {
-        todo!();
+        Self {
+            elements: self.elements.difference(&_other.elements).cloned().collect()
+        }
     }
 
     #[must_use]
     pub fn union(&self, _other: &Self) -> Self {
-        todo!();
+        Self { elements: self.elements.union(&_other.elements).cloned().collect() }
     }
 }
