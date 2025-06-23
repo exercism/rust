7fc387a7fa3ca8b93266a064b50bebe9a80544f1
Olivier Boulant <olivier.boulant@proton.me>
2025-06-23 18:28:51 +0200

    atbash-cipher

--- exercises/practice/atbash-cipher/src/lib.rs
+++ exercises/practice/atbash-cipher/src/lib.rs
@@ -1,9 +1,44 @@
+use once_cell::sync::Lazy;
+
+static ALPHABET: Lazy<&str> = Lazy::new(|| {
+    "abcdefghijklmnopqrstuvwxyz"
+});
+
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    todo!("Encoding of {plain:?} in Atbash cipher.");
+    let nb_letters = ALPHABET.chars().count();
+
+    plain
+    .chars()
+    .filter(|c| {
+        c.is_alphanumeric()
+    })
+    .map(|c| {
+        c.to_ascii_lowercase()
+    })
+    .map(|c| {
+        if c.is_numeric() {
+            c
+        }
+        else {
+            ALPHABET.chars().nth(nb_letters - 1 - (c as u8 - b'a') as usize).unwrap()
+        }
+    })
+    .collect::<Vec<char>>()
+    .chunks(5)
+    .map(|chunk| {
+        chunk.iter().collect::<String>()
+    })
+    .collect::<Vec<String>>()
+    .join(" ")
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    todo!("Decoding of {cipher:?} in Atbash cipher.");
+    encode(cipher)
+    .chars()
+    .filter(|c| {
+        !c.is_whitespace()
+    })
+    .collect()
 }
