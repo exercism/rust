571b9e5a8fc8b24a021f32ac1f0901f6b71b4b9b
Olivier Boulant <olivier.boulant@proton.me>
2025-06-30 21:02:36 +0200

    luhn-from

--- exercises/practice/luhn-from/src/lib.rs
+++ exercises/practice/luhn-from/src/lib.rs
@@ -1,8 +1,35 @@
-pub struct Luhn;
+pub struct Luhn(String);
 
 impl Luhn {
     pub fn is_valid(&self) -> bool {
-        todo!("Determine if the current Luhn struct contains a valid credit card number.");
+
+        self.0
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .rev()
+        .try_fold((0, 0), |(i, checksum), c| {
+            c.to_digit(10) // This (combined with the try_fold) force a return to false if c is not a digit
+            .map(|d| {
+                if i % 2 == 1 {
+                    d << 1
+                }
+                else {
+                    d
+                }
+            })
+            .map(|d| {
+                if d > 9 {
+                    d - 9
+                }
+                else {
+                    d
+                }
+            })
+            .map(|d| {
+                (i+1, checksum+d)
+            })
+        })
+        .is_some_and(|(len, checksum)| len > 1 && checksum % 10 == 0)
     }
 }
 
@@ -11,8 +38,11 @@ impl Luhn {
 /// by hand for every other type presented in the test suite,
 /// but your solution will fail if a new type is presented.
 /// Perhaps there exists a better solution for this problem?
-impl From<&str> for Luhn {
-    fn from(input: &str) -> Self {
-        todo!("From the given input '{input}' create a new Luhn struct.");
+impl<T: ToString> From<T> for Luhn {
+    // works because String also have a `to_string()` method
+    fn from(input: T) -> Self {
+        Self(
+            input.to_string()
+        )
     }
 }
