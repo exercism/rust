520a8c5e2fe779cc716de7f01b9242fe916e7f02
Olivier Boulant <olivier.boulant@proton.me>
2025-06-30 21:02:08 +0200

    largest-series-product

--- exercises/practice/largest-series-product/src/lib.rs
+++ exercises/practice/largest-series-product/src/lib.rs
@@ -5,5 +5,37 @@ pub enum Error {
 }
 
 pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
-    todo!("largest series product of a span of {span} digits in {string_digits}");
+    
+    if span > string_digits.len() {
+        return Err(Error::SpanTooLong);
+    }
+
+    // `windows(0) not allowed`, so handling it here
+    if span == 0 {
+        return Ok(1);
+    }
+
+    let mut max_product: u64 = 0;
+
+    for window in string_digits
+    .chars()
+    .collect::<Vec<char>>()
+    .windows(span) {
+        let mut product: u64 = 1;
+        
+        for c in window {
+            if let Some(d) = c.to_digit(10) {
+                product *= d as u64;
+            }
+            else {
+                return Err(Error::InvalidDigit(*c));
+            }
+        }
+
+        if product > max_product {
+            max_product = product;
+        }
+    }
+
+    Ok(max_product)   
 }
