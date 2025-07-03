9d845b581b716c1e805301c26691fb7779cde34e
Olivier Boulant <olivier.boulant@proton.me>
2025-07-03 21:13:53 +0200

    roman-numerals

--- exercises/practice/roman-numerals/src/lib.rs
+++ exercises/practice/roman-numerals/src/lib.rs
@@ -1,15 +1,40 @@
-use std::fmt::{Display, Formatter, Result};
+use std::{fmt::{Display, Formatter, Result}};
 
-pub struct Roman;
+pub struct Roman(u32);
 
 impl Display for Roman {
     fn fmt(&self, _f: &mut Formatter<'_>) -> Result {
-        todo!("Return a roman-numeral string representation of the Roman object");
+        let roman_map = [(1000, "M"),
+        (900, "CM"),
+        (500, "D"),
+        (400, "CD"),
+        (100, "C"),
+        (90, "XC"),
+        (50, "L"),
+        (40, "XL"),
+        (10, "X"),
+        (9, "IX"),
+        (5, "V"),
+        (4, "IV"),
+        (1, "I")];
+
+        let mut res = String::new();
+        let mut num = self.0;
+
+        for &(divisor, roman) in roman_map.iter() {
+            while num >= divisor {
+                res.push_str(roman);
+                num -= divisor;
+            }
+        }
+
+        write!(_f, "{}", res)
+
     }
 }
 
 impl From<u32> for Roman {
     fn from(num: u32) -> Self {
-        todo!("Construct a Roman object from the '{num}' number");
+        Self(num)
     }
 }
