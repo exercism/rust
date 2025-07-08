42bd28cd9570042c8bb2dead174f50d5ae2c302b
Olivier Boulant <olivier.boulant@proton.me>
2025-07-08 09:54:41 +0200

    simple-cipher

--- exercises/practice/simple-cipher/src/lib.rs
+++ exercises/practice/simple-cipher/src/lib.rs
@@ -1,11 +1,62 @@
+use rand::{rng, Rng};
+
+
+fn is_key_invalid(key: &str) -> bool {
+    
+    if key.len() == 0 {
+        return true;
+    } 
+    
+    key
+    .chars()
+    .any(|c| !c.is_alphabetic() || !c.is_ascii_lowercase() || !c.is_lowercase())
+}
+
 pub fn encode(key: &str, s: &str) -> Option<String> {
-    todo!("Use {key} to encode {s} using shift cipher")
+    if is_key_invalid(key) {
+        return None;
+    }
+
+    let mut res = String::new();
+    for (c, k) in s.chars().zip(key.chars().cycle()) {
+        println!("encode {c}, {k}");
+        println!("encode : {}, {}", c as u8, k as u8);
+        println!("{}", c as u8 + k as u8 - 'a' as u8);
+        // res.push(('a' as u8  + (26 + k as u8 - c as u8) % 26 ) as char);
+        // res.push((((k as u8 - 'a' as u8) + c as u8) % 26 ) as char);
+        res.push(('a' as u8 + (k as u8 - 'a' as u8 + c as u8 - 'a' as u8) % 26) as char);
+    }
+
+    println!("{}", res);
+
+    Some(res)
 }
 
 pub fn decode(key: &str, s: &str) -> Option<String> {
-    todo!("Use {key} to decode {s} using shift cipher")
+
+    if is_key_invalid(key) {
+        return None;
+    }
+
+    let mut res = String::new();
+    for (c, k) in s.chars().zip(key.chars().cycle()) {
+        println!("decode : {c}, {k}");
+        println!("decode : {}, {}", c as u8, k as u8);
+        res.push(('a' as u8 + (26 + c as u8 - k as u8) % 26 ) as char)
+    }
+    
+    println!("{}", res);
+
+    Some(res)
 }
 
 pub fn encode_random(s: &str) -> (String, String) {
-    todo!("Generate random key with only a-z chars and encode {s}. Return tuple (key, encoded s)")
+    let mut rng = rng();
+    let key: String = (0..100).map(|_| {
+        ('a' as u8 + rng.random_range(0..26)) as char
+    })
+    .collect();
+
+    let encoded = encode(&key, s);
+    (key, encoded.unwrap())
 }
