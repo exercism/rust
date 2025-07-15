ff5240d817aa86ea62afb66714253aec9d6d96ce
Remo Senekowitsch <remo@buenzli.dev>
2025-07-15 22:29:00 +0200

    Remove system for pinning problem-specifications
    We now have CI workflows that raise issues when configlet finds
    something that needs updating. This is in conflict with the pinned
    problem-specifications approach.

--- bin/symlink_problem_specifications.sh
+++ bin/symlink_problem_specifications.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+# This script creates a symlink to the problem-specifications cache used
+# by configlet. When working on an exercise, it is handy to have the
+# problem-specifions files in reach. Symlinking to configlet's cache ensures
+# you're always looking at the up-to-date files.
+
 cd "$(git rev-parse --show-toplevel)"
 
 [ -e "problem-specifications" ] || ln -s "$(./bin/get_problem_specifications_dir.sh)" "problem-specifications"
 
-./bin/checkout_pinned_problem_specifications_commit.sh
+# ensure populated cache
+[ -f ./bin/configlet ] || ./bin/fetch-configlet
+./bin/configlet info &> /dev/null
 
 for exercise in exercises/practice/*; do
     name="$(basename "$exercise")"
