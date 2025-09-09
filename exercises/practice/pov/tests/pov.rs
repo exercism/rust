6560f967c55ad39379197c27929e4b40462ce192
Remo Senekowitsch <remo@buenzli.dev>
2025-09-09 18:42:28 +0200

    make tests less verbose

--- exercises/practice/pov/tests/pov.rs
+++ exercises/practice/pov/tests/pov.rs
@@ -21,8 +21,7 @@ mod from_pov {
     #[ignore]
     fn results_in_the_same_tree_if_the_input_tree_is_a_singleton() {
         let mut tree = Tree::new("x");
-        let from = "x";
-        assert!(tree.pov_from(&from));
+        assert!(tree.pov_from(&"x"));
         let expected = Tree::new("x");
         assert_eq!(tree, expected);
     }
@@ -33,8 +32,7 @@ mod from_pov {
         let mut tree = Tree::new("parent")
             .with_child(Tree::new("x"))
             .with_child(Tree::new("sibling"));
-        let from = "x";
-        assert!(tree.pov_from(&from));
+        assert!(tree.pov_from(&"x"));
         let expected =
             Tree::new("x").with_child(Tree::new("parent").with_child(Tree::new("sibling")));
         assert_eq!(tree, expected);
@@ -48,8 +46,7 @@ mod from_pov {
             .with_child(Tree::new("x"))
             .with_child(Tree::new("b"))
             .with_child(Tree::new("c"));
-        let from = "x";
-        assert!(tree.pov_from(&from));
+        assert!(tree.pov_from(&"x"));
         let expected = Tree::new("x").with_child(
             Tree::new("parent")
                 .with_child(Tree::new("a"))
@@ -65,8 +62,7 @@ mod from_pov {
         let mut tree = Tree::new("level-0").with_child(Tree::new("level-1").with_child(
             Tree::new("level-2").with_child(Tree::new("level-3").with_child(Tree::new("x"))),
         ));
-        let from = "x";
-        assert!(tree.pov_from(&from));
+        assert!(tree.pov_from(&"x"));
         let expected = Tree::new("x").with_child(Tree::new("level-3").with_child(
             Tree::new("level-2").with_child(Tree::new("level-1").with_child(Tree::new("level-0"))),
         ));
@@ -81,8 +77,7 @@ mod from_pov {
                 .with_child(Tree::new("kid-0"))
                 .with_child(Tree::new("kid-1")),
         );
-        let from = "x";
-        assert!(tree.pov_from(&from));
+        assert!(tree.pov_from(&"x"));
         let expected = Tree::new("x")
             .with_child(Tree::new("kid-0"))
             .with_child(Tree::new("kid-1"))
@@ -109,8 +104,7 @@ mod from_pov {
                     .with_child(Tree::new("cousin-0"))
                     .with_child(Tree::new("cousin-1")),
             );
-        let from = "x";
-        assert!(tree.pov_from(&from));
+        assert!(tree.pov_from(&"x"));
         let expected = Tree::new("x")
             .with_child(Tree::new("kid-1"))
             .with_child(Tree::new("kid-0"))
@@ -133,8 +127,7 @@ mod from_pov {
     #[ignore]
     fn errors_if_target_does_not_exist_in_a_singleton_tree() {
         let mut tree = Tree::new("x");
-        let from = "nonexistent";
-        assert!(!tree.pov_from(&from));
+        assert!(!tree.pov_from(&"nonexistent"));
     }
 
     #[test]
@@ -148,8 +141,7 @@ mod from_pov {
             )
             .with_child(Tree::new("sibling-0"))
             .with_child(Tree::new("sibling-1"));
-        let from = "nonexistent";
-        assert!(!tree.pov_from(&from));
+        assert!(!tree.pov_from(&"nonexistent"));
     }
 }
 
@@ -164,9 +156,7 @@ mod path_to {
         let mut tree = Tree::new("parent")
             .with_child(Tree::new("x"))
             .with_child(Tree::new("sibling"));
-        let from = "x";
-        let to = "parent";
-        let result = tree.path_to(&from, &to);
+        let result = tree.path_to(&"x", &"parent");
         let expected = Some(vec![&"x", &"parent"]);
         assert_eq!(result, expected);
     }
@@ -179,9 +169,7 @@ mod path_to {
             .with_child(Tree::new("x"))
             .with_child(Tree::new("b"))
             .with_child(Tree::new("c"));
-        let from = "x";
-        let to = "b";
-        let result = tree.path_to(&from, &to);
+        let result = tree.path_to(&"x", &"b");
         let expected = Some(vec![&"x", &"parent", &"b"]);
         assert_eq!(result, expected);
     }
@@ -205,9 +193,7 @@ mod path_to {
                     .with_child(Tree::new("cousin-0"))
                     .with_child(Tree::new("cousin-1")),
             );
-        let from = "x";
-        let to = "cousin-1";
-        let result = tree.path_to(&from, &to);
+        let result = tree.path_to(&"x", &"cousin-1");
         let expected = Some(vec![&"x", &"parent", &"grandparent", &"uncle", &"cousin-1"]);
         assert_eq!(result, expected);
     }
@@ -221,9 +207,7 @@ mod path_to {
                 .with_child(Tree::new("sibling-0"))
                 .with_child(Tree::new("sibling-1")),
         );
-        let from = "x";
-        let to = "sibling-1";
-        let result = tree.path_to(&from, &to);
+        let result = tree.path_to(&"x", &"sibling-1");
         let expected = Some(vec![&"x", &"parent", &"sibling-1"]);
         assert_eq!(result, expected);
     }
@@ -236,9 +220,7 @@ mod path_to {
             .with_child(Tree::new("x"))
             .with_child(Tree::new("b"))
             .with_child(Tree::new("c"));
-        let from = "a";
-        let to = "c";
-        let result = tree.path_to(&from, &to);
+        let result = tree.path_to(&"a", &"c");
         let expected = Some(vec![&"a", &"parent", &"c"]);
         assert_eq!(result, expected);
     }
@@ -254,9 +236,7 @@ mod path_to {
             )
             .with_child(Tree::new("sibling-0"))
             .with_child(Tree::new("sibling-1"));
-        let from = "x";
-        let to = "nonexistent";
-        let result = tree.path_to(&from, &to);
+        let result = tree.path_to(&"x", &"nonexistent");
         let expected: Option<Vec<_>> = None;
         assert_eq!(result, expected);
     }
@@ -272,9 +252,7 @@ mod path_to {
             )
             .with_child(Tree::new("sibling-0"))
             .with_child(Tree::new("sibling-1"));
-        let from = "nonexistent";
-        let to = "x";
-        let result = tree.path_to(&from, &to);
+        let result = tree.path_to(&"nonexistent", &"x");
         let expected: Option<Vec<_>> = None;
         assert_eq!(result, expected);
     }
