4d46bf58799c055a26312216809c9aa595a9fe0e
Remo Senekowitsch <remo@buenzli.dev>
2025-10-15 18:57:02 +0200

    Fix clippy warnings (#2104)

--- exercises/practice/prime-factors/.meta/example.rs
+++ exercises/practice/prime-factors/.meta/example.rs
@@ -3,7 +3,7 @@ pub fn factors(n: u64) -> Vec<u64> {
     let mut out: Vec<u64> = vec![];
     let mut possible: u64 = 2;
     while val > 1 {
-        while val % possible == 0 {
+        while val.is_multiple_of(possible) {
             out.push(possible);
             val /= possible;
         }
