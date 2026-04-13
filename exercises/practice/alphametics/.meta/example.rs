7366db176998855d79bb0f575c85938b9986b469
Remo Senekowitsch <remo@buenzli.dev>
2026-04-13 16:59:04 +0200

    alphametics: improve example solution
    The previous example didn't compile in the test runner, because the
    dependencies aren't present anymore. I was able to fix it with minor
    adjustments, but it was very slow. So, I replaced it with a faster
    solution to speed up runs of the verify_exercises_in_docker script.

--- exercises/practice/alphametics/.meta/example.rs
+++ exercises/practice/alphametics/.meta/example.rs
@@ -1,81 +1,46 @@
-// This is a brute-force solution, use `cargo test --release` for faster testing
-
-extern crate itertools;
-extern crate permutohedron;
+use std::collections::HashMap;
+use std::iter;
 
 use itertools::Itertools;
-use permutohedron::Heap as Permutations;
 
-use std::char;
-use std::collections::HashMap;
-use std::collections::HashSet;
+pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
+    let (terms, solution) = input.split_once(" == ").unwrap();
+    let numbers = iter::once(solution)
+        .chain(terms.split(" + "))
+        .map(|word| word.chars().map(|c| (c as u8) - b'A').collect::<Vec<_>>())
+        .collect::<Vec<_>>();
 
-fn test_equation(
-    coefficients: &HashMap<char, i64>,
-    cant_be_zero: &HashSet<char>,
-    substitutions: &HashMap<char, u8>,
-) -> bool {
-    if cant_be_zero.iter().any(|d| substitutions[d] == 0) {
-        return false;
-    }
+    let mut unique = numbers.iter().flatten().copied().collect::<Vec<_>>();
+    unique.sort_unstable();
+    unique.dedup();
+    let mut assignment = [0; 26];
 
-    coefficients
-        .iter()
-        .map(|(d, &coeff)| i64::from(substitutions[d]) * coeff)
-        .sum::<i64>()
-        == 0
-}
-
-fn letter_coefficients(puzzle: &str) -> (HashMap<char, i64>, HashSet<char>) {
-    let mut coefficients = HashMap::new();
-    let mut cant_be_zero = HashSet::new();
-
-    // Split the puzzle into left and right side
-    let equation: Vec<&str> = puzzle.split("==").collect();
-
-    {
-        let mut insert_term = |term: &str, polarity: i64| {
-            cant_be_zero.insert(term.chars().next().unwrap());
-            let mut power_of_ten = polarity;
-            for letter in term.chars().rev() {
-                *coefficients.entry(letter).or_insert(0) += power_of_ten;
-                power_of_ten *= 10;
-            }
-        };
-
-        // Sum the parts on the left side
-        for left_term in equation[0].split('+').map(str::trim) {
-            insert_term(left_term, 1);
+    for perm in (0..10).permutations(unique.len()) {
+        for (&letter, value) in unique.iter().zip(perm) {
+            assignment[letter as usize] = value;
         }
 
-        let right_term = equation[1].trim();
-        insert_term(right_term, -1);
-    }
+        if numbers.iter().any(|n| assignment[n[0] as usize] == 0) {
+            continue; // leading zero is invalid
+        }
 
-    (coefficients, cant_be_zero)
-}
+        let sum: u64 = numbers[1..].iter().map(|n| parse(n, &assignment)).sum();
+        let solution = parse(&numbers[0], &assignment);
 
-pub fn solve(puzzle: &str) -> Option<HashMap<char, u8>> {
-    let (coefficients, cant_be_zero) = letter_coefficients(puzzle);
-    let letters: Vec<char> = coefficients.keys().cloned().collect();
-
-    // All available numbers for substitution
-    let numbers: &[u8] = &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-    // Iterate every combination with the length of unique letters in the puzzle
-    for combinations in numbers.iter().combinations(letters.len()) {
-        let mut c = combinations;
-        let permutations = Permutations::new(&mut c);
-        // Iterate every permutation of a letter combination
-        for p in permutations {
-            let substitution: HashMap<char, u8> =
-                letters.iter().zip(p).map(|(&c, &n)| (c, n)).collect();
-            if test_equation(&coefficients, &cant_be_zero, &substitution) {
-                // We found a good substitution
-                return Some(substitution);
-            }
+        if sum == solution {
+            return Some(
+                unique
+                    .into_iter()
+                    .map(|c| ((c + b'A') as char, assignment[c as usize]))
+                    .collect(),
+            );
         }
     }
-    // If we tested every combination and did not found a solution then return None
     None
+}
+
+fn parse(number: &[u8], assignment: &[u8; 26]) -> u64 {
+    number.iter().fold(0, |acc, next| {
+        acc * 10 + (assignment[*next as usize] as u64)
+    })
 }
