b534ab3fed96a30bc0c5812320cec0034d61da6b
Remo Senekowitsch <remo@buenzli.dev>
2026-07-02 20:32:13 +0200

    bob: sync (#2156)
    There have been two recent PRs to the upstream data for these tests. One
    added a test, the other reordered the existing ones.
    
    The added test case doesn't seem important, so I'm adding:
    [no important files changed]

--- exercises/practice/bob/tests/bob.rs
+++ exercises/practice/bob/tests/bob.rs
@@ -1,8 +1,11 @@
 use bob::*;
 
 #[test]
-fn stating_something() {
-    assert_eq!(reply("Tom-ay-to, tom-aaaah-to."), "Whatever.");
+fn asking_a_question() {
+    assert_eq!(
+        reply("Does this cryogenic chamber make me look fat?"),
+        "Sure."
+    );
 }
 
 #[test]
@@ -13,17 +16,23 @@ fn shouting() {
 
 #[test]
 #[ignore]
-fn shouting_gibberish() {
-    assert_eq!(reply("FCECDFCAAB"), "Whoa, chill out!");
+fn forceful_question() {
+    assert_eq!(
+        reply("WHAT'S GOING ON?"),
+        "Calm down, I know what I'm doing!"
+    );
 }
 
 #[test]
 #[ignore]
-fn asking_a_question() {
-    assert_eq!(
-        reply("Does this cryogenic chamber make me look fat?"),
-        "Sure."
-    );
+fn silence() {
+    assert_eq!(reply(""), "Fine. Be that way!");
+}
+
+#[test]
+#[ignore]
+fn stating_something() {
+    assert_eq!(reply("Tom-ay-to, tom-aaaah-to."), "Whatever.");
 }
 
 #[test]
@@ -40,44 +49,53 @@ fn asking_gibberish() {
 
 #[test]
 #[ignore]
-fn talking_forcefully() {
-    assert_eq!(reply("Hi there!"), "Whatever.");
+fn question_with_no_letters() {
+    assert_eq!(reply("4?"), "Sure.");
 }
 
 #[test]
 #[ignore]
-fn using_acronyms_in_regular_speech() {
+fn non_letters_with_question() {
+    assert_eq!(reply(":) ?"), "Sure.");
+}
+
+#[test]
+#[ignore]
+fn prattling_on() {
+    assert_eq!(reply("Wait! Hang on. Are you going to be OK?"), "Sure.");
+}
+
+#[test]
+#[ignore]
+fn ending_with_whitespace() {
+    assert_eq!(reply("Okay if like my  spacebar  quite a bit?   "), "Sure.");
+}
+
+#[test]
+#[ignore]
+fn multiple_line_question() {
     assert_eq!(
-        reply("It's OK if you don't want to go work for NASA."),
-        "Whatever."
+        reply("\nDoes this cryogenic chamber make\n me look fat?"),
+        "Sure."
     );
 }
 
 #[test]
 #[ignore]
-fn forceful_question() {
-    assert_eq!(
-        reply("WHAT'S GOING ON?"),
-        "Calm down, I know what I'm doing!"
-    );
+fn shouting_gibberish() {
+    assert_eq!(reply("FCECDFCAAB"), "Whoa, chill out!");
+}
+
+#[test]
+#[ignore]
+fn shouting_a_statement_containing_a_question_mark() {
+    assert_eq!(reply("DO LIONS EAT PEOPLE? AHHHHH."), "Whoa, chill out!");
 }
 
 #[test]
 #[ignore]
 fn shouting_numbers() {
     assert_eq!(reply("1, 2, 3 GO!"), "Whoa, chill out!");
-}
-
-#[test]
-#[ignore]
-fn no_letters() {
-    assert_eq!(reply("1, 2, 3"), "Whatever.");
-}
-
-#[test]
-#[ignore]
-fn question_with_no_letters() {
-    assert_eq!(reply("4?"), "Sure.");
 }
 
 #[test]
@@ -97,30 +115,6 @@ fn shouting_with_no_exclamation_mark() {
 
 #[test]
 #[ignore]
-fn statement_containing_question_mark() {
-    assert_eq!(reply("Ending with ? means a question."), "Whatever.");
-}
-
-#[test]
-#[ignore]
-fn non_letters_with_question() {
-    assert_eq!(reply(":) ?"), "Sure.");
-}
-
-#[test]
-#[ignore]
-fn prattling_on() {
-    assert_eq!(reply("Wait! Hang on. Are you going to be OK?"), "Sure.");
-}
-
-#[test]
-#[ignore]
-fn silence() {
-    assert_eq!(reply(""), "Fine. Be that way!");
-}
-
-#[test]
-#[ignore]
 fn prolonged_silence() {
     assert_eq!(reply("          "), "Fine. Be that way!");
 }
@@ -133,20 +127,41 @@ fn alternate_silence() {
 
 #[test]
 #[ignore]
-fn starting_with_whitespace() {
-    assert_eq!(reply("         hmmmmmmm..."), "Whatever.");
-}
-
-#[test]
-#[ignore]
-fn ending_with_whitespace() {
-    assert_eq!(reply("Okay if like my  spacebar  quite a bit?   "), "Sure.");
-}
-
-#[test]
-#[ignore]
 fn other_whitespace() {
     assert_eq!(reply("\n\r \t"), "Fine. Be that way!");
+}
+
+#[test]
+#[ignore]
+fn talking_forcefully() {
+    assert_eq!(reply("Hi there!"), "Whatever.");
+}
+
+#[test]
+#[ignore]
+fn using_acronyms_in_regular_speech() {
+    assert_eq!(
+        reply("It's OK if you don't want to go work for NASA."),
+        "Whatever."
+    );
+}
+
+#[test]
+#[ignore]
+fn no_letters() {
+    assert_eq!(reply("1, 2, 3"), "Whatever.");
+}
+
+#[test]
+#[ignore]
+fn statement_containing_question_mark() {
+    assert_eq!(reply("Ending with ? means a question."), "Whatever.");
+}
+
+#[test]
+#[ignore]
+fn starting_with_whitespace() {
+    assert_eq!(reply("         hmmmmmmm..."), "Whatever.");
 }
 
 #[test]
@@ -155,14 +170,5 @@ fn non_question_ending_with_whitespace() {
     assert_eq!(
         reply("This is a statement ending with whitespace      "),
         "Whatever."
-    );
-}
-
-#[test]
-#[ignore]
-fn multiple_line_question() {
-    assert_eq!(
-        reply("\nDoes this cryogenic chamber make\n me look fat?"),
-        "Sure."
     );
 }
